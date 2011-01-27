c375c0770cf598f6203b424e6bb400db21813a04
Josh Guilfoyle <Josh.Guilfoyle@T-Mobile.com>
2011-01-26 23:28:43 -0800

    Disable Zygote preloaded drawables.
    With a theme applied, most of these preloaded drawables go unused.  Any
    assets the theme has redirected will need to be loaded with each app
    process regardless.  Worse, preloads make it impossible to do asset
    redirection for constituent parts of a preloaded drawable (for instance,
    individual states of a StateListDrawable cannot be redirected).
    
    Some day it might be nice to revisit this and see if there's a way to
    reintroduce the drawable cache in a way that can be altered at runtime
    without significant complexity or runtime penalty.
    
    Change-Id: I42afa37bcf720b19b937fb53929501fd0de3ae97

--- core/java/com/android/internal/os/ZygoteInit.java
+++ core/java/com/android/internal/os/ZygoteInit.java
@@ -97,7 +97,7 @@ public class ZygoteInit {
     private static final String PRELOADED_CLASSES = "/system/etc/preloaded-classes";
 
     /** Controls whether we should preload resources during zygote init. */
-    private static final boolean PRELOAD_RESOURCES = true;
+    private static final boolean PRELOAD_RESOURCES = false;
 
     /**
      * Registers a server socket for zygote command connections
@@ -347,6 +347,8 @@ public class ZygoteInit {
                 ar.recycle();
                 Log.i(TAG, "...preloaded " + N + " resources in "
                         + (SystemClock.uptimeMillis()-startTime) + "ms.");
+            } else {
+                Log.i(TAG, "Preload resources disabled, skipped.");
             }
             mResources.finishPreloading();
         } catch (RuntimeException e) {
