3af94d5d1c46b35d4f3c949ed3219e6824dec355
Danesh Mondegarian <daneshm90@gmail.com>
2012-08-10 14:56:11 -0400

    [1/2] base: NavigationBar customization
    Includes:
    
    - Allows user to customize navigation bar.
      Features :
       - Choose from 1-6 # of buttons
       - Re-arrange / assign buttons
      Id: Idc9f5f401f15d207504e49de47666bb5510a06ca
    
    - Navbar : Rework logic for determining orientation
      This patch introduces better checks for hybrid ui's.
      This should fix issues of small buttons due to incorrect ui determination
      Thanks Rolf Arne Schulze for testing :)
      Id: If5b61dc4764a98deedb5b801d11451fed2488cba
    
    - Make navigation bar ready for per-user configuration.
      Also clean it up a bit.
      Id: Ic10bd161fce53bf30048a0cb927dcf1e2bfa7774
    
    - Fix layout on N10
    - Remove uncessary padding and logic
    - Phone fixes
    - Phone padding fixes
    
    Change-Id: I5bcdd53e8344e8ce137088544a4a3964655c53af

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2966,6 +2966,13 @@ public final class Settings {
         public static final String STATUS_BAR_CUSTOM_HEADER = "status_bar_custom_header";
 
         /**
+         * Navigation controls to Use
+         *
+         * @hide
+         */
+        public static final String NAV_BUTTONS = "nav_buttons";
+
+        /**
          * Volume key controls ringtone or media sound stream
          *
          * @hide
