20100bb2c41b4a906c5a153ad019215697c4fe44
Andy Mast <andy@cyngn.com>
2012-11-20 21:17:17 +0000

    [4/8] Base: Theme engine
    Parts of patch authored by: Ricardo Cerqueira <cyanogenmod@cerqueira.org>
    Nothing of this would be possible without Josh, Ed,
    and the rest of the TMo guys who wrote and maintained it until 2.2:
    
    Amit Chabra <amit.chabra@t-mobile.com>
    Amit Kohli <amit.kohli@t-mobile.com>
    Chris Cogar <chriscogar@t-mobile.com>
    Dirk Sigurdson <dirk.sigurdson@t-mobile.com>
    Ed Carrigan <edward.carrigan@t-mobile.com>
    Gaurav Sharma <gaurav.sharma3@t-mobile.com>
    Hui Feng <hui.feng@t-mobile.com>
    John Ritz <john.ritz1@t-mobile.com>
    Josh Guilfoyle <josh.guilfoyle@t-mobile.com>
    Mark Roberts <mark.roberts48@t-mobile.com>
    Pankaj Kumar <Pankaj.kumar6@t-mobile.com>
    Samuel Cheung <samuel.cheung@t-mobile.com>
    Sergey Ten <sergey.ten6@t-mobile.com>
    
    Major contributions by Marten Kongstad <marten.kongstad@sonymobile.com>
    
    Change-Id: Ie1cbb9c0506ce382ba0acfb06ad8afc19f4169c5

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -143,6 +143,8 @@ class QuickSettings {
     private BluetoothController mBluetoothController;
     private RotationLockController mRotationLockController;
     private LocationController mLocationController;
+    private NetworkController mNetworkController;
+    private BatteryController mBatteryController;
 
     private AsyncTask<Void, Void, Pair<String, Drawable>> mUserInfoTask;
     private AsyncTask<Void, Void, Pair<Boolean, Boolean>> mQueryCertTask;
@@ -211,6 +213,8 @@ class QuickSettings {
         mBluetoothController = bluetoothController;
         mRotationLockController = rotationLockController;
         mLocationController = locationController;
+        mNetworkController = networkController;
+        mBatteryController = batteryController;
 
         setupQuickSettings();
         updateResources();
@@ -1311,6 +1315,26 @@ class QuickSettings {
             array.add(tile.toString());
         }
         return array;
+    }
+
+    public void shutdown() {
+        if (mReceiver != null) {
+            mContext.unregisterReceiver(mReceiver);
+        }
+        if (mProfileReceiver != null) {
+            mContext.unregisterReceiver(mProfileReceiver);
+        }
+        if (mModel != null) {
+            mNetworkController.removeNetworkSignalChangedCallback(mModel);
+            mBluetoothController.removeStateChangedCallback(mModel);
+            mBatteryController.removeStateChangedCallback(mModel);
+            mLocationController.removeSettingsChangedCallback(mModel);
+            mRotationLockController.removeRotationLockControllerCallback(mModel);
+            mModel = null;
+        }
+        if (mContainerView != null) {
+            mContainerView.removeAllViews();
+        }
     }
 
     public void updateTiles() {
