dc7fd5080bc63bab08b21d8469621b1fb8e8cf51
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-11-20 21:17:17 +0000

    Squashed commit of the theme engine support. [2/4]
    Updated 4.2 support for the old T-Mobile theme engine, as usual
    needs the provider and switcher apps installed as well.
    
    I'm finally dropping the 400+ commit history on this, since everybody
    else keeps picking it up from CM as a single patch anyway... But
    for the record, nothing of this would be possible without Josh, Ed,
    and the rest of the TMo guys who wrote and maintained it until 2.2:
    
    amit chabra <amit.chabra@t-mobile.com>
    Amit Kohli <amit.kohli@t-mobile.com>
    Chris Cogar <chriscogar@t-mobile.com>
    Dirk Sigurdson <dirk.sigurdson@t-mobile.com>
    Ed Carrigan <edward.carrigan@t-mobile.com>
    Gaurav Sharma <gaurav.sharma3@t-mobile.com>
    Hui Feng <hui.feng@t-mobile.com>
    John Ritz <john.ritz1@t-mobile.com>
    Josh Guilfoyle <josh.guilfoyle@t-mobile.com>
    Mark Roberts <mark.roberts48@t-mobile.com>
    Pankaj Kumar <Pankaj.kumar6@t-mobile.com>
    Samuel Cheung <samuel.cheung@t-mobile.com>
    Sergey Ten <sergey.ten6@t-mobile.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	policy/src/com/android/internal/policy/impl/GlobalActions.java
    	services/java/com/android/server/NotificationManagerService.java
    	services/java/com/android/server/SystemServer.java
    	services/java/com/android/server/am/ActivityManagerService.java
    	services/java/com/android/server/wm/WindowManagerService.java
    
    Signed-off-by: mithun46 <mithuncursed4eva@gmail.com>
    
    Change-Id: Ie1cbb9c0506ce382ba0acfb06ad8afc19f4169c5

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -63,6 +63,7 @@ import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.view.WindowManager;
 import android.view.WindowManagerGlobal;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupMenu;
@@ -149,6 +150,8 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected IDreamManager mDreamManager;
     PowerManager mPowerManager;
     protected int mRowHeight;
+
+    protected FrameLayout mStatusBarContainer;
 
     // UI-specific methods
 
@@ -265,6 +268,8 @@ public abstract class BaseStatusBar extends SystemUI implements
 
         mLocale = mContext.getResources().getConfiguration().locale;
         mLayoutDirection = TextUtils.getLayoutDirectionFromLocale(mLocale);
+
+        mStatusBarContainer = new FrameLayout(mContext);
 
         // Connect in to the status bar manager service
         StatusBarIconList iconList = new StatusBarIconList();
