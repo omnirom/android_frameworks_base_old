2ae35ae9e0a30f69fdac466dc2b553a6de136928
nebkat <nebkat@teamhacksung.org>
2012-12-14 10:59:40 +0000

    [1/2] SystemUI: Pull down QS if x > 70% width
    Added Settings
    
    PS3: rebase
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Change-Id: Ic41d331e8f0166484b124e91a8ed8e904ff38edb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3118,6 +3118,12 @@ public final class Settings {
         };
 
         /**
+         * Whether to have statusbar right side pulldown.  The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String STATUS_BAR_QS_QUICK_PULLDOWN = "status_bar_qs_quick_pulldown";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
