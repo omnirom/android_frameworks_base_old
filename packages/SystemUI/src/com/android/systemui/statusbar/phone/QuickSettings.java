de5128b0be942cf8d912855858450b5ae4ced97a
DvTonder <david.vantonder@gmail.com>
2013-02-07 21:26:11 -0500

    Quick Settings: Add visual indicator of ON/OFF Setting of Mobile Data tile
    Change-Id: I7dc0c8943df7a9478655bdde6745662c3831288f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -549,7 +549,7 @@ class QuickSettings {
                     if (rssiState.dataTypeIconId > 0) {
                         iov.setImageResource(rssiState.dataTypeIconId);
                     } else {
-                        iov.setImageDrawable(null);
+                        iov.setImageResource(R.drawable.ic_qs_signal_data_off);
                     }
                     setActivity(view, rssiState);
 
