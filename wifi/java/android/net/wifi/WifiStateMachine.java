ed134cf3e2020e9998abe437b55bbe823965876d
Bruno Randolf <br1@einfach.org>
2013-03-22 17:12:17 +0000

    Wifi: Get the list of supported channels
    Get the list of supported channels/frequencies from wpa_supplicant and provide
    them thru WifiManager.getSupportedChannels();
    
    One channel is represented thru the class WifiChannel which includes channel
    number, frequency in MHz and a flag if IBSS mode is allowed on this channel.
    
    Like isIbssSupported() we can only expect a result after WIFI_STATE_ENABLED has
    been reached.
    
    Change-Id: I9fc34642b9a0047f6871b180a3a7a69d47b7e2ff

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -169,6 +169,8 @@ public class WifiStateMachine extends StateMachine {
 
     private PowerManager.WakeLock mSuspendWakeLock;
 
+    private List<WifiChannel> mSupportedChannels;
+
     /**
      * Interval in milliseconds between polling for RSSI
      * and linkspeed information
@@ -1568,6 +1570,13 @@ public class WifiStateMachine extends StateMachine {
         return result;
     }
 
+    public List<WifiChannel> syncGetSupportedChannels(AsyncChannel channel) {
+        Message resultMsg = channel.sendMessageSynchronously(CMD_GET_SUPPORTED_CHANNELS);
+        List<WifiChannel> result = (List<WifiChannel>) resultMsg.obj;
+        resultMsg.recycle();
+        return result;
+    }
+
     /**
      * Set the operational frequency band
      * @param band
@@ -2506,6 +2515,9 @@ public class WifiStateMachine extends StateMachine {
                 case CMD_GET_IBSS_SUPPORTED:
                     replyToMessage(message, message.what, FAILURE);
                     break;
+                case CMD_GET_SUPPORTED_CHANNELS:
+                    replyToMessage(message, message.what, (List<WifiChannel>) null);
+                    break;
                 case CMD_GET_CONFIGURED_NETWORKS:
                     replyToMessage(message, message.what, (List<WifiConfiguration>) null);
                     break;
@@ -2794,6 +2806,7 @@ public class WifiStateMachine extends StateMachine {
                     initializeWpsDetails();
 
                     mIbssSupported = mWifiNative.getModeCapability("IBSS");
+                    mSupportedChannels = mWifiNative.getSupportedChannels();
 
                     sendSupplicantConnectionChangedBroadcast(true);
                     transitionTo(mDriverStartedState);
@@ -2824,6 +2837,7 @@ public class WifiStateMachine extends StateMachine {
                 case CMD_START_PACKET_FILTERING:
                 case CMD_STOP_PACKET_FILTERING:
                 case CMD_GET_IBSS_SUPPORTED:
+                case CMD_GET_SUPPORTED_CHANNELS:
                     deferMessage(message);
                     break;
                 default:
@@ -2888,6 +2902,7 @@ public class WifiStateMachine extends StateMachine {
                     mOperationalMode = message.arg1;
                     break;
                 case CMD_GET_IBSS_SUPPORTED:
+                case CMD_GET_SUPPORTED_CHANNELS:
                     deferMessage(message);
                     break;
                 default:
@@ -3251,6 +3266,9 @@ public class WifiStateMachine extends StateMachine {
                     break;
                 case CMD_GET_IBSS_SUPPORTED:
                     replyToMessage(message, message.what, mIbssSupported ? 1 : 0);
+                    break;
+                case CMD_GET_SUPPORTED_CHANNELS:
+                    replyToMessage(message, message.what, mSupportedChannels);
                     break;
                 default:
                     return NOT_HANDLED;
