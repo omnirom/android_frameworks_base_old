72c112524cb0f155e9b389375843c45d7b0fc48e
Schubi <schubi@erlangen.ccc.de>
2013-06-17 16:58:25 +0200

    [1/2] Base: Option to enable Notification Light when screen is on
    This makes it possible to let the Notification LED flash even when the screen is on
    
    Change-Id: I58be9646cdc84d4765342a5ab45b4641c3d5daba

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2795,6 +2795,13 @@ public final class Settings {
         public static final String ACTIVE_DISPLAY_BYPASS = "active_display_bypass";
 
         /**
+         * Screen-On Notification Light,
+         * should default to 1 (yes, Notification Light is enabled when screen is on)
+         * @hide
+         */
+        public static final String SCREEN_ON_NOTIFICATION_LED = "screen_on_notification_led";
+
+        /**
          * @deprecated Use {@link android.provider.Settings.Global#LOW_BATTERY_SOUND}
          * instead
          * @hide
