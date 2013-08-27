40723e2496739067aadf1ad4e72ea39ea13cae9b
Pulser <pulser@xda-developers.com>
2013-08-27 11:07:38 +0100

    Framework: Add Advanced reboot (2 of 2) By DvTonder
    This commit responds to a setting in Development settings for including
    options in the power menu for rebooting into recovery or bootloader. It
    is defauled to off.
    
    When enabled, the Advanced reboot options will only be available once the
    device is unlocked.
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/custom_arrays.xml
    	core/res/res/values/symbols.xml
    
    Change-Id: I675f31de1f82c344c7923e953fd4a6e26dd036da

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6341,6 +6341,12 @@ public final class Settings {
         public static final String LOW_BATTERY_SOUND_TIMEOUT = "low_battery_sound_timeout";
 
         /**
+         * Whether to include options in power menu for rebooting into recovery or bootloader
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -6371,7 +6377,8 @@ public final class Settings {
             WIFI_NUM_OPEN_NETWORKS_KEPT,
             EMERGENCY_TONE,
             CALL_AUTO_RETRY,
-            DOCK_AUDIO_MEDIA_ENABLED
+            DOCK_AUDIO_MEDIA_ENABLED,
+            ADVANCED_REBOOT
         };
 
         // Populated lazily, guarded by class object:
