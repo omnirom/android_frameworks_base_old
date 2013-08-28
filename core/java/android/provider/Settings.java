e786d487399a1920f8c6d252188a1f2d9e62ed08
xplodwild <xplod@ouverta.fr>
2013-08-28 14:08:00 +0100

    Settings: add HEADSET_CONNECT_PLAYER key
    Change-Id: I1250d44fdde7f6d508ac6e49c4c6bb85c9f66233

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2416,6 +2416,12 @@ public final class Settings {
         public static final String EGG_MODE = "egg_mode";
 
         /**
+         * Whether or not to launch default music player when headset is connected
+         * @hide
+         */
+        public static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
