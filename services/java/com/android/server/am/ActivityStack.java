5520920ddefe2f3f94ecac30a2dcbcb4b4c2a434
xplodwild <xplod@ouverta.fr>
2013-09-03 20:55:56 +0200

    [WIP] Multi-window
    This is a big work in progress, which currently has some bugs,
    but begins to look okay.
    
    You can already multitask a little bit, by using the
    app switcher and long-pressing an icon, then select "Add to
    Split View".
    Last window touched gets the key focus.
    
    Code is very ugly, I know. Don't merge it, don't use it
    for now. Unless you want to make your code ugly.
    
    Enjoy :)
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
    
    Change-Id: I2cbf4a3998a6859bafe8d09757e07358661604db

--- services/java/com/android/server/am/ActivityStack.java
+++ services/java/com/android/server/am/ActivityStack.java
@@ -73,8 +73,11 @@ import android.os.RemoteException;
 import android.os.SystemClock;
 import android.os.UserHandle;
 import android.util.EventLog;
+import android.util.Log;
 import android.util.Slog;
 import android.view.Display;
+import android.view.IWindowManager;
+import android.view.WindowManagerGlobal;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -1060,13 +1063,11 @@ final class ActivityStack {
                                 mStackSupervisor.startSpecificActivityLocked(r, false, false);
                             }
                         }
-
                     } else if (r.visible) {
                         // If this activity is already visible, then there is nothing
                         // else to do here.
                         if (DEBUG_VISBILITY) Slog.v(TAG, "Skipping: already visible at " + r);
                         r.stopFreezingScreenLocked(false);
-
                     } else if (onlyThisProcess == null) {
                         // This activity is not currently visible, but is running.
                         // Tell it to become visible.
@@ -1097,7 +1098,16 @@ final class ActivityStack {
                     // Aggregate current change flags.
                     configChanges |= r.configChangeFlags;
 
-                    if (r.fullscreen) {
+                    boolean isSplitView = false;
+
+                    try {
+                        IWindowManager wm = (IWindowManager) WindowManagerGlobal.getWindowManagerService();
+                        isSplitView = wm.isTaskSplitView(r.task.taskId);
+                    } catch (RemoteException e) {
+                        Slog.e(TAG, "Cannot get split view status", e);
+                    }
+
+                    if (r.fullscreen && !isSplitView) {
                         // At this point, nothing else needs to be shown
                         if (DEBUG_VISBILITY) Slog.v(TAG, "Fullscreen: at " + r);
                         behindFullscreen = true;
@@ -1218,6 +1228,7 @@ final class ActivityStack {
      * nothing happened.
      */
     final boolean resumeTopActivityLocked(ActivityRecord prev) {
+		Log.e("XPLOD", "Resume Top Activity Locked " + prev);
         return resumeTopActivityLocked(prev, null);
     }
 
