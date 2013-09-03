ff8467ce8b3de257e423637938801cf4007534de
xplodwild <xplod@ouverta.fr>
2013-09-03 20:55:56 +0200

    [WIP] Multi-window
    This is a big work in progress, which currently has some bugs,
    but begins to look okay.
    
    You can already multitask a little bit, by using the
    app switcher and long-pressing an icon, then select "Add to
    Split View".
    Last window touched gets the key focus.
    
    Code is very ugly, I know. Don't merge it, don't use it
    for now. Unless you want to make your code ugly.
    
    Enjoy :)
    
    Change-Id: I4a03a84539fd3ac1273f0d3503fe8fafcd16be26

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -2946,6 +2946,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
         }
     }
+    
+    public Rect getContentRect() {
+        return new Rect(mContentLeft, mContentTop, mContentRight, mContentBottom);
+    }
 
     /** {@inheritDoc} */
     @Override
