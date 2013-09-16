d4d2edffa49cf45c46325535758a07c44a3e724b
Matthias Bosc <matthias.bosc@gmail.com>
2013-09-16 16:40:01 +0000

    Frameworks & SystemUI - additional DC-HSPAP support
    This commit adds some more informations about Dual-Carrier HSPAP
    It allows displaying a "DC" icon on status bar for compatible devices
    and add a "DCHSPAP" value in state phone
    patch 1: add TelephonyManager to fix build
    patch 2: modify ServiceState
    patch 3: switch to cm_strings
    patch 4: fix a mistake
    patch 5: upload a new set of icons, a bit prettier than previous
    patch 6: update icons (more opacity to match colors)
    patch 7: removed drawable-sw*
    
    here is a screenshot for example: http://imgur.com/mQ2du3l,PSyHoqC
    
    Change-Id: I77d05e23c8ad3efdfeb19cf06ed92e7c57105ebe

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -611,6 +611,8 @@ public class TelephonyManager {
     public static final int NETWORK_TYPE_EHRPD = 14;
     /** Current network is HSPA+ */
     public static final int NETWORK_TYPE_HSPAP = 15;
+    /** Current network is DC-HSPAP */
+    public static final int NETWORK_TYPE_DCHSPAP = 30;
 
     /**
      * @return the NETWORK_TYPE_xxxx for current data connection.
@@ -640,6 +642,7 @@ public class TelephonyManager {
      * @see #NETWORK_TYPE_LTE
      * @see #NETWORK_TYPE_EHRPD
      * @see #NETWORK_TYPE_HSPAP
+     * @see #NETWORK_TYPE_DCHSPAP
      *
      * @hide
      */
@@ -716,6 +719,7 @@ public class TelephonyManager {
             case NETWORK_TYPE_EVDO_B:
             case NETWORK_TYPE_EHRPD:
             case NETWORK_TYPE_HSPAP:
+            case NETWORK_TYPE_DCHSPAP:
                 return NETWORK_CLASS_3_G;
             case NETWORK_TYPE_LTE:
                 return NETWORK_CLASS_4_G;
@@ -768,6 +772,8 @@ public class TelephonyManager {
                 return "iDEN";
             case NETWORK_TYPE_HSPAP:
                 return "HSPA+";
+            case NETWORK_TYPE_DCHSPAP:
+                return "DCHSPAP";
             default:
                 return "UNKNOWN";
         }
