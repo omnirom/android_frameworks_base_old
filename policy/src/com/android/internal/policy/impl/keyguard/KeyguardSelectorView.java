e0a174da5a5d3b37d25903a8a0defe71507ab024
xplodwild <xplod@ouverta.fr>
2013-09-26 14:39:39 +0100

    [WIP] Add battery level around unlock ring
    Change-Id: Ia4dbc2335657c096c34349ec67c4adfec0aeb91b

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardSelectorView.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardSelectorView.java
@@ -25,12 +25,15 @@ import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.InsetDrawable;
+import android.os.BatteryManager;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.text.TextUtils;
@@ -67,6 +70,7 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
     private String[] mStoredTargets;
     private int mTargetOffset;
     private boolean mIsScreenLarge;
+    private float mBatteryLevel;
 
     OnTriggerListener mOnTriggerListener = new OnTriggerListener() {
 
@@ -150,6 +154,11 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
         @Override
         public void onSimStateChanged(State simState) {
             updateTargets();
+        }
+
+        @Override
+        public void onRefreshBatteryInfo(KeyguardUpdateMonitor.BatteryStatus batStatus) {
+            updateLockscreenBattery(batStatus);
         }
     };
 
@@ -244,6 +253,7 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
         mSearchDisabled = disabledBySimState || !searchActionAvailable || !searchTargetPresent
                 || !currentUserSetup;
         updateResources();
+        updateLockscreenBattery(null);
     }
 
     public void updateResources() {
@@ -425,5 +435,25 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
         mIsBouncing = false;
         KeyguardSecurityViewHelper.
                 hideBouncer(mSecurityMessageDisplay, mFadeView, mBouncerFrame, duration);
+    }
+
+    public void updateLockscreenBattery(KeyguardUpdateMonitor.BatteryStatus status) {
+        // TODO: Settings
+	Color mColor = new Color();
+        if (true) {
+	    float mCappedBattery;
+            if (status != null) mBatteryLevel = status.level;
+	    if(mBatteryLevel < 15) {
+		mCappedBattery = 15;
+	    }
+	    else if(mBatteryLevel > 90) {
+		mCappedBattery = 90;
+	    }
+	    else {
+		mCappedBattery = mBatteryLevel;
+	    }
+	    float mHue = (mCappedBattery - 15) * 1.6f;
+            mGlowPadView.setArc(mBatteryLevel * 3.6f, mColor.HSVToColor(0x80,new float[]{ mHue, 1.f, 1.f }));
+        }
     }
 }
