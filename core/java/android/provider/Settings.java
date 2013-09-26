0e39d531910e8876d8990ab2d26f5a32191ab7a8
xplodwild <xplod@ouverta.fr>
2013-09-26 14:39:39 +0100

    [1/2] Add battery level around unlock ring
    Change-Id: Ia4dbc2335657c096c34349ec67c4adfec0aeb91b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2662,6 +2662,12 @@ public final class Settings {
         public static final String TRANSLUCENT_NAVIGATION_BAR = "translucent_navigation_bar";
 
         /**
+         * Whether or not to show circle battery around the lockscreen ring
+         * @hide
+         */
+        public static final String BATTERY_AROUND_LOCKSCREEN_RING = "battery_around_lockscreen_ring";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
