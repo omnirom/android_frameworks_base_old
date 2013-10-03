c78af86b2b0cc8d868d6f43cccb26251ca33a158
maxwen <max.weninger@gmail.com>
2013-10-03 22:44:56 +0200

    framework: volume wake
    If enabled pressing volume key will wake the device
    and screen. If screen is off because of the proxi
    sensor e.g. in call it will not be handled.
    
    Change-Id: I189bcfed7228f24c030784d490fb7a35e382b7f6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2656,6 +2656,12 @@ public final class Settings {
         public static final String STATUS_BAR_TRANSPARENT_ON_KEYGUARD = "status_bar_transparent_on_keyguard";
 
         /**
+         * volume rocker wake
+         * @hide
+         */
+        public static final String VOLUME_WAKE_SCREEN = "volume_wake_screen";
+        
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
