06d8934a7742772a7b6006191187bfc865d4aeef
maxwen <max.weninger@gmail.com>
2013-10-03 22:44:56 +0200

    [1/3] framework: volume wake
    If enabled pressing volume key will wake the device
    and screen. If screen is off because of the proxi
    sensor e.g. in call it will not be handled.
    
    PS2:
    whitespaces
    
    PS3:
    rebased
    
    Change-Id: I189bcfed7228f24c030784d490fb7a35e382b7f6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2735,6 +2735,12 @@ public final class Settings {
         public static final String BATTERY_AROUND_LOCKSCREEN_RING = "battery_around_lockscreen_ring";
 
         /**
+         * volume rocker wake
+         * @hide
+         */
+        public static final String VOLUME_WAKE_SCREEN = "volume_wake_screen";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
