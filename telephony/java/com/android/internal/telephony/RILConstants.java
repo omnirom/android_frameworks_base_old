363e82de66e61429ef7443b91ade01763d348447
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-04 02:22:43 +0000

    Forward port Samsung STK support (frameworks/base)
    [pawitp: port to Lollipop]
    
    Home Button Long Press fixed too
    
    Change-Id: I08d169bc466df290fca838b09f6b0fbea2ac82a8

--- telephony/java/com/android/internal/telephony/RILConstants.java
+++ telephony/java/com/android/internal/telephony/RILConstants.java
@@ -381,6 +381,7 @@ cat include/telephony/ril.h | \
     int RIL_UNSOL_RADIO_CAPABILITY = 1042;
     int RIL_UNSOL_ON_SS = 1043;
     int RIL_UNSOL_STK_CC_ALPHA_NOTIFY = 1044;
+    int RIL_UNSOL_STK_SEND_SMS_RESULT = 11002; /* Samsung STK */
     int RIL_UNSOL_LCEDATA_RECV = 1045;
     int RIL_UNSOL_STK_SEND_SMS_RESULT = 11002; /* Samsung STK */
 }
