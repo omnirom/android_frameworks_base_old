2e10b5aca74d5069d431f7829e498c3bc47d35c3
Steve Kondik <shade@chemlab.org>
2013-11-04 02:54:13 +0000

    wifi: Set the mode at native layer
     * Set normal/AP mode so that the HAL can specify correct module
       arguments when loading the driver.
    
    Change-Id: Ic460f00f0308840d4875582673020786e46258f7

--- wifi/java/android/net/wifi/WifiNative.java
+++ wifi/java/android/net/wifi/WifiNative.java
@@ -967,4 +967,6 @@ public class WifiNative {
         // Note: optional feature on the driver. It is ok for this to fail.
         doBooleanCommand("DRIVER MIRACAST " + mode);
     }
+
+    public native static boolean setMode(int mode);
 }
