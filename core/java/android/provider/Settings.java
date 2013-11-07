8316db9dd8ff7a91951f469d7d1503725d3673b3
blunden <blunden2@gmail.com>
2013-11-07 21:37:44 +0100

    Bring back the status bar activity indicators for mobile data and wifi (1/2)
    Google were nice enough to create xxhdpi drawables for us before removing them.
    
    Reverts "Remove signal/wifi activity indicator icon overlays."
    
    This reverts commit fcc901c10bffdfb9c6b31e676dec36b47d949979.
    
    Change-Id: I7aa85f7c8442c45d17e42cac7823dfe93e3f24c1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2713,6 +2713,24 @@ public final class Settings {
         public static final String VOLUME_WAKE_SCREEN = "volume_wake_screen";
 
         /**
+         * show clear all recents button
+         *  @hide
+         */
+        public static final String SHOW_CLEAR_RECENTS_BUTTON = "clear_recents_button";
+
+        /**
+         * location of the clear all rectents button
+         * @hide
+         */
+        public static final String CLEAR_RECENTS_BUTTON_LOCATION = "clear_recents_button_location";
+
+        /**
+         * Show when WiFi or data mobile is sending/receiving data
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_ACTIVITY = "status_bar_network_activity";
+
+        /**
          * Whether or not to show circle battery around the lockscreen ring
          * @hide
          */
