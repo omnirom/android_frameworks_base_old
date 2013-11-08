2740534e7c3e100e048b198c1432eaa3e523af90
0xD34D <clark@scheffsblend.com>
2013-11-07 18:46:56 -0800

    [1/3] Base: 4.4 implementation of active display
    Change-Id: I08330a28d0880f82c09ccaec7e36844d9e6570d5

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -514,6 +514,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
             // no window manager? good luck with that
         }
 
+
+        if (mRecreating) {
+        } else {
+            addActiveDisplayView();
+        }
+
         // figure out which pixel-format to use for the status bar.
         mPixelFormat = PixelFormat.OPAQUE;
 
