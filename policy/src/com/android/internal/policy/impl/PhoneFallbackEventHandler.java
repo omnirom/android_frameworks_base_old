3e63e1cf192c84c4574e363a22187f65725ed414
Tectas <adrian.stoeckl@gmail.com>
2013-11-13 12:06:46 +0100

    [1/2] Base: volume rocker change ringtone volume enable/disable capabilities
    Change-Id: I4e2647d60b0ce04992fa2b5718b0782316434a30

--- policy/src/com/android/internal/policy/impl/PhoneFallbackEventHandler.java
+++ policy/src/com/android/internal/policy/impl/PhoneFallbackEventHandler.java
@@ -27,6 +27,7 @@ import android.media.IAudioService;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.util.Slog;
@@ -82,7 +83,9 @@ public class PhoneFallbackEventHandler implements FallbackEventHandler {
             case KeyEvent.KEYCODE_VOLUME_UP:
             case KeyEvent.KEYCODE_VOLUME_DOWN:
             case KeyEvent.KEYCODE_VOLUME_MUTE: {
-                getAudioManager().handleKeyDown(event, AudioManager.USE_DEFAULT_STREAM_TYPE);
+                if (Settings.System.getInt(getContext(), Settings.System.VOLUME_ROCKER_RINGTONE_CHANGE_ENABLED, 1) == 1) {
+                    getAudioManager().handleKeyDown(event, AudioManager.USE_DEFAULT_STREAM_TYPE);
+                }
                 return true;
             }
 
