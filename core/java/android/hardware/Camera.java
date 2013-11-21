CyanogenMod/android_frameworks_base

Camera: add additional ISO modes (2/2)
Change-Id: I3460c3de4cbef467430673146a9ddb2fdc8f5032

core/java/android/hardware/Camera.java
@@ -1792,6 +1792,12 @@ public class Camera {
         /** @hide */
         public static final String ISO_HJR = "ISO_HJR";
         /** @hide */
+        public static final String ISO_SPORTS = "ISOSPORTS";
+        /** @hide */
+        public static final String ISO_NIGHT = "ISONIGHT";
+        /** @hide */
+        public static final String ISO_MOVIE = "ISOMOVIE";
+        /** @hide */
         public static final String ISO_100 = "ISO100";
         /** @hide */
         public static final String ISO_200 = "ISO200";

omnirom/android_frameworks_base_old

core/java/android/hardware/Camera.java
@@ -1952,6 +1952,12 @@ public class Camera {
         /** @hide */
         public static final String ISO_HJR = "ISO_HJR";
         /** @hide */
+        public static final String ISO_SPORTS = "ISO_SPORTS";
+        /** @hide */
+        public static final String ISO_NIGHT = "ISO_NIGHT";
+        /** @hide */
+        public static final String ISO_MOVIE = "ISO_MOVIE";
+        /** @hide */
         public static final String ISO_100 = "ISO100";
         /** @hide */
         public static final String ISO_200 = "ISO200";
@@ -1961,6 +1967,10 @@ public class Camera {
         public static final String ISO_800 = "ISO800";
         /** @hide */
         public static final String ISO_1600 = "ISO1600";
+        /** @hide */
+        public static final String ISO_3200 = "ISO3200";
+        /** @hide */
+        public static final String ISO_6400 = "ISO6400";
 
         /**
          * Scene mode is off.