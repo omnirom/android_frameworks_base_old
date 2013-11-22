7d86dd42ee1132a0cc5b3c2c3982d728592d49d0
Anu6is <curtis.mayers@gmail.com>
2013-11-22 05:55:06 -0600

    [1/2] Frameworks: Optional statusbar battery icons     * Circle battery and circle battery percent (from CM <Sven Dawitz>)     * Battery bar percent from Google     * Quicksetting battery tile reflects status icon (from SlimROM <kufikugel>)
    Part 2: packages_apps_Settings
    
    Patch set 2: clean white spaces
    Patch set 3: fix conflicts
    
    Signed-off-by: Anu6is <curtis.mayers@gmail.com>
    
    Change-Id: Ic4056d08874672263444152813a4a8e805a4abf3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2980,6 +2980,13 @@ public final class Settings {
         public static final String NON_INTRUSIVE_INCALL = "non_intrusive_incall";
 
         /**
+
+         * Setting to show the battery percentage text
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
