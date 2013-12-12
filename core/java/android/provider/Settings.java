5a232d501606ba2115a086c7f1bf94c793ac562b
Dalingrin <dalingrin@gmail.com>
2013-12-12 03:19:48 -0600

    SystemUI: Add double tap to sleep gesture
    Change-Id: I4c54718e8ec8774677d13917b3a7ffa3b4b18cb5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3155,6 +3155,12 @@ public final class Settings {
         public static final String STATUS_BAR_QS_QUICK_PULLDOWN = "status_bar_qs_quick_pulldown";
 
         /**
+         *  Enable statusbar double tap gesture on to put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
