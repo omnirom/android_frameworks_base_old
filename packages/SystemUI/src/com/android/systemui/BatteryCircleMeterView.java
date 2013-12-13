72715a30d9b3202471658cee0bfe0167f38aa285
maxwen <max.weninger@gmail.com>
2013-12-13 01:39:25 +0100

    base: layout circle battery percent really in the center
    There is no need to use strokeWidth in this layout calculation
    
    Change-Id: I68471c5f63b49e815f305bc3d5bd86b67afeb647

--- packages/SystemUI/src/com/android/systemui/BatteryCircleMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryCircleMeterView.java
@@ -390,11 +390,11 @@ public class BatteryCircleMeterView extends ImageView {
 
         // calculate Y position for text
         Rect bounds = new Rect();
-        mPaintFont.getTextBounds("99", 0, "99".length(), bounds);
+        mPaintFont.getTextBounds("MM", 0, "MM".length(), bounds);
         mTextLeftX = mCircleSize / 2.0f + getPaddingLeft();
         mTextRightX = mTextLeftX + off;
-        // the +1 at end of formular balances out rounding issues. works out on all resolutions
-        mTextY = mCircleSize / 2.0f + (bounds.bottom - bounds.top) / 2.0f - strokeWidth / 2.0f + 1;
+        
+        mTextY = mCircleSize / 2.0f + (bounds.bottom - bounds.top) / 2.0f;
 
         // force new measurement for wrap-content xml tag
         onMeasure(0, 0);
@@ -419,7 +419,6 @@ public class BatteryCircleMeterView extends ImageView {
         if (measure == null) {
             return;
         }
-        final int x = measure.getWidth() / 2;
 
         mCircleSize = measure.getHeight();
     }
