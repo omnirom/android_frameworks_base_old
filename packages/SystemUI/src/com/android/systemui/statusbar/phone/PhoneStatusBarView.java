b55cd0e466baf46fe910bee10c0433f0225556dc
Jesús David Gulfo Agudelo <jesdga95@gmail.com>
2013-12-26 22:35:42 +0700

    Base: QuickToggles (AOSPA) modification
    this commit base from
    https://github.com/AOSPA/android_frameworks_base/commit/1405aca50a1854a4b547fa699efe514a852e5c3c
    with major changes to OmniRom
    
    added UsbTether Tile
    added Network Type on RssiTile
    added dynamic column changes
    add back FlipTile
    added 2 fingers allowed reorganize (removed in patch set 5)
    added long press EditButton to reset
    fix AlarmTile clicked after editmode enabled
    fix immersivetile on reboot
    FlipTile works now
    
    video >> https://www.dropbox.com/s/nm2ls25fcabyj99/SCR_20131226_021213.mp4
    
    Change-Id: If8afe643eac9ed48edd98b6a7d373b257e86d3ef

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -46,6 +46,7 @@ public class PhoneStatusBarView extends PanelBar {
     PanelView mNotificationPanel, mSettingsPanel;
     private boolean mShouldFade;
     private final PhoneStatusBarTransitions mBarTransitions;
+    private QuickSettingsContainerView mQSContainer;
 
     public PhoneStatusBarView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -166,6 +167,13 @@ public class PhoneStatusBarView extends PanelBar {
     @Override
     public void onAllPanelsCollapsed() {
         super.onAllPanelsCollapsed();
+
+        mQSContainer = (QuickSettingsContainerView)
+            mBar.mStatusBarWindow.findViewById(R.id.quick_settings_container);
+        if(mQSContainer != null && mQSContainer.isEditModeEnabled()) {
+            mQSContainer.setEditModeEnabled(false);
+        }
+
         // give animations time to settle
         mBar.makeExpandedInvisibleSoon();
         mFadingPanel = null;
@@ -214,7 +222,8 @@ public class PhoneStatusBarView extends PanelBar {
             Log.v(TAG, "panelExpansionChanged: f=" + frac);
         }
 
-        if (panel == mFadingPanel && mScrimColor != 0 && ActivityManager.isHighEndGfx()) {
+        if ((panel == mFadingPanel || mFadingPanel == null)
+                && mScrimColor != 0 && ActivityManager.isHighEndGfx()) {
             if (mShouldFade) {
                 frac = mPanelExpandedFractionSum; // don't judge me
                 // let's start this 20% of the way down the screen
