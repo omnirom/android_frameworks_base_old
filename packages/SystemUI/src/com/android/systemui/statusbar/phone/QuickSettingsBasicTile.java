9c63cccd5c00ff18df4e29663e9c80e777e5c1df
Jesús David Gulfo Agudelo <jesdga95@gmail.com>
2013-12-26 22:35:42 +0700

    Base: QuickToggles (AOSPA) modification
    this commit base from
    https://github.com/AOSPA/android_frameworks_base/commit/1405aca50a1854a4b547fa699efe514a852e5c3c
    with major changes to OmniRom
    
    added UsbTether Tile
    added Network Type on RssiTile
    added dynamic column changes
    add back FlipTile
    added 2 fingers allowed reorganize (removed in patch set 5)
    added long press EditButton to reset
    fix AlarmTile clicked after editmode enabled
    fix immersivetile on reboot
    fix changing textsize in dynamic column changes
    fix header
    fix RotationTile not showing
    added VolumeTile flip into RingerTile
    added SyncTile
    added SleepTile
    add permission for SYNC
    FlipTile works now
    
    video >> https://www.dropbox.com/s/nm2ls25fcabyj99/SCR_20131226_021213.mp4
    
    NOTE:
    only SleepTile now using basicTile, will change to FlipTile in the future
    
    TODO:
    SleepTile Flip into SleepTimeTile
    EditButton Icon need to change
    improve animations
    
    Change-Id: If8afe643eac9ed48edd98b6a7d373b257e86d3ef

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsBasicTile.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsBasicTile.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013 The Android Open Source Project
+ * This code has been modified. Portions copyright (C) 2013, OmniRom Project.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +20,7 @@ package com.android.systemui.statusbar.phone;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -81,6 +83,10 @@ class QuickSettingsBasicTile extends QuickSettingsTileView {
 
     public void setText(CharSequence text) {
         mTextView.setText(text);
+    }
+
+    void setTextSizes(int size) {
+        mTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX, size);
     }
 
     public void setTextResource(int resId) {
