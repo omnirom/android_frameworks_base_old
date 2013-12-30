beb5bcf0e3d671fddbf3436f75c5c0bc41068dca
Lars Greiss <kufikugel@googlemail.com>
2013-12-30 20:02:52 +0530

    [1/2]Base: Add keyboard features
    this commit adds:
    
    - ime switcher notification - thanks cm
    - slims fullscreen disable on landscape
    - slims force autorotation on IME
    - volume button control for navigate in the message - thanks cm
    - slims force to show always enter key
    
    some fixes to the 4.2/4.3 implementations + added full multiuser
    support for all these features.
    
    Change-Id: Ie2672e69816afa2571bf6611caa95754ea29cd93

--- services/java/com/android/server/InputMethodManagerService.java
+++ services/java/com/android/server/InputMethodManagerService.java
@@ -77,6 +77,7 @@ import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
 import android.text.TextUtils;
 import android.text.style.SuggestionSpan;
 import android.util.AtomicFile;
@@ -390,8 +391,6 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
     private final IPackageManager mIPackageManager;
 
     class SettingsObserver extends ContentObserver {
-        String mLastEnabled = "";
-
         SettingsObserver(Handler handler) {
             super(handler);
             ContentResolver resolver = mContext.getContentResolver();
@@ -401,18 +400,13 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
                     Settings.Secure.ENABLED_INPUT_METHODS), false, this);
             resolver.registerContentObserver(Settings.Secure.getUriFor(
                     Settings.Secure.SELECTED_INPUT_METHOD_SUBTYPE), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_IME_SWITCHER),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override public void onChange(boolean selfChange) {
-            synchronized (mMethodMap) {
-                boolean enabledChanged = false;
-                String newEnabled = mSettings.getEnabledInputMethodsStr();
-                if (!mLastEnabled.equals(newEnabled)) {
-                    mLastEnabled = newEnabled;
-                    enabledChanged = true;
-                }
-                updateFromSettingsLocked(enabledChanged);
-            }
+            updateFromSettingsLocked(true);
         }
     }
 
@@ -1652,6 +1646,15 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
             // There is no longer an input method set, so stop any current one.
             mCurMethodId = null;
             unbindCurrentMethodLocked(true, false);
+        }
+        // code to disable the IME switcher with config_show_IMESwitcher set = false
+        try {
+            mShowOngoingImeSwitcherForPhones =
+                Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_IME_SWITCHER, UserHandle.USER_CURRENT) == 1;
+        } catch (SettingNotFoundException e) {
+            mShowOngoingImeSwitcherForPhones = mRes.getBoolean(
+                com.android.internal.R.bool.config_show_IMESwitcher);
         }
     }
 
