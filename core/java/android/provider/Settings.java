4fb402bcc352df1d7bbdc1d95eeba9f565b86a79
Nicolas Lorin <androw95220@gmail.com>
2014-01-02 18:27:58 +0100

    [1/2] framework/base: QH disable battery warning
    Change-Id: I32f4125935d66e24cc118bb301769440969c199b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3203,6 +3203,12 @@ public final class Settings {
         public static final String QUIET_HOURS_RINGER = "quiet_hours_ringer";
 
         /**
+         * Whether to disable battery warning during quiet hours.
+         * @hide
+         */
+        public static final String QUIET_HOURS_BATTERY = "quiet_hours_battery";
+
+        /**
          * Whether to have statusbar right side pulldown.  The value is boolean (1 or 0).
          * @hide
          */
