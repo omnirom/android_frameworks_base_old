f154f477b635d77f08bfcd0931206ccb10dc6ffe
Andrew Dodd <atd7@cornell.edu>
2014-01-04 18:47:25 -0500

    inputmethod: Hide added public method so checkapi does not fail
    Change-Id: I9ec95eb0ec13bba663c7bfce525939964ecc4ad9

--- core/java/android/view/inputmethod/EditorInfo.java
+++ core/java/android/view/inputmethod/EditorInfo.java
@@ -345,6 +345,11 @@ public class EditorInfo implements InputType, Parcelable {
         }
     }
 
+    /**
+     * formal text input
+     * @hide
+     */
+
     public final void formalTextInput(boolean forceLong) {
         if (forceLong) {
             switch (inputType&(TYPE_MASK_CLASS|TYPE_MASK_VARIATION)) {
