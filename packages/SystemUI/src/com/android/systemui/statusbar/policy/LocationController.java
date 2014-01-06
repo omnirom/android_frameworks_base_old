29668e1e603962ac3943ca09b5a7b5a93638bb97
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-01-07 01:42:07 +0700

    Base: add Location Flip Tile
    Front
    stay with default
    
    Back
    you can choose
    between OFF, DEVICE ONLY, BATTERY, and HIGH ACCURACY
    
    Change-Id: If070a23fa036fec2e1ac28fd342113c8e6564efe

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/LocationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/LocationController.java
@@ -135,6 +135,39 @@ public class LocationController extends BroadcastReceiver {
                 .putIntForUser(cr, Settings.Secure.LOCATION_MODE, mode, currentUserId);
     }
 
+    public boolean setBackLocationEnabled(int location) {
+        int currentUserId = ActivityManager.getCurrentUser();
+        if (isUserLocationRestricted(currentUserId)) {
+            return false;
+        }
+        final ContentResolver cr = mContext.getContentResolver();
+        switch (location) {
+            case Settings.Secure.LOCATION_MODE_OFF:
+                return Settings.Secure
+                       .putIntForUser(cr, Settings.Secure.LOCATION_MODE,
+                       Settings.Secure.LOCATION_MODE_SENSORS_ONLY, currentUserId);
+            case Settings.Secure.LOCATION_MODE_SENSORS_ONLY:
+                return Settings.Secure
+                       .putIntForUser(cr, Settings.Secure.LOCATION_MODE,
+                       Settings.Secure.LOCATION_MODE_BATTERY_SAVING, currentUserId);
+            case Settings.Secure.LOCATION_MODE_BATTERY_SAVING:
+                return Settings.Secure
+                       .putIntForUser(cr, Settings.Secure.LOCATION_MODE,
+                       Settings.Secure.LOCATION_MODE_HIGH_ACCURACY, currentUserId);
+            case Settings.Secure.LOCATION_MODE_HIGH_ACCURACY:
+                return Settings.Secure
+                       .putIntForUser(cr, Settings.Secure.LOCATION_MODE,
+                       Settings.Secure.LOCATION_MODE_OFF, currentUserId);
+        }
+        return false;
+    }
+
+    public int locationMode() {
+        ContentResolver resolver = mContext.getContentResolver();
+        return Settings.Secure.getIntForUser(resolver, Settings.Secure.LOCATION_MODE,
+                Settings.Secure.LOCATION_MODE_OFF, ActivityManager.getCurrentUser());
+    }
+
     /**
      * Returns true if location isn't disabled in settings.
      */
