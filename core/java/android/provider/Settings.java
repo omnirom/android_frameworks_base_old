2a7f6e1f19b06cdfe647e87eee8eb11860b09a2e
maxwen <max.weninger@gmail.com>
2014-01-15 21:30:10 +0100

    [1/2] base: task utils and navigation bar
    http://jira.omnirom.org/browse/OMNI-398
    http://jira.omnirom.org/browse/OMNI-345
    
    -added switch to last app as navigation ring target
    -added optional kill on long press on back
    
    PS2
    much easier :)
    
    PS3
    whitespaces
    
    Change-Id: I31f11fcd0efb90f007017fa16567e6711002ecc8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3266,6 +3266,12 @@ public final class Settings {
         public static final String FORMAL_TEXT_INPUT = "formal_text_input";
 
         /**
+         * Enable long press on back kill for soft buttons.
+         * @hide
+         */
+        public static final String SOFT_BACK_KILL_APP_ENABLE = "soft_back_kill_app_enable";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
