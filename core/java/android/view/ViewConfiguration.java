cdca545a309fccda108359f965ad7483c5998f1d
crondog <patches@crondog.com>
2014-01-21 12:39:30 +1100

    [2/2] base: overflow menu tweaks
    Combines two features:
    
    1) for devices with a hw keys support force show the
    3dot overflow menu in applications. This allows remapping
    of the menu key to other actions and/or better support devices that
    have hw keys but no menu key like m7.
    
    2) for devices with soft-keys force hiding the 3dot overflow
    menu by emulating existence of a hw menu button and only
    showing a navbar soft menu key
    
    Change-Id: Ic7a7e4bde440cee0e274e6ea871c952998a56dd7

--- core/java/android/view/ViewConfiguration.java
+++ core/java/android/view/ViewConfiguration.java
@@ -212,6 +212,7 @@ public class ViewConfiguration {
      */
     private static final int OVERFLING_DISTANCE = 6;
 
+    private Context mContext;
     private final int mEdgeSlop;
     private final int mFadingEdgeLength;
     private final int mMinimumFlingVelocity;
@@ -266,6 +267,7 @@ public class ViewConfiguration {
      * @see android.util.DisplayMetrics
      */
     private ViewConfiguration(Context context) {
+        mContext = context;
         final Resources res = context.getResources();
         final DisplayMetrics metrics = res.getDisplayMetrics();
         final Configuration config = res.getConfiguration();
@@ -696,6 +698,21 @@ public class ViewConfiguration {
      * @return true if a permanent menu key is present, false otherwise.
      */
     public boolean hasPermanentMenuKey() {
+        // normally only one of those settings will be 1
+        // except for the use-case to force show soft keys on devices
+        // that have hardware buttons
+        boolean forceShowMenu = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.FORCE_SHOW_OVERFLOW_MENU, 0) == 1;
+
+        boolean emulateHwMenuKey = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.EMULATE_HW_MENU_KEY, 0) == 1;
+
+        if (forceShowMenu){
+            return false;
+        }
+        if (emulateHwMenuKey){
+            return true;
+        }
         return sHasPermanentMenuKey;
     }
 
