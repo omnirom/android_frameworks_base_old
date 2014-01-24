6eb8d7d5fb73cd140cb85f7bc25dc793d8e6f120
maxwen <max.weninger@gmail.com>
2014-01-24 12:43:05 +0100

    base: tweaks to support force usage of soft keys for devices with capacitive buttons
    Two minor tweaks to support that use case.
    For devices with capactivive buttons disabling those buttons
    need two steps
    
    1) disable buttons backlight - dark mode
    2) settings - buttons - custom actions and set all buttons
    to "no-action". This fix is needed to get a working home button
    and working "long press back to kill" for that setup
    
    Change-Id: I563e877580c09df45383e4485c6cbb965aca0b31

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -1309,6 +1309,16 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mLongPressOnBackBehavior = Settings.System.getIntForUser(resolver,
                         Settings.System.KEY_BACK_LONG_PRESS_ACTION, KEY_ACTION_NOTHING,
                         UserHandle.USER_CURRENT);
+                // ASSUMPTION! if mSoftBackKillApp == true this is a device with
+                // capacitive buttons but user has enabled soft keys somehow else this could
+                // not have been set to true.
+                // in that case use the mSoftBackKillApp and overrule the settings
+                // of long press back key since we can assume that user wants to
+                // disable the capactive buttons in that case which would
+                // mean set all buttons to "no action"
+                if(mSoftBackKillApp){
+                    mLongPressOnBackBehavior = KEY_ACTION_KILL_APP;
+                }
             }
             if (hasAssist) {
                 mPressOnAssistBehavior = Settings.System.getIntForUser(resolver,
@@ -2391,7 +2401,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     return -1;
                 }
 
-                if (mPressOnHomeBehavior != KEY_ACTION_HOME){
+                // dont change handling of virtual home button events
+                if (mPressOnHomeBehavior != KEY_ACTION_HOME && !virtualKey){
                     performKeyAction(mPressOnHomeBehavior);
                 } else {
                     launchHomeFromHotKey();
