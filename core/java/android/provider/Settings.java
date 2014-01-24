2f5a897b1d889346f5cf24b00ee1c06d58706d21
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-01-25 03:01:21 +0700

    [1/2] Base: ActiveDisplay (rework interface)
    really fix AD buggy stuff
    tested with Music play
    tested with No lockscreen
    tested with faster screen turn off
    check all sensor going off if AD not Showing or Disabled
    fix notification posted/removed
    make AD using System Window so it will covered all screen
    fix Navbar translucent if Lockscreen Blur merged :P
    now using Rounded Image
    also introduce new RoundedImageView widget
     courtesy of @vinc3m1 (thanks)
    fix build
    fix sometimes screen turn off after unlock because screen timeout (hopefully)
    now using new parser to get Notification data (thanks @Achep for the parser)
    rework lightsensor manager
    add NotificationUtils
    fix brightness sometimes not changing
    
    add support applications privacy mode (will not show content message if enable on each app)
    see http://jira.omnirom.org/browse/OMNI-600
    
    allow bypass AD if no notification and lockscreen is secure
    see http://jira.omnirom.org/browse/OMNI-594
    
    fix proximity always enable after adding new feature allow bypass AD
    fix parsing notification data from app with targetSDK < 17
    
    fix excluded app value (no body notice this broken since AD merged into OmniRom xD )
    
    add option to allow bypass AD
    
    Conflicts:
    	packages/SystemUI/res/values/custom_dimens.xml
    	packages/SystemUI/res/values/custom_styles.xml
    
    Change-Id: I1fc3047496cce71cefc1718b8ef3f4c4fc1605eb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2762,6 +2762,12 @@ public final class Settings {
         public static final String ACTIVE_DISPLAY_THRESHOLD = "active_display_threshold";
 
         /**
+         * use Active display content view instead default one.
+         * @hide
+         */
+        public static final String ACTIVE_DISPLAY_CONTENT = "active_display_content";
+
+        /**
          * Timeout of the display when there is no user interaction
          * @hide
          */
@@ -2773,6 +2779,20 @@ public final class Settings {
          * @hide
          */
         public static final String ACTIVE_DISPLAY_EXCLUDED_APPS = "active_display_excluded_apps";
+
+        /**
+         * A list of packages to exclude from being message displayed in active display.
+         * This should be a string of packages separated by |
+         * @hide
+         */
+        public static final String ACTIVE_DISPLAY_PRIVACY_APPS = "active_display_privacy_apps";
+
+        /**
+         * allow bypass active display when lockscreen isSecure
+         * and there is no notifications
+         * @hide
+         */
+        public static final String ACTIVE_DISPLAY_BYPASS = "active_display_bypass";
 
         /**
          * @deprecated Use {@link android.provider.Settings.Global#LOW_BATTERY_SOUND}
