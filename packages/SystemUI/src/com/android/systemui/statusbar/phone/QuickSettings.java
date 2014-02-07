3e7ecf96ce296ec751c4c98d660bcc3f9544ca09
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-02-07 14:34:49 +0700

    [1/3] Base: Battery Saver mode
    Concept:
    http://jira.omnirom.org/browse/CONCEPT-24
    
    introduce battery saver mode
    this based from what user want or what user do to saving the battery life
    this include:
    1. Automatically switch mobile network into what user choice and also disable mobile data IF wifi connected to an AP
    2. Automatically switch mobile network and disable mobile data IF battery low (battery level can configure)
    3. Reduce screen brightness based low battery (this feature will disabled if Auto brightness mode enabled)
    4. Not changing network IF on Call or Usb tether enabled or WifiAP enabled
    5. Not changing network IF there is Download/upload progress
    6. Support For Both GSM and CDMA devices (need someone to test for CDMA devices)
    7. every condition has an option :D
    8. Restore all network configuration if battery saver mode disabled
    9. Add Time Range to start battery saver mode
    10. allow device without mobile data support, to using this feature
    11. fix inconsistent battery low level
    12. rewrite to better performance
    13. modified the controller to support battery saver mode
    14. add bluetooth, gps, wifi, and mobile data (if support) to be disabled while power saving activated
    15. fix build QuickSettings because of LocationController change
    16. rewrite to better performance
    17. fix brightness value
    18. rewrite (again) to allow add more saving mode
    19. do delay changing only for POWER SAVING not NORMAL mode
    20. add Cpu power saving mode
    21. fix Cpu freq value
    22. fix call saving state for non user configuration mode
    23. fix NPE
    24. add kill all task
    25. add disable sync
    26. add disable vibrate
    27. add disable led
    28. add notification :)
    29. finally fix sync interval :D
    
    preview:
    http://jira.omnirom.org/secure/attachment/10394/Screenshot_2014-03-07-19-54-57.png
    http://jira.omnirom.org/secure/attachment/10395/Screenshot_2014-03-07-19-55-56.png
    http://jira.omnirom.org/secure/attachment/10396/Screenshot_2014-03-07-19-56-06.png
    http://jira.omnirom.org/secure/attachment/10397/Screenshot_2014-03-07-19-56-14.png
    http://jira.omnirom.org/secure/attachment/10398/Screenshot_2014-03-07-19-56-27.png
    http://jira.omnirom.org/secure/attachment/10399/Screenshot_2014-03-07-19-56-34.png
    
    PS47: fix conflicts.
    PS48: forgot delete obsolete call to refreshImmersiveTile()
    
    Change-Id: I9d86ef1425228e11cadd85d37c5b6e8b20e50fb0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -116,7 +116,8 @@ class QuickSettings {
         SLEEP,
         SYNC,
         USBMODE,
-        TORCH
+        TORCH,
+        BATTERYSAVER
     }
 
     public static final String NO_TILES = "NO_TILES";
@@ -127,7 +128,8 @@ class QuickSettings {
         + DELIMITER + Tile.BATTERY + DELIMITER + Tile.ROTATION+ DELIMITER + Tile.IMMERSIVE
         + DELIMITER + Tile.LOCATION + DELIMITER + Tile.AIRPLANE + DELIMITER + Tile.QUITEHOUR
         + DELIMITER + Tile.USBMODE + DELIMITER + Tile.SLEEP + DELIMITER + Tile.SYNC
-        + DELIMITER + Tile.NFC;
+        + DELIMITER + Tile.NFC
+        + DELIMITER + Tile.BATTERYSAVER;
 
     private Context mContext;
     private PanelBar mBar;
@@ -869,6 +871,37 @@ class QuickSettings {
                   });
                   parent.addView(SyncTile);
                   if (addMissing) SyncTile.setVisibility(View.GONE);
+               } else if (Tile.BATTERYSAVER.toString().equals(tile.toString())) { // battery saver tile
+                  // battery saver tile
+                  final QuickSettingsBasicTile batterySaverTile = new QuickSettingsBasicTile(mContext);
+
+                  batterySaverTile.setTileId(Tile.BATTERYSAVER);
+                  batterySaverTile.setImageResource(R.drawable.ic_qs_battery_saver_off);
+                  batterySaverTile.setTextResource(R.string.quick_settings_battery_saver_off_label);
+                  batterySaverTile.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            collapsePanels();
+                            boolean checkModeOn = Settings.Global.getInt(mContext.getContentResolver(),
+                                                Settings.Global.BATTERY_SAVER_OPTION, 0) == 1;
+                            Settings.Global.putInt(mContext.getContentResolver(),
+                                 Settings.Global.BATTERY_SAVER_OPTION, checkModeOn ? 0 : 1);
+                            Intent scheduleSaver = new Intent();
+                            scheduleSaver.setAction(Intent.ACTION_BATTERY_SERVICES);
+                            mContext.sendBroadcast(scheduleSaver);
+                        }
+                  });
+                  batterySaverTile.setOnLongClickListener(new View.OnLongClickListener() {
+                      @Override
+                      public boolean onLongClick(View v) {
+                          startSettingsActivity(Intent.ACTION_POWER_USAGE_SUMMARY);
+                          return true;
+                      }
+                  });
+                  mModel.addBatterySaverTile(batterySaverTile,
+                         new QuickSettingsModel.BasicRefreshCallback(batterySaverTile));
+                  parent.addView(batterySaverTile);
+                  if (addMissing) batterySaverTile.setVisibility(View.GONE);
                } else if (Tile.QUITEHOUR.toString().equals(tile.toString())) { // Quite hours tile
                   // Quite hours mode
                   final QuickSettingsBasicTile quiteHourTile
