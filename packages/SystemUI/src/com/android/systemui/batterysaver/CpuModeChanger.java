0a43132298a10ef11ffec8401def4bf35624fc6b
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-02-07 14:34:49 +0700

    [1/3] Base: Battery Saver mode
    introduce battery saver mode
    this based from what user want or what user do to saving the battery life
    this include:
    1. Automatically switch mobile network into what user choice and also disable mobile data IF wifi connected to an AP
    2. Automatically switch mobile network and disable mobile data IF battery low (battery level can configure)
    3. Reduce screen brightness based low battery (this feature will disabled if Auto brightness mode enabled)
    4. Not changing network IF on Call or Usb tether enabled or WifiAP enabled
    5. Not changing network IF there is Download/upload progress
    6. Support For Both GSM and CDMA devices (need someone to test for CDMA devices)
    7. every condition has an option :D
    8. Restore all network configuration if Smart Radio disabled
    9. Add Time Range to start Smart Radio
    10. allow device without mobile data support, to using this feature
    11. fix inconsistent battery low level
    12. rewrite to better performance
    13. modified the controller to support battery saver mode
    14. add bluetooth, gps, wifi, and mobile data (if support) to be disabled while power saving activated
    15. fix build QuickSettings because of LocationController change
    16. rewrite to better performance
    17. fix brightness value
    18. rewrite (again) to allow add more saving mode
    19. do delay changing only for POWER SAVING not NORMAL mode
    20. add Cpu power saving mode
    21. fix Cpu freq value
    
    Change-Id: I9d86ef1425228e11cadd85d37c5b6e8b20e50fb0

---
+++ packages/SystemUI/src/com/android/systemui/batterysaver/CpuModeChanger.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (C) 2014 The OmniRom Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.systemui.batterysaver;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.widget.Toast;
+import android.util.Log;
+
+import com.android.systemui.R;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class CpuModeChanger extends ModeChanger {
+
+    private boolean mIsTegra3 = false;
+    private boolean mIsDynFreq = false;
+    private String mMaxFreqSetting;
+    private String mMaxFreqSaverSetting;
+
+    public CpuModeChanger(Context context) {
+        super(context);
+        updateDefaultCpuValue();
+    }
+
+    private void updateDefaultCpuValue() {
+        mIsTegra3 = Helpers.fileExists(Helpers.TEGRA_MAX_FREQ_PATH);
+        mIsDynFreq = Helpers.fileExists(Helpers.DYN_MAX_FREQ_PATH) && Helpers.fileExists(Helpers.DYN_MIN_FREQ_PATH);
+
+        if (Helpers.fileExists(Helpers.DYN_MAX_FREQ_PATH)) {
+            mMaxFreqSetting = Helpers.readOneLine(Helpers.DYN_MAX_FREQ_PATH);
+        } else {
+            mMaxFreqSetting = Helpers.readOneLine(Helpers.MAX_FREQ_PATH);
+        }
+
+        if (mIsTegra3) {
+            String curTegraMaxSpeed = Helpers.readOneLine(Helpers.TEGRA_MAX_FREQ_PATH);
+            int curTegraMax;
+            try {
+                curTegraMax = Integer.parseInt(curTegraMaxSpeed);
+                if (curTegraMax > 0) {
+                    mMaxFreqSetting = Integer.toString(curTegraMax);
+                }
+            } catch (NumberFormatException ignored) {
+                // Nothing to do
+            }
+        }
+        if (mMaxFreqSetting == null) {
+            mMaxFreqSetting = Settings.Global.getString(mContext.getContentResolver(),
+                     Settings.Global.BATTERY_SAVER_CPU_FREQ_DEFAULT);
+        }
+        if (mMaxFreqSaverSetting == null) {
+            mMaxFreqSaverSetting = mMaxFreqSetting;
+        }
+        Log.i(BatterySaverService.TAG, " default maximum cpu freq = " + mMaxFreqSetting);
+    }
+
+    private void restoreCpuState(boolean restore) {
+        if (mMaxFreqSetting == null || mMaxFreqSaverSetting == null) {
+            updateDefaultCpuValue();
+            return;
+        }
+        String setCpuFreq = restore ? mMaxFreqSetting : mMaxFreqSaverSetting;
+        for (int i = 0; i < Helpers.getNumOfCpus(); i++) {
+             Helpers.writeOneLine(Helpers.MAX_FREQ_PATH.replace("cpu0", "cpu" + i), mMaxFreqSetting);
+        }
+        if (mIsTegra3) {
+            Helpers.writeOneLine(Helpers.TEGRA_MAX_FREQ_PATH, mMaxFreqSetting);
+        }
+        if (mIsDynFreq) {
+            Helpers.writeOneLine(Helpers.DYN_MAX_FREQ_PATH, mMaxFreqSetting);
+        }
+        Log.i(BatterySaverService.TAG, " setup maximum cpu freq = " + Helpers.toMHz(setCpuFreq));
+        if (setCpuFreq != null) {
+            Toast.makeText(mContext,
+                  mContext.getString(R.string.battery_saver_cpu_change) + " "
+                  + Helpers.toMHz(setCpuFreq), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void setCpuValue(String value) {
+        mMaxFreqSaverSetting = value;
+        updateDefaultCpuValue();
+        Log.i(BatterySaverService.TAG, " user setup maximum cpu freq = " + Helpers.toMHz(value));
+    }
+
+    @Override
+    public boolean isStateEnabled() {
+        return isModeEnabled();
+    };
+
+    @Override
+    public boolean isSupported() {
+        return isModeEnabled();
+    };
+
+    @Override
+    public int getMode() {
+        return 0;
+    }
+
+    @Override
+    public void stateNormal() {
+        restoreCpuState(true);
+    }
+
+    @Override
+    public void stateSaving() {
+        restoreCpuState(false);
+    }
+
+    @Override
+    public boolean checkModes() {
+        return isModeEnabled();
+    };
+
+    @Override
+    public void setModes() {
+        super.setModes();
+    }
+
+    @Override
+    public boolean restoreState() {
+        if (isSupported() && isDisabledByService()) {
+            stateNormal();
+            return true;
+        }
+        return false;
+    }
+}
