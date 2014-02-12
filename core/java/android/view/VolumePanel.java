8b24f028239445f24085d2a4b06f543f92af1a0a
UtkarshGupta <utkarsh.eminem@gmail.com>
2014-02-12 16:06:35 +0530

    [1/2] Base: Volume panel timeout
    OmniGears: https://gerrit.omnirom.org/#/c/5517/
    
    Change-Id: I15c4c589bde5794d7c961a02022a8e25a4e59500

--- core/java/android/view/VolumePanel.java
+++ core/java/android/view/VolumePanel.java
@@ -85,7 +85,7 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
     private static final int BEEP_DURATION = 150;
     private static final int MAX_VOLUME = 100;
     private static final int FREE_DELAY = 10000;
-    private static final int TIMEOUT_DELAY = 3000;
+    private static int TIMEOUT_DELAY = 3000;
 
     private static final int MSG_VOLUME_CHANGED = 0;
     private static final int MSG_FREE_RESOURCES = 1;
@@ -233,6 +233,8 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
             int overlayStyle = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.MODE_VOLUME_OVERLAY, VOLUME_OVERLAY_EXPANDABLE);
             changeOverlayStyle(overlayStyle);
+            TIMEOUT_DELAY = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.VOLUME_PANEL_TIMEOUT, 3000);
         }
     };
 
@@ -354,6 +356,9 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
                 mSettingsObserver);
         context.getContentResolver().registerContentObserver(
                 Settings.System.getUriFor(Settings.System.MODE_VOLUME_OVERLAY), false,
+                mSettingsObserver);
+        context.getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.VOLUME_PANEL_TIMEOUT), false,
                 mSettingsObserver);
 
         boolean masterVolumeKeySounds = mContext.getResources().getBoolean(
