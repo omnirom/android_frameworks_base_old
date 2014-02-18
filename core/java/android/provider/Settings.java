fb3e48982e74a59dec927a3f45a0f105ceed7a5d
DvTonder <david.vantonder@gmail.com>
2014-02-18 20:02:09 +0530

    [1/6] Base: Blacklist support
    Change-Id: Ic5a59be11f76b4a9eba075d9c3525086b3ee389e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3231,6 +3231,58 @@ public final class Settings {
         public static final String CALL_END_SOUND = "call_end_sound";
 
         /**
+          * Whether the blacklisting feature for phone calls is enabled
+          * @hide
+          */
+         public static final String PHONE_BLACKLIST_ENABLED = "phone_blacklist_enabled";
+
+         /**
+          * Whether a notification should be shown when a call/message is blocked
+          * @hide
+          */
+         public static final String PHONE_BLACKLIST_NOTIFY_ENABLED = "phone_blacklist_notify_enabled";
+
+         /**
+          * Whether the blacklisting feature for phone calls from private numbers is enabled
+          * @hide
+          */
+         public static final String PHONE_BLACKLIST_PRIVATE_NUMBER_MODE = "phone_blacklist_private_number_enabled";
+
+         /**
+          * Whether the blacklisting feature for phone calls from private numbers is enabled
+          * @hide
+          */
+         public static final String PHONE_BLACKLIST_UNKNOWN_NUMBER_MODE = "phone_blacklist_private_number_enabled";
+
+         /**
+          * Constants to be used for {@link PHONE_BLACKLIST_PRIVATE_NUMBER_MODE} and
+          * {@link PHONE_BLACKLIST_UNKNOWN_NUMBER_MODE}.
+          * @hide
+          */
+         public static final int BLACKLIST_DO_NOT_BLOCK = 0;
+
+         /**
+          * @hide
+          */
+         public static final int BLACKLIST_BLOCK = 1;
+
+         /**
+          * @hide
+          */
+         public static final int BLACKLIST_PHONE_SHIFT = 0;
+
+         /**
+          * @hide
+          */
+         public static final int BLACKLIST_MESSAGE_SHIFT = 4;
+
+         /**
+          * Whether the regex blacklisting feature for phone calls is enabled
+          * @hide
+          */
+         public static final String PHONE_BLACKLIST_REGEX_ENABLED = "phone_blacklist_regex_enabled";
+
+        /**
          * Setting to show the battery percentage text
          * @hide
          */
@@ -3485,6 +3537,11 @@ public final class Settings {
             POINTER_SPEED,
             VIBRATE_WHEN_RINGING,
             RINGTONE,
+            PHONE_BLACKLIST_ENABLED,
+            PHONE_BLACKLIST_NOTIFY_ENABLED,
+            PHONE_BLACKLIST_PRIVATE_NUMBER_MODE,
+            PHONE_BLACKLIST_UNKNOWN_NUMBER_MODE,
+            PHONE_BLACKLIST_REGEX_ENABLED,
             NOTIFICATION_SOUND
         };
 
