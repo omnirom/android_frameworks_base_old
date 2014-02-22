fa38510cf684d0f5852b0ee3b83d4e6f96203fa0
David96 <xd_e-mail@gmx.de>
2014-02-22 14:29:56 +0530

    [1/2] Base: Lockscreen Notifications
    Settings: https://gerrit.omnirom.org/#/c/6062/
    
    Thanks to schubi for dynamic width control
    
    Change-Id: Ifba54d3b8e3035e3cacedcb60c2691e325326e05

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3237,6 +3237,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         if (false) Log.v(TAG, "updateResources");
     }
 
+    @Override
+    public void setButtonDrawable(int buttonId, int iconId) {
+        mNavigationBarView.setButtonDrawable(buttonId, iconId);
+    }
     //
     // tracing
     //
