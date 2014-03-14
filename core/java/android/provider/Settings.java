2087d6c4f4385264f746d16074be406149a12cff
Juan Ezquerro LLanes <arrase@gamil.com>
2014-03-14 01:36:51 +0100

    [WIP] [2/2] Base: Active Display add shake event
    Change-Id: Iabd42517a4e9df82e9cf653359104303667e43be

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2813,6 +2813,12 @@ public final class Settings {
         public static final String ACTIVE_DISPLAY_DOUBLE_TAP = "active_display_double_tap";
 
         /**
+         * shake device to redisplay notifications on active display.
+         * @hide
+         */
+        public static final String ACTIVE_DISPLAY_SHAKE_EVENT = "active_display_shake_event";
+
+        /**
          * Screen-On Notification Light
          * 0 - disable, default
          * 1 - enable
