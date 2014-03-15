854ea636b6bbcd7f1e2166fd3cf78c4e584d6bf1
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-03-15 12:03:52 +0700

    [1/4] Base: Smart Phone Call
    Put on your ear, and this will automatically call current number in message or dialer
    
    Change-Id: I6ba4aa07e94121b3618200e7f43fefc8d116aca1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3157,7 +3157,8 @@ public final class Settings {
          */
         public static final String VIRTUAL_KEYS_HAPTIC_FEEDBACK = "virtual_keys_haptic_feedback";
 
-        /** Weather to allow headsethook to launch voice commands
+        /** 
+         * Weather to allow headsethook to launch voice commands
          * @hide
          */
         public static final String HEADSETHOOK_LAUNCH_VOICE = "headsethook_launch_voice";
@@ -3268,6 +3269,11 @@ public final class Settings {
          * @hide
          */
         public static final String NON_INTRUSIVE_INCALL = "non_intrusive_incall";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_PHONE_CALLER = "smart_phone_caller";
 
         /**
          * Should Dialer suggest nearby phone numbers
