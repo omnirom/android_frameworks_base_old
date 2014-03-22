0df76285820da864dab091955b196300f6b579cd
ByteHamster <info@bytehamster.com>
2014-03-22 11:33:53 +0100

    Base: NFC toggle
    Jira: http://jira.omnirom.org/browse/OMNI-553
    Icons from Cyanogenmod.
    
    Change-Id: I3230978211bdd25e84a919fe2ba3ceb8f8b79c83

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -32,6 +32,7 @@ import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.UserInfo;
 import android.content.res.Resources;
@@ -45,6 +46,7 @@ import android.media.AudioManager;
 import android.media.MediaRouter;
 import android.net.ConnectivityManager;
 import android.net.wifi.WifiManager;
+import android.nfc.NfcAdapter;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.PowerManager;
@@ -103,6 +105,7 @@ class QuickSettings {
         WIFI,
         RSSI,
         BLUETOOTH,
+        NFC,
         VOLUME,
         BATTERY,
         ROTATION,
@@ -123,7 +126,8 @@ class QuickSettings {
         + DELIMITER + Tile.RSSI + DELIMITER + Tile.BLUETOOTH + DELIMITER + Tile.VOLUME
         + DELIMITER + Tile.BATTERY + DELIMITER + Tile.ROTATION+ DELIMITER + Tile.IMMERSIVE
         + DELIMITER + Tile.LOCATION + DELIMITER + Tile.AIRPLANE + DELIMITER + Tile.QUITEHOUR
-        + DELIMITER + Tile.USBMODE + DELIMITER + Tile.SLEEP + DELIMITER + Tile.SYNC;
+        + DELIMITER + Tile.USBMODE + DELIMITER + Tile.SLEEP + DELIMITER + Tile.SYNC
+        + DELIMITER + Tile.NFC;
 
     private Context mContext;
     private PanelBar mBar;
@@ -1066,6 +1070,38 @@ class QuickSettings {
                       });
                       parent.addView(bluetoothTile);
                       if (addMissing) bluetoothTile.setVisibility(View.GONE);
+                  }
+               } else if (Tile.NFC.toString().equals(tile.toString())) { // NFC tile
+                  // NFC
+                  if(mContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_NFC)) {
+                      final QuickSettingsBasicTile nfcTile = new QuickSettingsBasicTile(mContext);
+
+                      nfcTile.setTileId(Tile.NFC);
+                      nfcTile.setImageResource(R.drawable.ic_qs_nfc_off);
+                      nfcTile.setTextResource(R.string.quick_settings_nfc_off);
+                      nfcTile.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                try {
+                                    if(NfcAdapter.getNfcAdapter(mContext).isEnabled()) {
+                                        NfcAdapter.getNfcAdapter(mContext).disable();
+                                    } else {
+                                        NfcAdapter.getNfcAdapter(mContext).enable();
+                                    }
+                                } catch (Exception e) {}
+                            }
+                      });
+                      nfcTile.setOnLongClickListener(new View.OnLongClickListener() {
+                            @Override
+                            public boolean onLongClick(View v) {
+                                startSettingsActivity(android.provider.Settings.ACTION_WIRELESS_SETTINGS);
+                                return true;
+                            }
+                      });
+                      mModel.addNfcTile(nfcTile,
+                             new QuickSettingsModel.BasicRefreshCallback(nfcTile));
+                      parent.addView(nfcTile);
+                      if (addMissing) nfcTile.setVisibility(View.GONE);
                   }
                } else if (Tile.LOCATION.toString().equals(tile.toString())) { // Location
                  // Location
