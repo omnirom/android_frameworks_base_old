7577241121ecdb05bd141f2781cad69f11a8e267
Marco Magliona <marco.magliona@gmail.com>
2014-03-26 00:49:19 +0100

    RecentsPanel: Make CircleMemoryMeter bigger, adjust accordingly ClearAllButton
    PS5: Fix rect size of CircleMemoryMeter, now it's shown respecting the screen borders
    PS6: Fix position and size of "the three bars" ("Clear all" button)
    PS7: Fix copyright headers and spaces
    PS8: Add some comments to the new parts
    PS9: Changed code style, now lines are shorter
    PS11:
       CircleMemoryMeter size is now decided in cm (only for tablets),
       in this way we can know the size of the circle before it is drawn
    PS12: Removed debug tag I inserted for testing, centered and risized ClearAll button
    PS13:
       Changed size and edited ClearAll images directly in res
       folder(it's easier this way and the device don't have to do this by itself like in previous
       patchsets),now this will make the circle bigger for all devices
    PS14: Final cleaning
    PS15: Found a better way to make changes(in res folder)
    PS16: Restored original format of the edited files
    PS17:
       Added new resources for ClearAllButton in recents
       (now different from the ones for ClearAllButton in notification shade)
       Thanks to Max Weninger for pointing this out
    
    Change-Id: I56f2ae7f23433ef0b4d4eaf893be14a09edf7b19

--- packages/SystemUI/src/com/android/systemui/recent/CircleMemoryMeter.java
+++ packages/SystemUI/src/com/android/systemui/recent/CircleMemoryMeter.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2013 The ChameleonOS Open Source Project
- * Copyright (C) 2013 The OmniROM Project
+ * Copyright (C) 2014 The OmniROM Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
