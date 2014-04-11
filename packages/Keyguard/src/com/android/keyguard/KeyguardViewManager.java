220202a641599e0f73c4a3791d3c2443da416b1b
maxwen <max.weninger@gmail.com>
2014-04-11 15:41:36 +0200

    base: lockscreen rotation fix
    You must not check the value of ACCELEROMETER_ROTATION here
    cause this will be 0 also in the case of using 'rotation locked'
    from quick settings
    
    Change-Id: If610e86d1df6be1a60aa996450344c07f8266e5f

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -209,11 +209,9 @@ public class KeyguardViewManager {
         final boolean configLockRotationValue = res.getBoolean(R.bool.config_enableLockScreenRotation);
         boolean enableLockScreenRotation = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.LOCKSCREEN_ROTATION, configLockRotationValue ? 1 : 0, UserHandle.USER_CURRENT) != 0;
-        boolean enableAccelerometerRotation = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.ACCELEROMETER_ROTATION, 0, UserHandle.USER_CURRENT) != 0;
 
         return SystemProperties.getBoolean("lockscreen.rot_override",false)
-               || (enableLockScreenRotation && enableAccelerometerRotation);
+               || enableLockScreenRotation;
     }
 
     private boolean shouldEnableTranslucentDecor() {
