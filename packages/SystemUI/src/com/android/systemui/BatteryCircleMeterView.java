603c4eefe48d4fb4277c206595ea4f651458385f
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2014-05-06 22:06:54 +0530

    Circle Battery: Fix miss matched font & circle color
    Change-Id: I539d762ce49947a19e6821748e08f3fc2ce71162

--- packages/SystemUI/src/com/android/systemui/BatteryCircleMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryCircleMeterView.java
@@ -195,8 +195,8 @@ public class BatteryCircleMeterView extends ImageView {
     private void drawCircle(Canvas canvas, int level, int animOffset, float textX, RectF drawRect) {
         Paint usePaint = mPaintSystem;
 
-        // turn red at 14% - same level android battery warning appears
-        if (level <= 14) {
+        // turn red when battery is at warning level - same level android battery warning appears
+        if (level <= mWarningLevel) {
             usePaint = mPaintRed;
         }
         usePaint.setAntiAlias(true);
