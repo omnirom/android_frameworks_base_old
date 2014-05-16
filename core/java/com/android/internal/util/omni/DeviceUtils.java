f3489f0293f7c851572ada50656d59aca286513b
Juan Ezquerro LLanes <arrase@gamil.com>
2014-05-16 17:52:33 +0200

    Base: Moves isAvailableApp to DeviceUtils
    Change-Id: I78494839fb2e521666fae82d2168131fa2bbd093

--- core/java/com/android/internal/util/omni/DeviceUtils.java
+++ core/java/com/android/internal/util/omni/DeviceUtils.java
@@ -19,6 +19,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.hardware.SensorManager;
 import android.net.ConnectivityManager;
@@ -131,6 +132,19 @@ public class DeviceUtils {
             pm.getPackageInfo(appUri, PackageManager.GET_ACTIVITIES);
             return true;
         } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean isAvailableApp(String packageName, Context context) {
+        Context mContext = context;
+        final PackageManager pm = mContext.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            int enabled = pm.getApplicationEnabledSetting(packageName);
+            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+        } catch (NameNotFoundException e) {
             return false;
         }
     }
