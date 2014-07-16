ff769ea2e6222106e106895d2e9a40771944db19
David James <james.n.david@gmail.com>
2014-07-16 17:42:05 -0500

    Reset battery stats [1/2]
    Change-Id: I90c1d1ab73c2187b68230a6db1db19dec2a5924b
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	core/res/res/values/strings_custom.xml
    
    Conflicts:
    	core/java/android/content/Context.java
    	core/res/res/values/liquid_strings.xml

--- core/java/android/content/Context.java
+++ core/java/android/content/Context.java
@@ -2477,6 +2477,16 @@ public abstract class Context {
     public static final String THEME_SERVICE = "themes";
 
     /**
+     * Use with {@link #getSystemService} to retrieve a
+     * {@link android.os.BatteryManager} for access battery information.
+     *
+     * @see #getSystemService
+     * @see android.os.BatteryManager
+     * @hide
+     */
+    public static final String BATTERY_SERVICE = "battery";
+
+    /**
      * Determine whether the given permission is allowed for a particular
      * process and user ID running in the system.
      *
