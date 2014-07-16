c65213efb03a059885ca636613169d2a75b085b8
David James <james.n.david@gmail.com>
2014-07-16 11:22:27 -0500

    update parameters in uploadToTexture() for GLES20
    Google optimized the uploadToTexture with GLES3.0+,
    This allows us to upload a sub-rectangle of texture,
    but this has compliance issue with GLES 2.0.
    
    With OpenGL ES 2.0 we need to copy the bitmap in a
    temporary buffer if the stride doesn't match the width,
    and then upload entire temporary stripes.
    
    Change-Id: I8987cd8347913c61b7fa45bd47be1189aa6bdb05
    Signed-off-by: Shenghua Lu <shenghua.lu@intel.com>
    Signed-off-by: Shuo Gao <shuo.gao@intel.com>
    Signed-off-by: Guobin Zhang<guobin.zhang@intel.com>
    hwui: fix possible null pointer de-refrence
    
    add layer check for  null before de-refrencing it.
    
    issue link: http://code.google.com/p/android/issues/detail?id=61622
    
    Change-Id: I54fc21ec594effcfb1eededd80dd5b0b52645c4c
    Remove getBoundTexture().
    
    GetBoundTexture checks the bound and active texture,
    and we should clear the id from all bound textures
    (regardless of whether they're active or not).
    Remove the check and getBoundTexture() method,
    and just always call caches.unbindTexture(texture.id).
    
    CRs-fixed: 681102
    
    Change-Id: I481386394f382519bb7ea2700c4e808e00474c70
    Fixing parcel leaks to avoid virtual memory leak
    
    Client has to call recycle() on parcel object after its usage
    otherwise native layer of binder won't clear the resources of
    parcel which were allocated for IPC
    
    Change-Id: Ib31ddcc92aa4ebd80bb66729922b9133692e9c9e
    MountService: Check for null pointer when looking for vold code
    
    * There is actually no code path that will set an event into
      NativeDaemonConnectorException, so any reference to the event code
      will result in a null pointer exception, taking down MountService and
      the rest of the system with it.
    
    Change-Id: Id62f724953aacc363b3455584c6a3ad1381c6138
    Revert "listview: ensure active views are filled prior than scrap views"
    
    This reverts commit 830a0024f1de9655d50c703ed1d10a81005db40e.
    
    See http://review.cyanogenmod.org/#/c/67348/
    
    Change-Id: I066e6ee8d9dc478c752ecfa161fd638bce7cee40

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -6707,12 +6707,6 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         private SparseArray<View> mTransientStateViews;
         private LongSparseArray<View> mTransientStateViewsById;
 
-        private boolean mIsActiveViewsInitialized;
-
-        boolean isActiveViewsInitialized() {
-            return mIsActiveViewsInitialized;
-        }
-
         public void setViewTypeCount(int viewTypeCount) {
             if (viewTypeCount < 1) {
                 throw new IllegalArgumentException("Can't have a viewTypeCount < 1");
@@ -6783,15 +6777,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
                 }
             }
 
-            if (mTransientStateViews != null) {
-                mTransientStateViews.clear();
-            }
-            if (mTransientStateViewsById != null) {
-                mTransientStateViewsById.clear();
-            }
-
-            // We want to a refresh of mActiveViews
-            mIsActiveViewsInitialized = false;
+            clearTransientStateViews();
         }
 
         /**
@@ -6802,11 +6788,6 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
          *        mActiveViews
          */
         void fillActiveViews(int childCount, int firstActivePosition) {
-            // The recyclebin is initialized when we have some active views.
-            if (childCount > 0) {
-                mIsActiveViewsInitialized = true;
-            }
-
             if (mActiveViews.length < childCount) {
                 mActiveViews = new View[childCount];
             }
