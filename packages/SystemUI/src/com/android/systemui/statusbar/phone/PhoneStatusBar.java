0c71b37dfe15a98fc58f95067805a7f0557d14c6
David James <james.n.david@gmail.com>
2014-07-19 06:12:11 -0500

    HeadsUp: Only remove heads up if attached to window
    Attempting to remove mHeadsUpNoficiationView from the window manager
    when it is not currently attached will throw an IllegalArgumentException
    so we check if it is attached and if so remove it, otherwise carry on.
    
    As well remove possible left children in statusbarview
    
    Change-Id: Ida916eba496c4183e47288ef24a8a93c46a7eddf
    SystemUI: improve one finger notification expansion
    
    Sometimes the quick settings ribbon would eat the initial ACTION_DOWN
    setting mInitialTouchY in the expand helper, but we are clearly not in a
    notification that we can expand here. So only set the initial y point if
    we are inside the notification scrollview.
    
    Change-Id: I628553ac15cc6b4ee3c682fe46bd1d96c6bc154d
    HeadsUp: Correctly add and remove the window
    
    isAttachedToWindow() basically works to determine if the view was attached or not.
    
    But on the boot up process when already addHeadsUpView is called but the system is not
    fully up and a recreation happens (eg on TRDS...due that the UIModeManager is doing its work as soon as system is ready)
    the view gets attached....but not removed on the recreation due that at this time isAttachedToWindow() will return false even though the
    window was added.
    
    To solve simply use a boolean instead of isAttachedToWindow()
    
    Change-Id: Ib576a83cadc581683f2a5b0afc922c53b01888b7

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -331,6 +331,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private HeadsUpNotificationView mHeadsUpNotificationView;
     private int mHeadsUpNotificationDecay;
     private boolean mHeadsUpExpandedByDefault;
+    private boolean mHeadsUpNotificationViewAttached;
 
     // on-screen navigation buttons
     private NavigationBarView mNavigationBarView = null;
@@ -1880,6 +1881,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     private void addHeadsUpView() {
+        if (mHeadsUpNotificationViewAttached) {
+            return;
+        }
+
+        mHeadsUpNotificationViewAttached = true;
         WindowManager.LayoutParams lp = new WindowManager.LayoutParams(
                 LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.TYPE_STATUS_BAR_PANEL, // above the status bar!
@@ -1901,7 +1907,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     private void removeHeadsUpView() {
-        mWindowManager.removeView(mHeadsUpNotificationView);
+        if (mHeadsUpNotificationViewAttached) {
+            mHeadsUpNotificationViewAttached = false;
+            mWindowManager.removeView(mHeadsUpNotificationView);
+        }
     }
 
     public void refreshAllStatusBarIcons() {
@@ -4239,11 +4248,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     private void setHeadsUpVisibility(boolean vis) {
         if (DEBUG) Log.v(TAG, (vis ? "showing" : "hiding") + " heads up window");
-        mHeadsUpNotificationView.setVisibility(vis ? View.VISIBLE : View.GONE);
-        if (!vis) {
-            if (DEBUG) Log.d(TAG, "setting heads up entry to null");
-            mInterruptingNotificationEntry = null;
-            mHeadsUpPackageName = null;
+        if (mHeadsUpNotificationViewAttached) {
+            mHeadsUpNotificationView.setVisibility(vis ? View.VISIBLE : View.GONE);
+            if (!vis) {
+                if (DEBUG) Log.d(TAG, "setting heads up entry to null");
+                mInterruptingNotificationEntry = null;
+                mHeadsUpPackageName = null;
+            }
         }
     }
 
@@ -4290,8 +4301,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private void recreateStatusBar(boolean recreateNavigationBar) {
         mRecreating = true;
 
-        mStatusBarContainer.removeAllViews();
         removeHeadsUpView();
+
+        mStatusBarContainer.removeAllViews();
+        mStatusBarContainer.clearDisappearingChildren();
 
         // extract icons from the soon-to-be recreated viewgroup.
         int nIcons = mStatusIcons.getChildCount();
