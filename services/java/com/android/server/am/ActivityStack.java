49ee56895c1983154eae34083adbef352830aff3
David James <james.n.david@gmail.com>
2014-07-19 17:48:46 -0500

    improve expression
    Change-Id: I681efa9b327cd671f900c90684218e932db2ef95

--- services/java/com/android/server/am/ActivityStack.java
+++ services/java/com/android/server/am/ActivityStack.java
@@ -1185,7 +1185,7 @@ final class ActivityStack {
 		            }
 		    }
 
-                    if (r.fullscreen && !isSplitView && !r.floatingWindow) {
+                    if (r.fullscreen && (!r.floatingWindow || !isSplitView)) {
                         // At this point, nothing else needs to be shown
                         if (DEBUG_VISBILITY) Slog.v(TAG, "Fullscreen: at " + r);
                         behindFullscreen = true;
@@ -2861,7 +2861,7 @@ final class ActivityStack {
                 if (r.finishing) {
                     continue;
                 }
-                if (r.fullscreen) {
+                if (r.fullscreen && !r.floatingWindow) {
                     lastIsOpaque = true;
                 }
                 if (owner != null && r.app != owner) {
