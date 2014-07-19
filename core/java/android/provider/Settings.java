2cf24bf0ecf983c55e71431cf779f6d18fba716c
David James <james.n.david@gmail.com>
2014-07-19 12:54:05 -0500

    Completely integrate HFM into Settings [4/4]
    * Checks if hosts file is correct after completing boot (useful after updating ROM)
    * One less package
    
    Change-Id: I103ed99a911cbeb503bb090087a3fee274784b6e
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6290,6 +6290,12 @@ public final class Settings {
         public static final String PEEK_WAKE_TIMEOUT = "peek_wake_timeout";
 
         /**
+         * Disable ads (HFM)
+         * @hide
+         */
+        public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
