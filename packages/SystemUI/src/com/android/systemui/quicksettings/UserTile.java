d35b6e07bdda5b7875d70a6b3d0c5542d3d248d8
David James <james.n.david@gmail.com>
2014-07-20 16:45:42 -0500

    SystemUI: Update UserTile from Google 4.4 code
    Change-Id: I867167b83897636a3e28d08849e018a3b25caad7

--- packages/SystemUI/src/com/android/systemui/quicksettings/UserTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/UserTile.java
@@ -1,22 +1,8 @@
-/*
- * Copyright (C) 2012 The Android Open Source Project
- * Copyright (C) 2013 CyanogenMod Project
- * Copyright (C) 2013 The SlimRoms Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.android.systemui.quicksettings;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 import android.app.ActivityManagerNative;
 import android.content.Context;
@@ -25,18 +11,22 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.UserInfo;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.os.RemoteException;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.ContactsContract;
+import android.provider.ContactsContract.Contacts;
+import android.provider.ContactsContract.RawContacts;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.provider.ContactsContract.Profile;
 import android.util.Log;
 import android.util.Pair;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManagerGlobal;
@@ -50,29 +40,47 @@ import com.android.systemui.statusbar.phone.QuickSettingsController;
 public class UserTile extends QuickSettingsTile {
 
     private static final String TAG = "UserTile";
+    private static final String INTENT_EXTRA_NEW_LOCAL_PROFILE = "newLocalProfile";
+    private Handler mHandler;
     private Drawable userAvatar;
     private AsyncTask<Void, Void, Pair<String, Drawable>> mUserInfoTask;
 
-    public UserTile(Context context, QuickSettingsController qsc) {
+    public UserTile(Context context, QuickSettingsController qsc, Handler handler) {
         super(context, qsc, R.layout.quick_settings_tile_user);
-
+        mHandler = handler;
         mOnClick = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mQsc.mBar.collapseAllPanels(true);
-                final UserManager um =
-                        (UserManager) mContext.getSystemService(Context.USER_SERVICE);
+                final UserManager um = UserManager.get(mContext);
                 if (um.getUsers(true).size() > 1) {
-                    try {
-                        WindowManagerGlobal.getWindowManagerService().lockNow(
-                                null);
-                    } catch (RemoteException e) {
-                        Log.e(TAG, "Couldn't show user switcher", e);
-                    }
+                    // Since keyguard and systemui were merged into the same process to save
+                    // memory, they share the same Looper and graphics context.  As a result,
+                    // there's no way to allow concurrent animation while keyguard inflates.
+                    // The workaround is to add a slight delay to allow the animation to finish.
+                    mHandler.postDelayed(new Runnable() {
+                        public void run() {
+                            try {
+                                WindowManagerGlobal.getWindowManagerService().lockNow(null);
+                            } catch (RemoteException e) {
+                                Log.e(TAG, "Couldn't show user switcher", e);
+                            }
+                        }
+                    }, 400); // TODO: ideally this would be tied to the collapse of the panel
                 } else {
-                    Intent intent = new Intent(Intent.ACTION_VIEW,
-                            ContactsContract.Profile.CONTENT_URI);
-                    startSettingsActivity(intent);
+                    final Cursor cursor = mContext.getContentResolver().query(
+                            Profile.CONTENT_URI, null, null, null, null);
+                    if (cursor.moveToNext() && !cursor.isNull(0)) {
+                        Intent intent = ContactsContract.QuickContact.composeQuickContactsIntent(
+                                mContext, v, ContactsContract.Profile.CONTENT_URI,
+                                ContactsContract.QuickContact.MODE_LARGE, null);
+                        mContext.startActivityAsUser(intent, new UserHandle(UserHandle.USER_CURRENT));
+                    } else {
+                        Intent intent = new Intent(Intent.ACTION_INSERT, Contacts.CONTENT_URI);
+                        intent.putExtra(INTENT_EXTRA_NEW_LOCAL_PROFILE, true);
+                        startSettingsActivity(intent);
+                    }
+                    cursor.close();
                 }
             }
         };
@@ -100,13 +108,7 @@ public class UserTile extends QuickSettingsTile {
     void updateQuickSettings() {
         ImageView iv = (ImageView) mTile.findViewById(R.id.user_imageview);
         TextView tv = (TextView) mTile.findViewById(R.id.user_textview);
-        if (tv != null) {
-            tv.setText(mLabel);
-            tv.setTextSize(TypedValue.COMPLEX_UNIT_PX, mTileTextSize);
-            if (mTileTextColor != -2) {
-                tv.setTextColor(mTileTextColor);
-            }
-        }
+        tv.setText(mLabel);
         iv.setImageDrawable(userAvatar);
     }
 
@@ -130,44 +132,63 @@ public class UserTile extends QuickSettingsTile {
         mUserInfoTask = new AsyncTask<Void, Void, Pair<String, Drawable>>() {
             @Override
             protected Pair<String, Drawable> doInBackground(Void... params) {
-                try {
-                    // The system needs some time to change the picture,
-                    // if we try to load it when we receive the broadcast, we will load the old one
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                final UserManager um =
-                        (UserManager) mContext.getSystemService(Context.USER_SERVICE);
+                final UserManager um = UserManager.get(mContext);
 
-                // Fall back to the UserManager nickname if we can't read the name from the local
-                // profile below.
-                String name = userName;
+                String name = null;
                 Drawable avatar = null;
-                Bitmap rawAvatar = um.getUserIcon(userId);
-                if (rawAvatar != null) {
-                    avatar = new BitmapDrawable(mContext.getResources(), rawAvatar);
-                } else {
-                    avatar = mContext.getResources().getDrawable(R.drawable.ic_qs_default_user);
-                }
-
+                String id = null;
                 // If it's a single-user device, get the profile name, since the nickname is not
                 // usually valid
                 if (um.getUsers().size() <= 1) {
                     // Try and read the display name from the local profile
                     final Cursor cursor = context.getContentResolver().query(
-                            Profile.CONTENT_URI, new String[] {Phone._ID, Phone.DISPLAY_NAME},
+                            Profile.CONTENT_URI, new String[] {Phone.PHOTO_FILE_ID, Phone.DISPLAY_NAME},
                             null, null, null);
                     if (cursor != null) {
                         try {
                             if (cursor.moveToFirst()) {
                                 name = cursor.getString(cursor.getColumnIndex(Phone.DISPLAY_NAME));
+                                id = cursor.getString(cursor.getColumnIndex(Phone.PHOTO_FILE_ID));
                             }
                         } finally {
                             cursor.close();
                         }
+                        // Fall back to the UserManager nickname if we can't read the name from the local
+                        // profile below.
+                        if (name == null) {
+                            avatar = mContext.getResources().getDrawable(R.drawable.ic_qs_default_user);
+                            name = mContext.getResources().getString(com.android.internal.R.string.owner_name);
+                        } else {
+                            Bitmap rawAvatar = null;
+                            InputStream is = null;
+                            try {
+                                Uri.Builder uriBuilder = ContactsContract.DisplayPhoto.CONTENT_URI.buildUpon();
+                                uriBuilder.appendPath(id);
+                                is = mContext.getContentResolver().openInputStream(uriBuilder.build());
+                                rawAvatar = BitmapFactory.decodeStream(is);
+                                avatar = new BitmapDrawable(mContext.getResources(), rawAvatar);
+                            } catch (FileNotFoundException e) {
+                                avatar = mContext.getResources().getDrawable(R.drawable.ic_qs_default_user);
+                            } finally {
+                                if (is != null) {
+                                    try {
+                                        is.close();
+                                    } catch (IOException e) {
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    name = userName;
+                    Bitmap rawAvatar = um.getUserIcon(userId);
+                    if (rawAvatar != null) {
+                        avatar = new BitmapDrawable(mContext.getResources(), rawAvatar);
+                    } else {
+                        avatar = mContext.getResources().getDrawable(R.drawable.ic_qs_default_user);
                     }
                 }
+
                 return new Pair<String, Drawable>(name, avatar);
             }
 
