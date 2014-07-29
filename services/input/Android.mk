CyanogenMod/android_frameworks_base

Add support for using the flip switch to turn off the screen
Change-Id: I3457af831bf5e9cd2d49efda52fb5961fee5d68f

services/input/Android.mk
@@ -52,6 +52,10 @@ ifeq ($(BOARD_USE_LEGACY_TRACKPAD),true)
 LOCAL_CFLAGS += -DLEGACY_TRACKPAD
 endif
 
+ifeq ($(BOARD_HAS_FLIP_COVER_SENSOR),true)
+LOCAL_CFLAGS += -DFLIP_COVER_SUPPORT
+endif
+
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)

omnirom/android_frameworks_base_old

services/input/Android.mk
@@ -44,6 +44,10 @@ LOCAL_C_INCLUDES := \
 
 LOCAL_MODULE:= libinputservice
 
+ifeq ($(BOARD_HAS_EXTENDED_SWITCH),true)
+LOCAL_CFLAGS += -DEXTENDED_SWITCH
+endif
+
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)