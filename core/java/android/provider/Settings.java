857224cba7188d179d959ac8876a2315fa368ca6
Adnan <adnan@cyngn.com>
2014-08-03 17:00:20 -0700

    [1/2] base: Add option to scramble PIN layout when unlocking
    Change-Id: I6b3763c05f8dc6d49c62c5dc5c01dce8d18d09ca

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2977,6 +2977,13 @@ public final class Settings {
             "lock_pattern_tactile_feedback_enabled";
 
         /**
+         * Whether to scramble a pin unlock layout
+         * @hide
+         */
+        public static final String LOCKSCREEN_PIN_SCRAMBLE_LAYOUT =
+                "lockscreen_scramble_pin_layout";
+
+        /**
          * A formatted string of the next alarm that is set, or the empty string
          * if there is no alarm set.
          *
@@ -4828,6 +4835,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(QS_QUICKBAR_COLUMNS);
             PRIVATE_SETTINGS.add(FINGERPRINT_SUCCESS_VIB);
             PRIVATE_SETTINGS.add(STATUS_BAR_QUICK_QS_PULLDOWN);
+            PRIVATE_SETTINGS.add(LOCKSCREEN_PIN_SCRAMBLE_LAYOUT);
         }
 
         /**
