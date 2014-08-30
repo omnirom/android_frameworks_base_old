6435d83b533a37b0da388985da0a042e555e9cfd
sarbyn <sarbyn@gmail.com>
2014-08-30 20:29:04 +0530

    Development shortcuts (1/2)
    Change-Id: Id585483d2ec00f2b5bf929bf2b5e8948a5aae510
    
    Conflicts:
    	packages/SystemUI/AndroidManifest.xml
    
    Change-Id: Id585483d2ec00f2b5bf929bf2b5e8948a5aae510

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -24,6 +24,7 @@ import android.app.ActivityManagerNative;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
+import android.app.admin.DevicePolicyManager;
 import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -31,6 +32,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.IPackageDataObserver;
+import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
 import android.content.pm.ResolveInfo;
@@ -53,6 +56,7 @@ import android.service.dreams.IDreamManager;
 import android.service.notification.StatusBarNotification;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.Slog;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.IWindowManager;
@@ -456,6 +460,31 @@ public abstract class BaseStatusBar extends SystemUI implements
                 mNotificationBlamePopup.getMenuInflater().inflate(
                         R.menu.notification_popup_menu,
                         mNotificationBlamePopup.getMenu());
+		final ContentResolver cr =  mContext.getContentResolver();
+
+		if (Settings.Secure.getInt(cr,
+			Settings.Secure.DEVELOPMENT_SHORTCUT, 0) == 0) {
+		    mNotificationBlamePopup.getMenu()
+			    .findItem(R.id.notification_inspect_item_force_stop).setVisible(false);
+		    mNotificationBlamePopup.getMenu()
+			    .findItem(R.id.notification_inspect_item_wipe_app).setVisible(false);
+		} else {
+		    try {
+			PackageManager pm =  (PackageManager) mContext.getPackageManager();
+			ApplicationInfo mAppInfo = pm.getApplicationInfo(packageNameF, 0);
+			DevicePolicyManager mDpm = (DevicePolicyManager) mContext
+			       .getSystemService(Context.DEVICE_POLICY_SERVICE);
+			if ((mAppInfo.flags&(ApplicationInfo.FLAG_SYSTEM
+			      | ApplicationInfo.FLAG_ALLOW_CLEAR_USER_DATA))
+			      == ApplicationInfo.FLAG_SYSTEM
+			      || mDpm.packageHasActiveAdmins(packageNameF)) {
+			    mNotificationBlamePopup.getMenu()
+			    .findItem(R.id.notification_inspect_item_wipe_app).setEnabled(false);
+			}
+		    } catch (NameNotFoundException ex) {
+			Slog.e(TAG, "Failed looking up ApplicationInfo for " + packageNameF, ex);
+		    }
+		}
 
                 MenuItem hideIconCheck = mNotificationBlamePopup.getMenu().findItem(R.id.notification_hide_icon_packages);
                 if (hideIconCheck != null) {
@@ -471,6 +500,17 @@ public abstract class BaseStatusBar extends SystemUI implements
                         if (item.getItemId() == R.id.notification_inspect_item) {
                             startApplicationDetailsActivity(packageNameF);
                             animateCollapsePanels(CommandQueue.FLAG_EXCLUDE_NONE);
+			} else if (item.getItemId() == R.id.notification_inspect_item_force_stop) {
+			    ActivityManager am = (ActivityManager) mContext
+				    .getSystemService(
+				    Context.ACTIVITY_SERVICE);
+			    am.forceStopPackage(packageNameF);
+			} else if (item.getItemId() == R.id.notification_inspect_item_wipe_app) {
+			    ActivityManager am = (ActivityManager) mContext
+				    .getSystemService(
+				    Context.ACTIVITY_SERVICE);
+			    am.clearApplicationUserData(packageNameF,
+				    new FakeClearUserDataObserver()); 
                         } else if (item.getItemId() == R.id.notification_hide_icon_packages) {
                             item.setChecked(!item.isChecked());
                             setIconHiddenByUser(packageNameF, item.isChecked());
@@ -486,6 +526,11 @@ public abstract class BaseStatusBar extends SystemUI implements
                 return true;
             }
         };
+    }
+
+    class FakeClearUserDataObserver extends IPackageDataObserver.Stub {
+	public void onRemoveCompleted(final String packageName, final boolean succeeded) {
+	}
     }
 
     public void dismissPopups() {
