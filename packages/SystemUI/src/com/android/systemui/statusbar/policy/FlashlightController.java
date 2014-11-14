f50ff1fcf30183fac0eb0a0454a93a198807f7da
kaiyiz <kaiyiz@codeaurora.org>
2014-11-14 16:51:51 +0800

    Fix a SystemUI crash while continuously clicking flashlight tile
    In FlashlightController, after set mSession/mSurface null in teardown(),
    mSession is reassigned in callback of mSessionListener.onConfigured().
    This will lead to no Surface object for mSession.
    
    Don't reassigned mSession when mSurface is already set null.
    
    CRs-Fixed: 750124
    
    Change-Id: I1c4214cc4b58375e82740853c5aff9b68530f816

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/FlashlightController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/FlashlightController.java
@@ -313,8 +313,10 @@ public class FlashlightController {
             new CameraCaptureSession.StateListener() {
         @Override
         public void onConfigured(CameraCaptureSession session) {
-            mSession = session;
-            postUpdateFlashlight();
+            if (mSurface != null) {
+                mSession = session;
+                postUpdateFlashlight();
+            }
         }
 
         @Override
