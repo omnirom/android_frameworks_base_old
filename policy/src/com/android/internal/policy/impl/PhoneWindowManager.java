d287a3433baf07fa23218b7f3ca568d5f6061bb1
Jiangyi <sam.andrew.jiang@gmail.com>
2014-11-20 04:43:04 -0500

    base: add basic support for device wake events
    Handling of events that can be used to wake the device like
    button events and/or screen events like dt2w.
    
    PS3:
    -start with general offscreen event handling
    
    PS4:
    -dont kill power button :)
    
    PS5:
    -added settings for volume and home button wake
    now only someone need to add settings for it :)
    
    PS6:
    -stupid
    
    TODO: ideally devices should provide their keys
    
    Change-Id: I359654ec29c7f44f41bcf7ed71738568e45f8993

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -526,6 +526,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private int mCurrentUserId;
 
+    private boolean mOffscreenGestureSupport;
+    private boolean mVolumeWakeSupport;
+    private boolean mHomeWakeSupport;
+
     // Maps global key codes to the components that will handle them.
     private GlobalKeyManager mGlobalKeyManager;
 
@@ -637,6 +641,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             resolver.registerContentObserver(Settings.Global.getUriFor(
                     Settings.Global.POLICY_CONTROL), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Global.getUriFor(
+                    Settings.System.VOLUME_BUTTON_WAKE), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Global.getUriFor(
+                    Settings.System.HOME_BUTTON_WAKE), false, this,
+                    UserHandle.USER_ALL);
+
             updateSettings();
         }
 
@@ -1022,6 +1033,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 com.android.internal.R.bool.config_lidControlsSleep);
         mTranslucentDecorEnabled = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_enableTranslucentDecor);
+        mOffscreenGestureSupport = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_offscreenGestureSupport);
+
         readConfigurationDependentBehaviors();
 
         mAccessibilityManager = (AccessibilityManager) context.getSystemService(
@@ -1309,6 +1323,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mImmersiveModeConfirmation.loadSetting(mCurrentUserId);
             }
             PolicyControl.reloadFromSetting(mContext);
+
+            mVolumeWakeSupport = Settings.System.getIntForUser(resolver,
+                    Settings.System.VOLUME_BUTTON_WAKE,
+                    0,
+                    UserHandle.USER_CURRENT) != 0;
+            mHomeWakeSupport = Settings.System.getIntForUser(resolver,
+                    Settings.System.HOME_BUTTON_WAKE,
+                    0,
+                    UserHandle.USER_CURRENT) != 0;
         }
         if (updateRotation) {
             updateRotation(true);
@@ -4247,6 +4270,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         int result;
         boolean isWakeKey = (policyFlags & WindowManagerPolicy.FLAG_WAKE) != 0
                 || event.isWakeKey();
+
+        if (mOffscreenGestureSupport && !isWakeKey) {
+            isWakeKey = isOffscreenWakeKey(keyCode);
+        }
         if (interactive || (isInjected && !isWakeKey)) {
             // When the device is interactive or the key is injected pass the
             // key to the application.
@@ -4533,6 +4560,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
      * is always considered a wake key.
      */
     private boolean isWakeKeyWhenScreenOff(int keyCode) {
+        if (mOffscreenGestureSupport){
+            if (isOffscreenWakeKey(keyCode)){
+                return true;
+            }
+        }
         switch (keyCode) {
             // ignore volume keys unless docked
             case KeyEvent.KEYCODE_VOLUME_UP:
@@ -6080,5 +6112,18 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (mOrientationListener != null) {
             mOrientationListener.dump(pw, prefix);
         }
+    }
+
+    private boolean isOffscreenWakeKey(int keyCode) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_F3:
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_UP:
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                return mVolumeWakeSupport;
+            case KeyEvent.KEYCODE_HOME:
+                return mHomeWakeSupport;
+        }
+        return false;
     }
 }
