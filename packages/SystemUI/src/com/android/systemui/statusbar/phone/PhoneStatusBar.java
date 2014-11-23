6bdf24bb659b9d03ab01a1d3b414924e478e9d98
Roman Birg <roman@cyngn.com>
2014-11-23 06:54:06 -0800

    [1/2] SystemUI: Add double tap to sleep gesture
    Change-Id: I5ee6818040733b473ad77f2d9f4071fe3dac6f6c

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -402,6 +402,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private int mInitialTouchX;
     private int mInitialTouchY;
     private IPowerManager mPower;
+    private boolean mDoubleTabSleep;
 
     // for disabling the status bar
     int mDisabled1 = 0;
@@ -502,6 +503,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
                     Settings.System.SCREEN_BRIGHTNESS_MODE),
                     false, this, UserHandle.USER_ALL);
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.DOUBLE_TAP_SLEEP_GESTURE),
+                    false, this, UserHandle.USER_ALL);
 
             update();
         }
@@ -526,6 +530,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL;
             mBrightnessControl = Settings.System.getIntForUser(
                     mContext.getContentResolver(), Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL, 0, mCurrentUserId) == 1;
+            mDoubleTabSleep = Settings.System.getIntForUser(
+                    mContext.getContentResolver(), Settings.System.DOUBLE_TAP_SLEEP_GESTURE, 0, mCurrentUserId) == 1;
+
+            if (mStatusBarWindow != null) {
+                mStatusBarWindow.setDoubleTabSleep(mDoubleTabSleep);
+            }
         }
     }
     private OmniSettingsObserver mOmniSettingsObserver = new OmniSettingsObserver(mHandler);
