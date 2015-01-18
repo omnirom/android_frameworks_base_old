147cf03663ac39c446fd785563aac0cc55270ef8
Adnan Begovic <adnan@cyngn.com>
2015-01-18 12:54:29 -0500

    Implement linked volumes and add upgrade path.
    Patchset 3: Moved from Secure to System, we don´t need it
    to be Secure.
    Change-Id: Ibbeff656fcaaa4a2ff79de530b48b216df271b21

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5104,6 +5104,13 @@ public final class Settings {
         public static final String ADVANCED_REBOOT = "advanced_reboot";
 
         /**
+         * Boolean value whether to link ringtone and notification volume
+         *
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        /**
          * Whether developer settings dare enabled.
          * @hide
          */
