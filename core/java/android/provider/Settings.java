08af22187a06d3b5a88a95b142a0758049b28182
maxwen <max.weninger@gmail.com>
2015-02-07 03:00:51 +0100

    [1/2] base: allow hiding storage removed notification
    this notification can be pretty anyoing e.g. for
    usb otg storage devices
    
    Change-Id: If6401cca67155fe14d4db01eadac81ca3b55e8cf

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2821,6 +2821,13 @@ public final class Settings {
         public static final String SHOW_ALARM_FULLSCREEN = "show_alarm_fullscreen";
 
         /**
+         * Should the storage removed notification be shown
+         *
+         * @hide
+         */
+        public static final String STORAGE_MEDIA_REMOVED_NOTIFICTION = "storage_media_removed_notification";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
