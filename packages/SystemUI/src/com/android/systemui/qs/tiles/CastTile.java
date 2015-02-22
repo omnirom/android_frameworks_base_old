05b869d09c76c6e246ef55dfb78ef3593aec2561
darkobas <darkobas@gmail.com>
2015-02-22 18:20:51 +0100

    QS Tiles: Make location and cast tile dynamic
    PS1> changes for 5.1
    PS4> code cleanup
    
    Change-Id: Ia9e1fc5eef7f07cd9d07a2a3499e020553822f08

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CastTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CastTile.java
@@ -16,6 +16,7 @@
 
 package com.android.systemui.qs.tiles;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.provider.Settings;
@@ -28,6 +29,7 @@ import com.android.systemui.R;
 import com.android.systemui.qs.QSDetailItems;
 import com.android.systemui.qs.QSDetailItems.Item;
 import com.android.systemui.qs.QSTile;
+import com.android.systemui.qs.UsageTracker;
 import com.android.systemui.statusbar.policy.CastController;
 import com.android.systemui.statusbar.policy.CastController.CastDevice;
 import com.android.systemui.statusbar.policy.KeyguardMonitor;
@@ -50,6 +52,16 @@ public class CastTile extends QSTile<QSTile.BooleanState> {
         mController = host.getCastController();
         mDetailAdapter = new CastDetailAdapter();
         mKeyguard = host.getKeyguardMonitor();
+        mUsageTracker = new UsageTracker(host.getContext(), CastTile.class,
+                R.integer.days_to_show_cast_tile);
+        mUsageTracker.setListening(true);
+
+    }
+
+    @Override
+    protected void handleDestroy() {
+        super.handleDestroy();
+        mUsageTracker.setListening(false);
     }
 
     @Override
@@ -90,7 +102,7 @@ public class CastTile extends QSTile<QSTile.BooleanState> {
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
-        state.visible = !(mKeyguard.isSecure() && mKeyguard.isShowing());
+        state.visible = !(mKeyguard.isSecure() && mKeyguard.isShowing()) && mUsageTracker.isRecentlyUsed();
         state.label = mContext.getString(R.string.quick_settings_cast_title);
         state.value = false;
         state.autoMirrorDrawable = false;
@@ -245,4 +257,21 @@ public class CastTile extends QSTile<QSTile.BooleanState> {
             mController.stopCasting(device);
         }
     }
+
+    /**
+     * This will catch broadcasts for changes in cast state so we can show
+     * the cast tile for a number of days after use.
+     */
+    public static class CastTileChangedReceiver extends BroadcastReceiver {
+        private UsageTracker mUsageTracker;
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (mUsageTracker == null) {
+                mUsageTracker = new UsageTracker(context, CastTile.class);
+            }
+            mUsageTracker.trackUsage();
+        }
+    }
+
 }
