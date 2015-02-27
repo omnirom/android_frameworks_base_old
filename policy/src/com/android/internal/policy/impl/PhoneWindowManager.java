663cf21d7de5a9c2b0e922e105f76f00a206e2ff
maxwen <max.weninger@gmail.com>
2015-02-27 18:47:29 +0100

    base: fix wake key handling
    ignore up event from e.g. volume wake else a volume
    adjust sound will be triggered
    
    Change-Id: I56fe4e60f66267699f9414507ac3f8e7c4c88cc1

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -597,6 +597,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private boolean mVolumeUpKeyTriggered;
     private boolean mPowerKeyTriggered;
     private long mPowerKeyTime;
+    private boolean mWakeKeyTriggered;
 
     /* The number of steps between min and max brightness */
     private static final int BRIGHTNESS_STEPS = 10;
@@ -4855,7 +4856,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
             Slog.d(TAG, "interceptKeyTq keycode=" + keyCode
                     + " interactive=" + interactive + " keyguardActive=" + keyguardActive
-                    + " policyFlags=" + Integer.toHexString(policyFlags));
+                    + " policyFlags=" + Integer.toHexString(policyFlags) + " down=" + down);
         }
 
         // Basic policy based on interactive state.
@@ -4869,8 +4870,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (interactive || (isInjected && !isWakeKey)) {
             // When the device is interactive or the key is injected pass the
             // key to the application.
-            result = ACTION_PASS_TO_USER;
+            if (mWakeKeyTriggered && !down) {
+                // inore the up from the volume wake event else
+                // you will get tha volume adjust sound
+                result = 0;
+            } else {
+                result = ACTION_PASS_TO_USER;
+            }
             isWakeKey = false;
+            mWakeKeyTriggered = false;
         } else if (!interactive && shouldDispatchInputWhenNonInteractive()) {
             // If we're currently dozing with the screen on and the keyguard showing, pass the key
             // to the application but preserve its wake key status to make sure we still move
@@ -6753,8 +6761,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KeyEvent.KEYCODE_VOLUME_UP:
             case KeyEvent.KEYCODE_VOLUME_DOWN:
                 if (DEBUG_WAKEUP) Log.i(TAG, "isOffscreenWakeKey: mVolumeWakeSupport " + mVolumeWakeSupport);
+                if (mVolumeWakeSupport) {
+                    mWakeKeyTriggered = true;
+                }
                 return mVolumeWakeSupport;
             case KeyEvent.KEYCODE_HOME:
+                if (DEBUG_WAKEUP) Log.i(TAG, "isOffscreenWakeKey: mHomeWakeSupport " + mHomeWakeSupport);
+                if (mHomeWakeSupport) {
+                    mWakeKeyTriggered = true;
+                }
                 return mHomeWakeSupport;
         }
         return false;
