38f44b311dcc870dadc671e4ab49a37ff7573787
XpLoDWilD <me@xplod.fr>
2015-03-10 15:38:09 +0000

    Revert "base: temp fix ColorFade memory leak"
    This reverts commit d2d572a49a847db6fb118a845454eabc15c6c946.

--- services/core/java/com/android/server/display/ColorFade.java
+++ services/core/java/com/android/server/display/ColorFade.java
@@ -450,7 +450,6 @@ final class ColorFade {
         if (!attachEglContext()) {
             return false;
         }
-        SurfaceTexture st = null;
         try {
             if (!mTexNamesGenerated) {
                 GLES20.glGenTextures(1, mTexNames, 0);
@@ -460,7 +459,7 @@ final class ColorFade {
                 mTexNamesGenerated = true;
             }
 
-            st = new SurfaceTexture(mTexNames[0]);
+            final SurfaceTexture st = new SurfaceTexture(mTexNames[0]);
             final Surface s = new Surface(st);
             try {
                 SurfaceControl.screenshot(SurfaceControl.getBuiltInDisplay(
@@ -484,9 +483,6 @@ final class ColorFade {
             GLES20.glViewport(0, 0, mDisplayWidth, mDisplayHeight);
             ortho(0, mDisplayWidth, 0, mDisplayHeight, -1, 1);
         } finally {
-            if (st != null) {
-                st.release();
-            }
             detachEglContext();
         }
         return true;
