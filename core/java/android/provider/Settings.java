62417e012b66900167fe0c8d36225f62c69a0cb7
kldoc <fred.berbigier@gmail.com>
2015-03-18 12:47:03 +0800

    [1/3] base: battery light settings
    Based on Max Weninger commits
    
    https://gerrit.omnirom.org/#/c/3498/
    https://gerrit.omnirom.org/#/c/2810/
    
    Based on Guillaume Lesniak commits, Originally from dvtonder (CyanogenMod)
    https://gerrit.omnirom.org/#/c/1068/
    
    [1/2] base: Activate battery light on full battery
    
    Add setting to activate LED only when fully charged.
    
    base: Fix LED short blink during charge
    
    If 'Only light up when fully charged' is enabled, a short LED blink
    was to be seen during charge on every ACTION_BATTERY_CHANGED intent.
    Move the check up and bail out earlier now.
    
    Change-Id: I4571dd317d99eb2b64f5183ca01800db647c9ec0
    PS2: rework the method by moving all mLights access to the end

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3995,6 +3995,51 @@ public final class Settings {
         public static final String VOLUME_DIALOG_HEADERS = "volume_dialog_headers";
 
         /**
+         * Whether the battery light should be enabled (if hardware supports it)
+         * The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_ENABLED = "battery_light_enabled";
+
+        /**
+         * Whether the battery LED should repeatedly flash when the battery is low
+         * on charge. The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_PULSE = "battery_light_pulse";
+
+        /**
+         * Whether the battery light should only be enabled on fully charged battery.
+         * The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_ONLY_FULLY_CHARGED = "battery_light_only_fully_charged";
+
+        /**
+         * What color to use for the battery LED while charging - low
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_LOW_COLOR = "battery_light_low_color";
+
+        /**
+         * What color to use for the battery LED while charging - medium
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_MEDIUM_COLOR = "battery_light_medium_color";
+
+        /**
+         * What color to use for the battery LED while charging - full
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_FULL_COLOR = "battery_light_full_color";
+
+        /**
+         * What color to use for the battery LED while charging - really full (100%)
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_REALLY_FULL_COLOR = "battery_light_really_full_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4166,6 +4211,13 @@ public final class Settings {
             PRIVATE_SETTINGS.add(VOLUME_LINK_NOTIFICATION);
             PRIVATE_SETTINGS.add(VOLUME_BUTTON_FORCE_MEDIA);
             PRIVATE_SETTINGS.add(VOLUME_DIALOG_HEADERS);
+            PRIVATE_SETTINGS.add(BATTERY_LIGHT_ENABLED);
+            PRIVATE_SETTINGS.add(BATTERY_LIGHT_PULSE);
+            PRIVATE_SETTINGS.add(BATTERY_LIGHT_ONLY_FULLY_CHARGED);
+            PRIVATE_SETTINGS.add(BATTERY_LIGHT_FULL_COLOR);
+            PRIVATE_SETTINGS.add(BATTERY_LIGHT_LOW_COLOR);
+            PRIVATE_SETTINGS.add(BATTERY_LIGHT_MEDIUM_COLOR);
+            PRIVATE_SETTINGS.add(BATTERY_LIGHT_REALLY_FULL_COLOR);
         }
 
         /**
