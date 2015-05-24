c67051975448e6e76e8666e8ac3db58add291e04
maxwen <max.weninger@gmail.com>
2015-05-25 00:35:06 +0200

    base: base: change guest mode activation - follow up
    change supportsMultipleUsers to reflect that guest
    mode only is possible
    
    Change-Id: Ib2785fe6912c4301db192f755d0df883a216b558

--- core/java/android/os/UserManager.java
+++ core/java/android/os/UserManager.java
@@ -436,8 +436,7 @@ public class UserManager {
      * @hide
      */
     public static boolean supportsMultipleUsers() {
-        return getMaxSupportedUsers() > 1
-                && SystemProperties.getBoolean("fw.show_multiuserui",
+        return SystemProperties.getBoolean("fw.show_multiuserui",
                 Resources.getSystem().getBoolean(R.bool.config_enableMultiUserUI));
     }
 
