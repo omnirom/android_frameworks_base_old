0f0d0ba84fc96d9682dbc1216c1eac42da366a87
maxwen <max.weninger@gmail.com>
2015-06-18 22:27:33 +0200

    base: stop low battery light pulse correctly
    Change-Id: I55671dc7fb587939c801d7d2ac5fe08fc23b3deb

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -739,10 +739,15 @@ public final class BatteryService extends SystemService {
                 if (status == BatteryManager.BATTERY_STATUS_CHARGING) {
                     // Solid red when battery is charging
                     mBatteryLight.setColor(mBatteryLowARGB);
-                } else if (mLedPulseEnabled) {
-                    // Flash red when battery is low and not charging
-                    mBatteryLight.setFlashing(mBatteryLowARGB, Light.LIGHT_FLASH_TIMED,
-                            mBatteryLedOn, mBatteryLedOff);
+                } else {
+                    if (mLedPulseEnabled) {
+                        // Flash red when battery is low and not charging
+                        mBatteryLight.setFlashing(mBatteryLowARGB, Light.LIGHT_FLASH_TIMED,
+                                mBatteryLedOn, mBatteryLedOff);
+                    } else {
+                        // make sure we stop any flashing
+                        mBatteryLight.turnOff();
+                    }
                 }
             } else if (status == BatteryManager.BATTERY_STATUS_CHARGING
                     || status == BatteryManager.BATTERY_STATUS_FULL) {
