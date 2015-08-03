56f2981a45bfbe7e30ba1f8796b1c7583f6ea0dc
maxwen <max.weninger@gmail.com>
2015-08-04 01:51:10 +0200

    [1/2] base: configure doze brightness value
    Change-Id: I751ab5cfd200d63e5e40e90c0f77ed1b2b384713

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3056,6 +3056,13 @@ public final class Settings {
         public static final String NAVIGATION_BAR_RECENTS = "navigation_bar_recents";
 
         /**
+         * custom screen brightness for doze mode
+         * @hide
+         */
+        public static final String SCREEN_BRIGHTNESS_DOZE = "screen_brightness_doze";
+
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
