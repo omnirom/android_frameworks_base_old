e165e381e78e69798367fc42b171fde604332b7f
William Clark <wclark@codeaurora.org>
2015-08-21 17:44:46 -0700

    SEEMP: add framework instrumentation
    - Instrument framework files to log events of interest for QSSP.
    
    - Fix the String out of boundary
    
    CRs-Fixed: 1020829
    
    Change-Id: Iab970a78e48a504a9eff066cb3c0abe83a62513a

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -816,6 +816,7 @@ public class TelephonyManager {
      * @param slotId of which deviceID is returned
      */
     public String getDeviceId(int slotId) {
+        android.util.SeempLog.record_str(8, ""+slotId);
         // FIXME this assumes phoneId == slotId
         try {
             IPhoneSubInfo info = getSubscriberInfo();
@@ -911,6 +912,7 @@ public class TelephonyManager {
      * {@link android.Manifest.permission#ACCESS_COARSE_LOCATION ACCESS_FINE_LOCATION}.
      */
     public CellLocation getCellLocation() {
+        android.util.SeempLog.record(49);
         try {
             ITelephony telephony = getITelephony();
             if (telephony == null) {
@@ -1008,6 +1010,7 @@ public class TelephonyManager {
      */
     @Deprecated
     public List<NeighboringCellInfo> getNeighboringCellInfo() {
+        android.util.SeempLog.record(50);
         try {
             ITelephony telephony = getITelephony();
             if (telephony == null)
@@ -2007,6 +2010,7 @@ public class TelephonyManager {
      * @hide
      */
     public String getSimSerialNumber(int subId) {
+        android.util.SeempLog.record_str(388, ""+subId);
         try {
             IPhoneSubInfo info = getSubscriberInfo();
             if (info == null)
@@ -2096,6 +2100,7 @@ public class TelephonyManager {
      * @hide
      */
     public String getSubscriberId(int subId) {
+        android.util.SeempLog.record_str(389, ""+subId);
         try {
             IPhoneSubInfo info = getSubscriberInfo();
             if (info == null)
@@ -2184,6 +2189,7 @@ public class TelephonyManager {
      * @hide
      */
     public String getLine1Number(int subId) {
+        android.util.SeempLog.record_str(9, ""+subId);
         String number = null;
         try {
             ITelephony telephony = getITelephony();
