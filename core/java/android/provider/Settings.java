9a858f650c8a1876707a9cd57180918f5761fd3b
mar-v-in <github@rvin.mooo.com>
2015-10-09 17:41:22 +0200

    [1/2] core: Add support for fake signatures, enabled by developer setting
    Change-Id: Ib366f8d40e12ae83f06d797c94a2ad983d91a250

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4123,6 +4123,15 @@ public final class Settings {
         public static final String INSTALL_NON_MARKET_APPS = "install_non_market_apps";
 
         /**
+         * Whether applications can fake a signature.
+         *
+         * <p>1 = permit apps to fake signature
+         * <p>0 = disable this feature
+         * @hide
+         */
+        public static final String ALLOW_SIGNATURE_FAKE = "allow_signature_fake";
+
+        /**
          * Comma-separated list of location providers that activities may access. Do not rely on
          * this value being present in settings.db or on ContentObserver notifications on the
          * corresponding Uri.
