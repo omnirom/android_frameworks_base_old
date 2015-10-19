bda66f3b67f13045ca0244d7cc2b291cca0b000e
Alex Cruz <mazdarider23@gmail.com>
2015-10-19 23:57:09 +0000

    Screen pinning improvements
    Inspired by the way ASUS implemented their screen pinning, I took it upon myself to do the
    same with our ROM and move the pinning icon next to the icon for dismissing a recent app.
    
    - Matched the behavior of the recent dismiss icon
    - Changes icon to dark if recent app uses a light primary color
    
    See images to see the differences
    
    AOSP/DU - http://i.imgur.com/DYoKmNL.png
    ASUS Zenfone 2 (inspiration) - http://i.imgur.com/BybyGHp.jpg
    
    What it looks like with this commit in LP - http://i.imgur.com/TFkgn5C.png
    
    - Replaced ASUS icon with a icon covered under the creative commons license
    https://thenounproject.com/term/pin/56183/
    
    Credit/Thanks to @xWASABI for creating the vector because God knows that was kicking my ass :-)
    https://plus.google.com/u/0/101729058057775926785
    
    - Changed multiwindow icon behavior / Added new multiwindow icon
    - Ported forward to Android M
    
    What it looks like with this commit in M - http://i.imgur.com/LlMsdX1.png
    
    Change-Id: Ida5dd4106c35f13e1d573b8bd8fc2b4d8f8ddae1

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskViewHeader.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskViewHeader.java
@@ -31,6 +31,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.RippleDrawable;
+import android.provider.Settings;
 import android.graphics.Outline;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
@@ -79,6 +80,8 @@ public class TaskViewHeader extends FrameLayout {
     Drawable mDarkDismissDrawable;
     Drawable mLightPinDrawable;
     Drawable mDarkPinDrawable;
+    Drawable mLightMultiwindowDrawable;
+    Drawable mDarkMultiwindowDrawable;
     RippleDrawable mBackground;
     GradientDrawable mBackgroundColorDrawable;
     AnimatorSet mFocusAnimator;
@@ -131,6 +134,14 @@ public class TaskViewHeader extends FrameLayout {
         mLightPinDrawable = res.getDrawable(R.drawable.ic_pin);
         mDarkPinDrawable = res.getDrawable(R.drawable.ic_pin_dark);
 
+        // Load the screen pinning resources
+        mLightPinDrawable = context.getDrawable(R.drawable.ic_pin);
+        mDarkPinDrawable = context.getDrawable(R.drawable.ic_pin_dark);
+
+        // Load multi-window resources
+        mLightMultiwindowDrawable = context.getDrawable(R.drawable.ic_multiwindow);
+        mDarkMultiwindowDrawable = context.getDrawable(R.drawable.ic_multiwindow_dark);
+
         // Configure the highlight paint
         if (sHighlightPaint == null) {
             sHighlightPaint = new Paint();
@@ -151,6 +162,7 @@ public class TaskViewHeader extends FrameLayout {
         mApplicationIcon = (ImageView) findViewById(R.id.application_icon);
         mActivityDescription = (TextView) findViewById(R.id.activity_description);
         mDismissButton = (ImageView) findViewById(R.id.dismiss_task);
+        mPinButton = (ImageView) findViewById(R.id.lock_to_app_fab);
         mMoveTaskButton = (ImageView) findViewById(R.id.move_task);
         mPinButton = (ImageView) findViewById(R.id.lock_to_app_fab);
 
@@ -239,45 +251,8 @@ public class TaskViewHeader extends FrameLayout {
         mDismissButton.setContentDescription(String.format(mDismissContentDescription,
                 t.contentDescription));
         mMoveTaskButton.setVisibility((mConfig.multiStackEnabled) ? View.VISIBLE : View.INVISIBLE);
-        if (mConfig.multiStackEnabled) {
-            updateResizeTaskBarIcon(t);
-        }
-    }
-
-    /** Updates the resize task bar button. */
-    void updateResizeTaskBarIcon(Task t) {
-        Rect display = mSsp.getWindowRect();
-        Rect taskRect = mSsp.getTaskBounds(t.key.stackId);
-        int resId = R.drawable.star;
-        if (display.equals(taskRect) || taskRect.isEmpty()) {
-            resId = R.drawable.vector_drawable_place_fullscreen;
-        } else {
-            boolean top = display.top == taskRect.top;
-            boolean bottom = display.bottom == taskRect.bottom;
-            boolean left = display.left == taskRect.left;
-            boolean right = display.right == taskRect.right;
-            if (top && bottom && left) {
-                resId = R.drawable.vector_drawable_place_left;
-            } else if (top && bottom && right) {
-                resId = R.drawable.vector_drawable_place_right;
-            } else if (top && left && right) {
-                resId = R.drawable.vector_drawable_place_top;
-            } else if (bottom && left && right) {
-                resId = R.drawable.vector_drawable_place_bottom;
-            } else if (top && right) {
-                resId = R.drawable.vector_drawable_place_top_right;
-            } else if (top && left) {
-                resId = R.drawable.vector_drawable_place_top_left;
-            } else if (bottom && right) {
-                resId = R.drawable.vector_drawable_place_bottom_right;
-            } else if (bottom && left) {
-                resId = R.drawable.vector_drawable_place_bottom_left;
-            }
-        }
-        mMoveTaskButton.setImageResource(resId);
-        mMoveTaskButton.setColorFilter(t.useLightOnPrimaryColor ?
-                mTaskbarIconLightColor :
-                mTaskbarIconDarkColor, Mode.SRC_ATOP);
+        mMoveTaskButton.setImageDrawable(t.useLightOnPrimaryColor ?
+                mLightMultiwindowDrawable : mDarkMultiwindowDrawable);
     }
 
     /** Unbinds the bar view from the task */
