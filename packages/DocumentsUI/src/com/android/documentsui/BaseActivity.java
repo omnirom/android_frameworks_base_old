a68f8f2ef42b0d7f8c0176479b08660986532da1
maxwen <max.weninger@gmail.com>
2015-10-24 01:02:28 +0200

    DocumentsUI: fix build for 6.0
    Change-Id: Ib9a8ad5233a326386f17d22b2b035c3bab545cca

--- packages/DocumentsUI/src/com/android/documentsui/BaseActivity.java
+++ packages/DocumentsUI/src/com/android/documentsui/BaseActivity.java
@@ -68,7 +68,6 @@ import com.android.documentsui.model.RootInfo;
 import com.google.common.collect.Maps;
 
 abstract class BaseActivity extends Activity {
-
     static final String EXTRA_STATE = "state";
 
     RootsCache mRoots;
@@ -347,6 +346,7 @@ abstract class BaseActivity extends Activity {
         public static final int ACTION_BROWSE = 6;
         public static final int ACTION_BROWSE_ALL = 7;
         public static final int ACTION_OPEN_COPY_DESTINATION = 8;
+        public static final int ACTION_STANDALONE = 9;
 
         public static final int MODE_UNKNOWN = 0;
         public static final int MODE_LIST = 1;
