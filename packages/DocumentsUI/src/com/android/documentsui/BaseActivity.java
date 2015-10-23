83e5c671517edffb31bc34048971207a079fa5ad
maxwen <max.weninger@gmail.com>
2015-10-24 01:02:28 +0200

    DocumentsUI: fix build for 6.0
    Change-Id: Ib9a8ad5233a326386f17d22b2b035c3bab545cca

--- packages/DocumentsUI/src/com/android/documentsui/BaseActivity.java
+++ packages/DocumentsUI/src/com/android/documentsui/BaseActivity.java
@@ -347,6 +347,7 @@ abstract class BaseActivity extends Activity {
         public static final int ACTION_BROWSE = 6;
         public static final int ACTION_BROWSE_ALL = 7;
         public static final int ACTION_OPEN_COPY_DESTINATION = 8;
+        public static final int ACTION_STANDALONE = 9;
 
         public static final int MODE_UNKNOWN = 0;
         public static final int MODE_LIST = 1;
