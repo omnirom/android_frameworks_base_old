270beeb3a969fe2fce5ac9285b6b6ae364b6ebc9
Roman Birg <roman@cyngn.com>
2015-10-28 02:22:48 +0000

    Add quick settings pull down with one finger [1/2]
    Logic is currently set to use 1/3 of right hand side of the statusbar to
    trigger a quick settings quick pull down, quickly.
    
    Change-Id: I3f536496f724f07d2419a5b2db5fa8f8261609e7
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    SystemUI: fix right hand side gestures on lockscreen
    
    two finger quick pulldown was not accounting for being in the keyguard
    adn was eating touch events when it shouldn't be.
    
    Change-Id: I625d48a4002aaaf2d55d7efaed65d892ab7aa13c
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    SystemUI: add Settings.System toggle for quick-quick settings pulldown
    
    Change-Id: I3c5dcfc23f39bf8be6c402e25f61e6f57a8add2c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2458,6 +2458,12 @@ public final class Settings {
          */
         public static final int SCREEN_BRIGHTNESS_MODE_AUTOMATIC = 1;
 
+        /** Whether to allow one finger quick settings expansion on the right side of the statusbar.
+         *
+         * @hide
+         */
+        public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
+
         /**
          * Enable/disable overwrite default value for ambient display
          * @hide
