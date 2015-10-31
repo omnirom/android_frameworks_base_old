389d8f3cc07d0b1ffb90ceb75614485e3fcdce9d
maxwen <max.weninger@gmail.com>
2015-10-31 01:09:58 +0100

    base: correct strings for advanced reboot
    add a customReboot API that is used for advanced
    reboot fromm GlobaActions and use correct strings
    for the reboot progress dialog
    
    [1/2] base: expose rebootCustom for OpenDelta
    
    Dont scare people by showing the wrong reboot progress
    
    Adapted for android 7.0
    
    Change-Id: Ie18e088d37e4acc7d30146ff95b3f608a2ca739e
    
    base: use new shutdown ui for all reboot actions
    
    Change-Id: I81fec5801382d255f903230259df39f9cd977ddd

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
@@ -20,6 +20,8 @@ import static android.view.WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_M
 import android.app.Dialog;
 import android.app.KeyguardManager;
 import android.content.Context;
+import android.graphics.Point;
+import android.os.PowerManager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -84,7 +86,7 @@ public class GlobalActionsImpl implements GlobalActions, CommandQueue.Callbacks 
     }
 
     @Override
-    public void showShutdownUi(boolean isReboot, String reason) {
+    public void showShutdownUi(boolean isReboot, String reason, boolean rebootCustom) {
         ScrimDrawable background = new ScrimDrawable();
         background.setAlpha((int) (SHUTDOWN_SCRIM_ALPHA * 255));
 
@@ -125,7 +127,21 @@ public class GlobalActionsImpl implements GlobalActions, CommandQueue.Callbacks 
         bar.getIndeterminateDrawable().setTint(color);
         TextView message = d.findViewById(R.id.text1);
         message.setTextColor(color);
-        if (isReboot) message.setText(R.string.reboot_to_reset_message);
+        if (rebootCustom) {
+            if (reason != null) {
+                if (PowerManager.REBOOT_BOOTLOADER.equals(reason)) {
+                    message.setText(com.android.internal.R.string.reboot_to_bootloader_message);
+                } else if (PowerManager.REBOOT_RECOVERY.equals(reason)) {
+                    message.setText(com.android.internal.R.string.reboot_to_recovery_message);
+                }
+            } else {
+                message.setText(com.android.internal.R.string.reboot_system_message);
+            }
+        } else {
+            if (isReboot) {
+                message.setText(R.string.reboot_to_reset_message);
+            }
+        }
 
         GradientColors colors = Dependency.get(SysuiColorExtractor.class).getNeutralColors();
         background.setColor(colors.getMainColor(), false);
