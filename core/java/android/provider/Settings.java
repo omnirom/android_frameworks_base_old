fd9d8e77d3ae680885c9243f9474d3b740b74de9
maxwen <max.weninger@windriver.com>
2015-11-04 15:20:21 +0100

    base: fix rotation based volume key swap
    volume key routing is different then in LP
    
    Change-Id: I190bf07fd89fa1688244a399df731cf33af06f43

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3620,6 +3620,12 @@ public final class Settings {
         public static final String GLOBAL_ACTIONS_LIST = "global_actions_list";
 
         /**
+         * Change volume up and down handlign based on rotation
+         * @hide
+         */
+        public static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
