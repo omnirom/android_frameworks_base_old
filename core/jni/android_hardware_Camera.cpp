f1300bdb8e5678edd75ccee61debbb58ea1caa26
Jake Whatley <jacob.whatley91@gmail.com>
2015-11-11 13:36:08 -0500

    base: remove unneeded QCOM_HARDWARE
    Change-Id: I1cc7596fd30b9cc57199e69be904e164848d530c

--- core/jni/android_hardware_Camera.cpp
+++ core/jni/android_hardware_Camera.cpp
@@ -437,58 +437,15 @@ void JNICameraContext::setCallbackMode(JNIEnv *env, bool installed, bool manualM
 static void android_hardware_Camera_setLongshot(JNIEnv *env, jobject thiz, jboolean enable)
 {
     ALOGV("setLongshot");
-#ifdef QCOM_HARDWARE
-    JNICameraContext* context;
-    status_t rc;
-    sp<Camera> camera = get_native_camera(env, thiz, &context);
-    if (camera == 0) return;
-
-    if ( enable ) {
-        rc = camera->sendCommand(CAMERA_CMD_LONGSHOT_ON, 0, 0);
-    } else {
-        rc = camera->sendCommand(CAMERA_CMD_LONGSHOT_OFF, 0, 0);
-    }
-
-    if (rc != NO_ERROR) {
-       jniThrowException(env, "java/lang/RuntimeException", "enabling longshot mode failed");
-    }
-#endif
 }
 
 static void android_hardware_Camera_sendHistogramData(JNIEnv *env, jobject thiz)
  {
    ALOGV("sendHistogramData" );
-#ifdef QCOM_HARDWARE
-   JNICameraContext* context;
-   status_t rc;
-   sp<Camera> camera = get_native_camera(env, thiz, &context);
-   if (camera == 0) return;
-
-   rc = camera->sendCommand(CAMERA_CMD_HISTOGRAM_SEND_DATA, 0, 0);
-
-   if (rc != NO_ERROR) {
-      jniThrowException(env, "java/lang/RuntimeException", "send histogram data failed");
-    }
-#endif
  }
  static void android_hardware_Camera_setHistogramMode(JNIEnv *env, jobject thiz, jboolean mode)
  {
    ALOGV("setHistogramMode: mode:%d", (int)mode);
-#ifdef QCOM_HARDWARE
-   JNICameraContext* context;
-   status_t rc;
-   sp<Camera> camera = get_native_camera(env, thiz, &context);
-   if (camera == 0) return;
-
-   if(mode == true)
-      rc = camera->sendCommand(CAMERA_CMD_HISTOGRAM_ON, 0, 0);
-   else
-      rc = camera->sendCommand(CAMERA_CMD_HISTOGRAM_OFF, 0, 0);
-
-   if (rc != NO_ERROR) {
-      jniThrowException(env, "java/lang/RuntimeException", "set histogram mode failed");
-     }
-#endif
  }
 void JNICameraContext::addCallbackBuffer(
         JNIEnv *env, jbyteArray cbb, int msgType)
