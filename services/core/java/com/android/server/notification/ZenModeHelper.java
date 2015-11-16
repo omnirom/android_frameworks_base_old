183edb04c80f3981a99af5ed9f452d22a59c4537
maxwen <max.weninger@gmail.com>
2015-11-17 00:45:25 +0100

    base: silent mode is back
    ringer mode silent for normal mode and priority interruptions
    
    can be enabled/disabled with
    -volume down/up
    -pressing icon in volume panel
    -global actions menu
    
    Changing ringer mode from silent while in other dnd mode
    will stop dnd except via volume up
    
    PS6:
    -add volume down in silent enters dnd
    
    Change-Id: I368e0dbf6770feb3ad3c39417a0014bf7f4c5cba

--- services/core/java/com/android/server/notification/ZenModeHelper.java
+++ services/core/java/com/android/server/notification/ZenModeHelper.java
@@ -616,7 +616,7 @@ public class ZenModeHelper {
                             && (mZenMode == Global.ZEN_MODE_NO_INTERRUPTIONS
                                     || mZenMode == Global.ZEN_MODE_ALARMS)) {
                         newZen = Global.ZEN_MODE_OFF;
-                    } else if (mZenMode != Global.ZEN_MODE_OFF) {
+                    } else if (mZenMode != Global.ZEN_MODE_OFF && mZenMode != Global.ZEN_MODE_IMPORTANT_INTERRUPTIONS) {
                         ringerModeExternalOut = AudioManager.RINGER_MODE_SILENT;
                     }
                     break;
@@ -670,7 +670,26 @@ public class ZenModeHelper {
 
         @Override
         public boolean canVolumeDownEnterSilent() {
-            return mZenMode == Global.ZEN_MODE_OFF;
+            return mZenMode == Global.ZEN_MODE_OFF || mZenMode == Global.ZEN_MODE_IMPORTANT_INTERRUPTIONS;
+        }
+
+        @Override
+        public boolean canVolumeUpExitSilent() {
+            return mZenMode == Global.ZEN_MODE_OFF || mZenMode == Global.ZEN_MODE_IMPORTANT_INTERRUPTIONS;
+        }
+
+        @Override
+        public void onVolumeDownInSilent(VolumePolicy policy) {
+            if (policy.doNotDisturbWhenVolumeDownInSilent) {
+                int newZen = -1;
+                if (mZenMode != Global.ZEN_MODE_NO_INTERRUPTIONS
+                        && mZenMode != Global.ZEN_MODE_ALARMS) {
+                    newZen = Global.ZEN_MODE_ALARMS;
+                }
+                if (newZen != -1) {
+                    setManualZenMode(newZen, null, "onVolumeDownInSilent", false /*setRingerMode*/);
+                }
+            }
         }
 
         @Override
