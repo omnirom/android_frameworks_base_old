aeff7db3e0d65164b98b2ab4856d953e1aa0f205
maxwen <max.weninger@gmail.com>
2015-11-18 21:43:53 +0100

    [1/2] base: unlink notification volume is back
    -delete old unused volume images
    -add new one for notification volumes
    maybe some day one will be able to create a vector
    drawable for this - I am not :)
    
    Change-Id: Id26021832ad1ca5e579878ee2471f7c401bc74ac

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3797,6 +3797,13 @@ public final class Settings {
         public static final String BUTTON_BACKLIGHT_TIMEOUT = "button_backlight_timeout";
 
         /**
+         * Boolean value whether to link ringtone and notification volume
+         *
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
