4c1c94e39d3fb1ae50e42d9d558e55be9a736853
maxwen <max.weninger@gmail.com>
2015-11-28 15:38:42 +0100

    SystemUI: screenrecord: make sure to always delete tmp file
    Change-Id: I2974969fa25a01c9422f13316d188b65db591156

--- packages/SystemUI/src/com/android/systemui/omni/screenrecord/GlobalScreenrecord.java
+++ packages/SystemUI/src/com/android/systemui/omni/screenrecord/GlobalScreenrecord.java
@@ -245,11 +245,13 @@ class GlobalScreenrecord {
 
             try {
                 copyFileUsingStream(input, output);
-                input.delete();
             } catch (IOException e) {
                 Log.e(TAG, "Unable to copy output file", e);
                 Message msg = Message.obtain(mHandler, MSG_TASK_ERROR);
                 mHandler.sendMessage(msg);
+                return;
+            } finally {
+                input.delete();
             }
 
             // Make it appear in gallery, run MediaScanner
