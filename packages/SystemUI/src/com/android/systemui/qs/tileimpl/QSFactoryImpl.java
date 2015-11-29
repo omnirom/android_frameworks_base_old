ba3c53b36fa3008b628ef5638301287cc30b19e8
kufikugel <kufikugel@googlemail.com>
2015-11-29 05:51:19 +0000

    [QUICK SETTINGS] Add Music QS Tile
    picked from here: http://goo.gl/ehaoMU
    
    Adapted to lollipop from commit:
    Author: kufikugel
    Quicksettings customizations Slim style
    d85304fac9bde550beeeeca0dba2cd49686f20d7
    
    Adapted double click to open music app from commit:
    Author: Jubakuba
    SystemUI: Pimp-up Media Tile (1/1)
    5a14de5db237969607e0aabb9b8c04737b122dd3
    
    AICPfy:
    PS1: Gave proper credit to kufikugel for the original code
    PS2: Fix build by overriding all needed methids
    
    Change-Id: I4d3008d3dcfdf56f1cef25a602a153264c00c80f
    
    [QUICK SETTINGS] Switch to vector drawables for Play Tile
    
    Thanks to dully79
    http://forum.xda-developers.com/member.php?u=2538774
    
    Change-Id: Ib79a9efd968bace4c0cd845324619f227fdd01e5

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -37,6 +37,7 @@ import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
+import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
@@ -105,6 +106,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new AODTile(mHost);
             case "caffeine":
                 return new CaffeineTile(mHost);
+            case "music":
+                return new MusicTile(mHost);
         }
 
         // Intent tiles.
