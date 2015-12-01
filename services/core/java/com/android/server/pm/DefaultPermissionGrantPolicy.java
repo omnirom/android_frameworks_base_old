34dcdf1c7c9741e22b55e3d60ba68dc59c343650
Menno van Grinsven <scanno71@gmail.com>
2015-12-01 17:26:41 +0100

    base: Add permissions to Browser package
    Includes:
    * CONTACTS_PERMISSIONS
    * STORAGE_PERMISSIONS
    Change-Id: I29cdf512888a38aba28ea2cfeb6b56352fe3a589

--- services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
@@ -493,6 +493,8 @@ final class DefaultPermissionGrantPolicy {
             if (browserPackage != null
                     && doesPackageSupportRuntimePermissions(browserPackage)) {
                 grantRuntimePermissionsLPw(browserPackage, LOCATION_PERMISSIONS, userId);
+                grantRuntimePermissionsLPw(browserPackage, CONTACTS_PERMISSIONS, userId);
+                grantRuntimePermissionsLPw(browserPackage, STORAGE_PERMISSIONS, userId);
             }
 
             // IME
@@ -668,6 +670,8 @@ final class DefaultPermissionGrantPolicy {
         if (browserPackage != null
                 && doesPackageSupportRuntimePermissions(browserPackage)) {
             grantRuntimePermissionsLPw(browserPackage, LOCATION_PERMISSIONS, false, false, userId);
+            grantRuntimePermissionsLPw(browserPackage, CONTACTS_PERMISSIONS, false, false, userId);
+            grantRuntimePermissionsLPw(browserPackage, STORAGE_PERMISSIONS, false, false, userId);
         }
     }
 
