379c3f419efc6a2eef098a9f5406ccf55c96b28b
maxwen <max.weninger@gmail.com>
2016-01-03 11:52:48 +0530

    Add auto brightness toggle to Quick Settings
    - Not a new tile, just a small icon/toggle beside the brightness slider
    
    Change-Id: I9632532aa3558911e6e7c8718feddc83e7bcfac6
    
    make the icon a bit smaller
    and white when its on Auto
    
    Change-Id: Id60779b50e9d95ddd2807a1525ad924f20c85448

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -86,6 +86,8 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
 
         mBrightnessView = LayoutInflater.from(context).inflate(
                 R.layout.quick_settings_brightness_dialog, this, false);
+        ImageView brightnessIcon = (ImageView) mBrightnessView.findViewById(R.id.brightness_icon);
+        brightnessIcon.setVisibility(View.VISIBLE);
         addView(mBrightnessView);
 
         setupTileLayout();
@@ -96,7 +98,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
         updateResources();
 
         mBrightnessController = new BrightnessController(getContext(),
-                (ImageView) findViewById(R.id.brightness_icon),
+                brightnessIcon,
                 (ToggleSlider) findViewById(R.id.brightness_slider));
 
     }
