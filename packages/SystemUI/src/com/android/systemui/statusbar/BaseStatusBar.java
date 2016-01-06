48cfcabd62f6c5ece1eb8ea1ab52a04036db3ff8
maxwen <max.weninger@gmail.com>
2016-01-06 17:24:51 +0100

    [1/2] base: allow config to hide app notifications when locked
    add another filter level to hide all app notifications when locked
    for fine grain config what notifications should be shown when locked
    
    Change-Id: I4c7b2d0adec83f5371d0969a30881d854e13c358

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -21,6 +21,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.TimeInterpolator;
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
+import android.app.INotificationManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -236,6 +237,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     private NotificationClicker mNotificationClicker = new NotificationClicker();
 
     protected AssistManager mAssistManager;
+    private INotificationManager mNoMan;
 
     @Override  // NotificationData.Environment
     public boolean isDeviceProvisioned() {
@@ -550,6 +552,8 @@ public abstract class BaseStatusBar extends SystemUI implements
         mDreamManager = IDreamManager.Stub.asInterface(
                 ServiceManager.checkService(DreamService.DREAM_SERVICE));
         mPowerManager = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+        mNoMan = (INotificationManager) INotificationManager.Stub.asInterface(
+                ServiceManager.getService(Context.NOTIFICATION_SERVICE));
 
         mContext.getContentResolver().registerContentObserver(
                 Settings.Global.getUriFor(Settings.Global.DEVICE_PROVISIONED), true,
@@ -1842,7 +1846,12 @@ public abstract class BaseStatusBar extends SystemUI implements
     }
 
     private boolean shouldShowOnKeyguard(StatusBarNotification sbn) {
-        return mShowLockscreenNotifications && !mNotificationData.isAmbient(sbn.getKey());
+        boolean keyguard = true;
+        try {
+            keyguard = mNoMan.getPackageKeyguard(sbn.getPackageName(), sbn.getUid());
+        } catch (RemoteException e) {
+        }
+        return mShowLockscreenNotifications && !mNotificationData.isAmbient(sbn.getKey()) && keyguard;
     }
 
     protected void setZenMode(int mode) {
