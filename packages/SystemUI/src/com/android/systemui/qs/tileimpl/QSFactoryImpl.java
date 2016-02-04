1ea04d3a69e3d3094882c3221bc1bbee57178765
Keith Mok <kmok@cyngn.com>
2016-02-04 11:10:22 -0800

    SystemUI: Add caffeine QS tile
    Author: Keith Mok <kmok@cyngn.com>
    Date:   Thu Feb 4 11:10:22 2016 -0800
    
        SystemUI: Add caffeine qs tile
    
        Change-Id: I5d4aa97ae63fc0bcc40f0fa7d9305efbba091029
    
    Author: chillger <alexander.loskutov@gmail.com>
    Date:   Fri May 22 13:16:00 2020 +0200
    
        SystemUI: Long press Caffeine for infinite duration
    
        Change-Id: Ic49539dcda6d7d6b1dab362eb8ec168d2ec65b07
    
    Change-Id: I07173f5fc20142390b42d14e3bc03ebd14646d6a
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use mHost file
  * except in compliance with the License. You may obtain a copy of the License at
@@ -30,6 +31,7 @@ import com.android.systemui.qs.tiles.AlarmTile;
 import com.android.systemui.qs.tiles.AODTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
+import com.android.systemui.qs.tiles.CaffeineTile;
 import com.android.systemui.qs.tiles.CameraToggleTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
@@ -96,6 +98,7 @@ public class QSFactoryImpl implements QSFactory {
     // Omni
     private final Provider<AODTile> mAODTileProvider;
     private final Provider<ScreenshotTile> mScreenshotTileProvider;
+    private final Provider<CaffeineTile> mCaffeineTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
     private final Provider<CustomTile.Builder> mCustomTileBuilderProvider;
@@ -132,7 +135,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<AlarmTile> alarmTileProvider,
             Provider<QuickAccessWalletTile> quickAccessWalletTileProvider,
             Provider<AODTile> aodTileProvider,
-            Provider<ScreenshotTile> screenshotTileProvider) {
+            Provider<ScreenshotTile> screenshotTileProvider,
+            Provider<CaffeineTile> caffeineTileProvider) {
         mQsHostLazy = qsHostLazy;
         mCustomTileBuilderProvider = customTileBuilderProvider;
 
@@ -167,6 +171,7 @@ public class QSFactoryImpl implements QSFactory {
         //Omni
         mAODTileProvider = aodTileProvider;
         mScreenshotTileProvider = screenshotTileProvider;
+        mCaffeineTileProvider = caffeineTileProvider;
     }
 
     public QSTile createTile(String tileSpec) {
@@ -238,6 +243,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mAODTileProvider.get();
             case "screenshot":
                 return mScreenshotTileProvider.get();
+            case "caffeine":
+                return mCaffeineTileProvider.get();
         }
 
         // Custom tiles
