9625e1205e8a4e06373b600c3abdaf811ac4534b
maxwen <max.weninger@gmail.com>
2016-02-13 00:49:39 +0100

    [1/3] base: power menu config
    to configure what power menu actions are shown
    
    PS6:
    -no white bears in the snow please
    
    Change-Id: I5fc8ca1e588fde846700eff1209dedd8f82db7a6
    
    [1/2]Base: Add Screenshot and Screencapture to Power Menu
    
    Change-Id: I4ff5cb4825d147131940480ca33935f7b15de566
    
    [1/2] Base: Add Torch to Power Menu
    
    PS7:
    Add propper torch state handling
    
    PS8:
    No OFFf
    
    PS10:
    its Flashlight in QS so use the same
    
    Change-Id: Ic3adf0d762d0af37e0894cce19840cda0d14d1c8
    
    base: power menu: add 1s delay to screenshot action
    
    Change-Id: I384c0372e3aa6c8b8c30e5e4a99955a827341757

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3608,6 +3608,11 @@ public final class Settings {
         public static final String NAVIGATION_BAR_SHOW = "navigation_bar_show";
 
         /**
+         * @hide
+         */
+        public static final String GLOBAL_ACTIONS_LIST = "global_actions_list";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
