56d91a98c2d73031ef61c9a31c91c9baf2375617
maxwen <max.weninger@windriver.com>
2016-02-18 16:26:16 +0100

    WIP: lock screen config
    Change-Id: Ib999fde8ce207d4a1adb4bc54d892eac8316f04e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -111,6 +111,7 @@ import com.android.internal.statusbar.StatusBarIcon;
 import com.android.internal.util.omni.TaskUtils;
 import com.android.internal.util.omni.OmniSwitchConstants;
 import com.android.keyguard.KeyguardHostView.OnDismissAction;
+import com.android.keyguard.KeyguardStatusView;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
 import com.android.keyguard.ViewMediatorCallback;
@@ -326,7 +327,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     // top bar
     StatusBarHeaderView mHeader;
     KeyguardStatusBarView mKeyguardStatusBar;
-    View mKeyguardStatusView;
+    KeyguardStatusView mKeyguardStatusView;
     KeyguardBottomAreaView mKeyguardBottomArea;
     boolean mLeaveOpenOnKeyguardHide;
     KeyguardIndicationController mKeyguardIndicationController;
@@ -512,6 +513,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_CUSTOM_HEADER_SHADOW),
                     false, this, UserHandle.USER_ALL);
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCK_CLOCK_COLOR),
+                    false, this, UserHandle.USER_ALL);
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCK_CLOCK_SIZE),
+                    false, this, UserHandle.USER_ALL);
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCK_CLOCK_FONT),
+                    false, this, UserHandle.USER_ALL);
 
             update();
         }
@@ -569,7 +579,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
 
             if (mHeader != null) {
-                mHeader.settingsChanged();
+                mHeader.updateSettings();
             }
 
             if (mQSPanel != null) {
@@ -580,6 +590,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 final int qsAlphaValue = Settings.System.getIntForUser(
                         mContext.getContentResolver(), Settings.System.QS_TILE_BG_OPACITY, 255, mCurrentUserId);
                 mNotificationPanel.setQSBackgroundAlpha(qsAlphaValue);
+            }
+
+            if (mKeyguardStatusView != null) {
+                mKeyguardStatusView.updateSettings();
             }
         }
     }
@@ -995,7 +1009,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mHeader = (StatusBarHeaderView) mStatusBarWindow.findViewById(R.id.header);
         mHeader.setActivityStarter(this);
         mKeyguardStatusBar = (KeyguardStatusBarView) mStatusBarWindow.findViewById(R.id.keyguard_header);
-        mKeyguardStatusView = mStatusBarWindow.findViewById(R.id.keyguard_status_view);
+        mKeyguardStatusView = (KeyguardStatusView) mStatusBarWindow.findViewById(R.id.keyguard_status_view);
         mKeyguardBottomArea =
                 (KeyguardBottomAreaView) mStatusBarWindow.findViewById(R.id.keyguard_bottom_area);
         mKeyguardBottomArea.setActivityStarter(this);
