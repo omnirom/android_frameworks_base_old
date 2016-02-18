43b135ec501661fbcefef27b329ae169b0fe7454
maxwen <max.weninger@gmail.com>
2016-02-19 00:14:15 +0100

    [1/2] base: add config to apply custom shadow on header images
    To make text and status icons better visible with bright
    images if required.
    
    Change-Id: I8a0c601316a24f3c89bcdee20c2885d248d5b4b5

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -509,6 +509,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QS_TILE_BG_OPACITY),
                     false, this, UserHandle.USER_ALL);
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_CUSTOM_HEADER_SHADOW),
+                    false, this, UserHandle.USER_ALL);
 
             update();
         }
