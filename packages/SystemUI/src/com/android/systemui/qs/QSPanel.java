cb4f43e0aea28278564146a45b5a54f57906a93f
maxwen <max.weninger@gmail.com>
2016-02-19 17:04:13 +0100

    base: remove unneded top margin in QS tuner
    Change-Id: I18f0fe04bad0958301285b7f4063fe3a4afffe39

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -545,8 +545,9 @@ public class QSPanel extends ViewGroup {
     }
 
     private int getRowTop(int row) {
-        if (row <= 0) return mBrightnessView.getMeasuredHeight() + mBrightnessPaddingTop;
-        return mBrightnessView.getMeasuredHeight() + mBrightnessPaddingTop
+        if (row <= 0)
+            return (mBrightnessView.getVisibility() != GONE ? mBrightnessView.getMeasuredHeight() : 0) + mBrightnessPaddingTop;
+        return (mBrightnessView.getVisibility() != GONE ? mBrightnessView.getMeasuredHeight() : 0) + mBrightnessPaddingTop
                 + mLargeCellHeight - mDualTileUnderlap + (row - 1) * mCellHeight;
     }
 
