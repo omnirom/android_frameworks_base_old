037617601df9a69f6aa372f433b5d58754458676
LorDClockaN <davor@losinj.com>
2016-03-02 18:53:23 -0500

    Powermenu Torch option [1/2]
    - Ported up by @Mazda-- from Lollipop
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/fd7707194c9fb760233759c69ec7966b62cdee9c
    
    - Use CameraManager instead of TorchManager since we no longer use CM's torch fixes/implementation
    
    - Adopted to OmniROM's way of doing things with the power menu
    
    Credit to @beanstown106 for his commit here
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/49e3cd541f68b00c04b6dda03341c09ba9ba57f9
    
    Change-Id: Ia837d353082ed96199c784a764f8d9d772ec8281

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -49,6 +49,9 @@ import android.graphics.RectF;
 import android.graphics.Shader;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraAccessException;
 import android.media.AudioManager;
 import android.net.ConnectivityManager;
 import android.os.Build;
@@ -126,6 +129,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
     private static final String GLOBAL_ACTION_KEY_DND = "dnd";
     private static final String GLOBAL_ACTION_KEY_SCREENSHOT = "screenshot";
     private static final String GLOBAL_ACTION_KEY_SCREENRECORD = "screenrecord";
+    private static final String GLOBAL_ACTION_KEY_TORCH = "torch";
 
     private final Context mContext;
     private final WindowManagerFuncs mWindowManagerFuncs;
@@ -155,6 +159,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
     private String[] mCurrentMenuActions;
     private boolean mRebootMenu;
     private boolean mUsersMenu;
+
+    private static boolean mTorchEnabled = false;
 
     /**
      * @param context everything needs a context :(
@@ -408,6 +414,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 mItems.add(new ScreenShotAction());
             } else if (GLOBAL_ACTION_KEY_SCREENRECORD.equals(actionKey)) {
                 mItems.add(new ScreenRecordAction());
+            } else if (GLOBAL_ACTION_KEY_TORCH.equals(actionKey)) {
+                mItems.add(getTorchAction());
             } else {
                 Log.e(TAG, "Invalid global action key " + actionKey);
             }
@@ -617,6 +625,49 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 mAdapter.notifyDataSetChanged();
             }
         }
+    }
+
+    private Action getTorchAction() {
+        return new SinglePressAction(com.android.internal.R.drawable.ic_global_torch,
+                R.string.global_action_torch) {
+            @Override
+            public void onPress() {
+                try {
+                    CameraManager cameraManager = (CameraManager)
+                            mContext.getSystemService(Context.CAMERA_SERVICE);
+                    for (final String cameraId : cameraManager.getCameraIdList()) {
+                        CameraCharacteristics characteristics =
+                            cameraManager.getCameraCharacteristics(cameraId);
+                        int orient = characteristics.get(CameraCharacteristics.LENS_FACING);
+                        if (orient == CameraCharacteristics.LENS_FACING_BACK) {
+                            cameraManager.setTorchMode(cameraId, !mTorchEnabled);
+                            mTorchEnabled = !mTorchEnabled;
+                        }
+                    }
+                } catch (CameraAccessException e) {
+                }
+            }
+
+            @Override
+            public boolean showDuringKeyguard() {
+                return false;
+            }
+
+            @Override
+            public boolean showDuringRestrictedKeyguard() {
+                return false;
+            }
+
+            @Override
+            public boolean showBeforeProvisioning() {
+                return false;
+            }
+
+            @Override
+            public boolean showForCurrentUser() {
+                return true;
+            }
+        };
     }
 
     private Action getBugReportAction() {
