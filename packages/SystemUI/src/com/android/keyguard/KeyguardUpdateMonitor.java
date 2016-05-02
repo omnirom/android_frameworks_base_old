97572df544cdbde5b0248235ac361e0cb20fc2ca
Steve Kondik <steve@cyngn.com>
2016-05-02 04:01:43 -0700

    keyguard: Allow disabling fingerprint wake-and-unlock
     * When the fingerprint sensor is embedded in the power key,
       wake-and-unlock is total chaos. Add an option to disable it.
     * The default behavior is unchanged.
    
    Change-Id: I50c0a857daba92c17470d8089aca94099c792956

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -1121,9 +1121,16 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
     }
 
     private boolean shouldListenForFingerprint() {
-        return (mKeyguardIsVisible || !mDeviceInteractive ||
-                    (mBouncer && !mKeyguardGoingAway) || mGoingToSleep)
-                && !mSwitchingUser && !isFingerprintDisabled(getCurrentUser());
+        if (!mSwitchingUser && !isFingerprintDisabled(getCurrentUser())) {
+            if (mContext.getResources().getBoolean(
+                    com.android.keyguard.R.bool.config_fingerprintWakeAndUnlock)) {
+                return (mKeyguardIsVisible || !mDeviceInteractive || 
+                            (mBouncer && !mKeyguardGoingAway) || mGoingToSleep);
+            } else {
+                return mDeviceInteractive && (mKeyguardIsVisible || (mBouncer && !mKeyguardGoingAway));
+            }
+        }
+        return false;
     }
 
     private void startListeningForFingerprint() {
