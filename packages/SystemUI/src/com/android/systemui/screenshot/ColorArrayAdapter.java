3983ef2da0b336b3104cb6dbd2600391909f918d
Daniel Huber <daniel.huber.business@gmail.com>
2016-05-29 21:49:37 +0200

    [1/2] base: Screenshot Crop & Share
    Change-Id: Iedec3d89362612a60a57b07884cae9de3466d1a9

---
+++ packages/SystemUI/src/com/android/systemui/screenshot/ColorArrayAdapter.java
@@ -0,0 +1,49 @@
+package com.android.systemui.screenshot;
+
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.TextView;
+
+import com.android.systemui.R;
+
+/**
+ * Created by DanielHuber on 16.04.2016.
+ */
+public class ColorArrayAdapter extends ArrayAdapter<Integer> {
+    private Integer[] colors;
+
+    public ColorArrayAdapter(Context context, Integer[] colors) {
+        super(context, R.layout.list_item, colors);
+        this.colors = colors;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return createColorImage(position);
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        return  createColorImage(position);
+    }
+
+    private ImageView createColorImage(int position){
+        ImageView imageView = new ImageView(getContext());
+        imageView.setBackgroundColor(colors[position]);
+        int width = getContext().getResources().getDimensionPixelSize(R.dimen.crop_buttons);
+        AbsListView.LayoutParams params = new AbsListView.LayoutParams(width, width);
+        imageView.setLayoutParams(params);
+        return imageView;
+    }
+}
