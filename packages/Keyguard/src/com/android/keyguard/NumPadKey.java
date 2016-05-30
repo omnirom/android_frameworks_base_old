4be8930f402531b9f74e9e3128c2171f1dc85384
maxwen <max.weninger@gmail.com>
2016-05-31 00:31:10 +0200

    Keyguard: fix disappearing letters on PIN scramble
    Make sure visibility is reset to VISIBLE
    OMNI-1309
    
    Change-Id: Iba5ce4c5d8847b5dbc2325fad825d9469b2fba51

--- packages/Keyguard/src/com/android/keyguard/NumPadKey.java
+++ packages/Keyguard/src/com/android/keyguard/NumPadKey.java
@@ -124,6 +124,7 @@ public class NumPadKey extends ViewGroup {
                 String klondike = sKlondike[mDigit];
                 final int len = klondike.length();
                 if (len > 0) {
+                    mKlondikeText.setVisibility(View.VISIBLE);
                     mKlondikeText.setText(klondike);
                 } else {
                     mKlondikeText.setVisibility(View.INVISIBLE);
