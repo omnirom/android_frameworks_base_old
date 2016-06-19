962c36f1992b647fa45bc1c19e6a2512106dd59d
Nicholas Chum <nicholaschum@gmail.com>
2016-06-19 10:37:13 -0400

    OMS: Persistence on boot through OverlayManagerServiceImpl [2/6]
    Since OMS patchset 1, the file has been moved and most method
    calls have changed. Update this based on the latest OMS patchset
    3 code.
    
    Squashed:
    OMS: We can't delay the inevitable, it must go missing!
    There is a built in function called onOverlayPackageUpgraded of
    which converts STATE_NOT_APPROVED_MISSING_TARGET to
    STATE_APPROVED_ENABLED.
    
    Author nicholaschum
    https://github.com/nicholaschum/platform_frameworks_base/commit/c424e60a09326a0667f349adf76599bf58e2718e
    
    Change-Id: Id08e00f7daa85f330de687896a5b5d2cd2b3a7c1

--- services/core/java/com/android/server/om/OverlayManagerServiceImpl.java
+++ services/core/java/com/android/server/om/OverlayManagerServiceImpl.java
@@ -391,34 +391,45 @@ class OverlayManagerServiceImpl {
 
     private int calculateNewState(PackageInfo targetPackage, @NonNull PackageInfo overlayPackage,
             int userId) throws OverlayManagerDatabase.BadKeyException {
+
+        // STATE 0 CHECK: Check if the overlay package is disabled by PackageManager
         if (!overlayPackage.applicationInfo.enabled) {
             return STATE_NOT_APPROVED_COMPONENT_DISABLED;
         }
 
+        // OVERLAY STATE CHECK: Check the current overlay's activation
+        boolean stateCheck = mDatabase.getEnabled(overlayPackage.packageName, userId);
+
+        // STATE 1 CHECK: Check if the overlay's target package is missing from the device
         if (targetPackage == null) {
             return STATE_NOT_APPROVED_MISSING_TARGET;
         }
 
+        // STATE 2 CHECK: Check if the overlay has an existing idmap file created. Perhaps
+        // there were no matching resources between the two packages? (Overlay & Target)
         if (!mIdmapManager.idmapExists(overlayPackage, userId)) {
             return STATE_NOT_APPROVED_NO_IDMAP;
         }
 
-        boolean enableIfApproved = mDatabase.getEnabled(overlayPackage.packageName, userId);
-
-        if (mPackageManager.signaturesMatching(targetPackage.packageName,
-                    overlayPackage.packageName, userId)) {
-            return enableIfApproved ? STATE_APPROVED_ENABLED : STATE_APPROVED_DISABLED;
-        }
-
+        // STATE 6 CHECK: System Overlays, also known as RRO overlay files, work the same
+        // as OMS, but with enable/disable limitations. A system overlay resides in the
+        // directory "/vendor/overlay" depending on your device.
+        //
+        // Team Substratum: Disable this as this is a security vulnerability and a
+        // memory-limited partition.
         if ((overlayPackage.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0) {
-            return enableIfApproved ? STATE_APPROVED_ENABLED : STATE_APPROVED_DISABLED;
+            return STATE_NOT_APPROVED_COMPONENT_DISABLED;
         }
 
+        // STATE 3 CHECK: If the overlay only modifies resources explicitly granted by the
+        // target, we approve it.
+        //
+        // Team Substratum: Always approve dangerous packages but disabled state
         if (!mIdmapManager.isDangerous(overlayPackage, userId)) {
-            return enableIfApproved ? STATE_APPROVED_ENABLED : STATE_APPROVED_DISABLED;
+            return STATE_APPROVED_DISABLED;
         }
 
-        return STATE_NOT_APPROVED_DANGEROUS_OVERLAY;
+        return stateCheck ? STATE_APPROVED_ENABLED : STATE_APPROVED_DISABLED;
     }
 
     private void removeIdmapIfPossible(OverlayInfo oi) {
