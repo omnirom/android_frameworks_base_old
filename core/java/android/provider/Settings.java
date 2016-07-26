093fac36f05b7b85ab7df837c41694392a5bc4a7
maxwen <max.weninger@gmail.com>
2016-07-27 01:46:31 +0200

    base: add support to translate buttons in DeviceHandler
    this can be used to implement button remappings
    in device settings e.g. swap back and recents on op3
    
    Change-Id: I6671fa13d827fc401614445db0449180baff86b4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3919,6 +3919,11 @@ public final class Settings {
         public static final String BUTTON_EXTRA_KEY_MAPPING = "button_extra_mapping";
 
         /**
+         * @hide
+         */
+        public static final String BUTTON_SWAP_BACK_RECENTS = "button_swap_back_recents";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
