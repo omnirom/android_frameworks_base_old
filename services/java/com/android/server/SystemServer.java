6411910eebd9bc0071e732e75f6bab1e5201748b
darkobas <darkobas@gmail.com>
2016-08-30 20:07:35 +0200

    Revert "DO NOT MERGE: Remove DayNight theme"
    This reverts commit 15d48a16f645509cb0c6a1f3abf52ddd233cd8b5.
    
    Change-Id: I0e9d099888573c91b536d94d5af1bce996201219

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -389,7 +389,7 @@ public final class SystemServer {
     private void createSystemContext() {
         ActivityThread activityThread = ActivityThread.systemMain();
         mSystemContext = activityThread.getSystemContext();
-        mSystemContext.setTheme(DEFAULT_SYSTEM_THEME);
+        mSystemContext.setTheme(android.R.style.Theme_Material_DayNight_DarkActionBar);
     }
 
     /**
