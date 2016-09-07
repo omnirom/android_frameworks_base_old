815f5d10608f869bc0ac0027ec09ffe5ae8b8feb
Marko Man <darkobas@gmail.com>
2016-09-07 12:04:24 +0200

    base: add back omni utils
    Change-Id: I2e4143c0ac1ad75312d83a2a544d575f944ce74c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3678,6 +3678,12 @@ public final class Settings {
         public static final String SHOW_ALARM_FULLSCREEN = "show_alarm_fullscreen";
 
         /**
+         * Show onscreen keys for hw button devices
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_SHOW = "navigation_bar_show";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
