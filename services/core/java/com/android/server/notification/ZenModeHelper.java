bddbbe08013607f4ee47b1180572ad519f09f90f
Sam <sam3000>
2016-09-07 11:48:26 +0200

    [1/3] frameworks/base: Notification Lights
    taken from cm: 53ab0dc9a8c195aa3b51d8ae19fbcd0f84537f89
    
    Change-Id: Id8798ced0d39f0826b34d9becb12aaf7e47dbcdd
    
    Lights with Screen On [2/2]: Optional allowment of lights
    Implement a setting allowing lights to be activated
    for new notifications even if the screen is on.
    
    Lights with screen on and Custom values are separated
    in an advanced section for a cleaner overview.
    
    This setting gives the user an oportunity to activate lights
    with the screen on and also during DayDream screensaver.
    The option is not activated by default.
    
    also:
    
    framework: add config_ledCanPulse overlay setting
    Some devices do not support pulsing LEDs. Allow them to hide pulse
    settings.
    
    by @romanbb
    
    Change-Id: I812812e94c5c6c4b9614e06c1cb7005ccfc2a625

--- services/core/java/com/android/server/notification/ZenModeHelper.java
+++ services/core/java/com/android/server/notification/ZenModeHelper.java
@@ -48,6 +48,7 @@ import android.os.Message;
 import android.os.Process;
 import android.os.SystemClock;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.provider.Settings.Global;
 import android.service.notification.ConditionProviderService;
 import android.service.notification.ZenModeConfig;
@@ -104,6 +105,7 @@ public class ZenModeHelper {
     private AudioManagerInternal mAudioManager;
     private PackageManager mPm;
     private long mSuppressedEffects;
+    private boolean mAllowLights;
 
     public static final long SUPPRESSED_EFFECT_NOTIFICATIONS = 1;
     public static final long SUPPRESSED_EFFECT_CALLS = 1 << 1;
@@ -721,6 +723,15 @@ public class ZenModeHelper {
         }
     }
 
+    public boolean getAreLightsAllowed() {
+        return mAllowLights;
+    }
+
+    public void readLightsAllowedModeFromSetting() {
+        mAllowLights = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.ALLOW_LIGHTS, 1, UserHandle.USER_CURRENT) == 1;
+    }
+
     private void applyRestrictions() {
         final boolean zen = mZenMode != Global.ZEN_MODE_OFF;
 
@@ -1041,6 +1052,7 @@ public class ZenModeHelper {
 
     private final class SettingsObserver extends ContentObserver {
         private final Uri ZEN_MODE = Global.getUriFor(Global.ZEN_MODE);
+        private final Uri ALLOW_LIGHTS = Settings.System.getUriFor(Settings.System.ALLOW_LIGHTS);
 
         public SettingsObserver(Handler handler) {
             super(handler);
@@ -1049,6 +1061,7 @@ public class ZenModeHelper {
         public void observe() {
             final ContentResolver resolver = mContext.getContentResolver();
             resolver.registerContentObserver(ZEN_MODE, false /*notifyForDescendents*/, this);
+            resolver.registerContentObserver(ALLOW_LIGHTS, false /*notifyForDescendents*/, this);
             update(null);
         }
 
@@ -1063,6 +1076,8 @@ public class ZenModeHelper {
                     if (DEBUG) Log.d(TAG, "Fixing zen mode setting");
                     setZenModeSetting(mZenMode);
                 }
+            } else if (ALLOW_LIGHTS.equals(uri)) {
+                readLightsAllowedModeFromSetting();
             }
         }
     }
