50a1dcc28305221c93b3b50ec8db3748b29bcef3
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2016-10-01 11:16:36 -0400

    Dialer Lookup [1/2]
    Squashed commit of the following:
    
    Author: Xiao-Long Chen <chenxiaolong@cxl.epac.to>
    Date:   Fri Mar 7 17:11:02 2014 -0500
    
        Add people lookup settings keys.
    
    Author: Xiao-Long Chen <chenxiaolong@cxl.epac.to>
    Date:   Fri Jan 31 11:43:10 2014 -0500
    
        Frameworks: Add settings keys for forward and reverse phone number
        lookup
    
    Author: Xiao-Long Chen <chenxiaolong@cxl.epac.to>
    Date:   Sat Apr 12 15:26:29 2014 -0400
    
        Add OpenCnam paid account settings keys
    
    Change-Id: I7291fd6138c24bf42d7fe019016826a3ae1b546c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4695,6 +4695,62 @@ public final class Settings {
         public static final String[] LEGACY_RESTORE_SETTINGS = {
         };
 
+       /**
+         * Enable looking up of phone numbers of nearby places
+         *
+         * @hide
+         */
+        public static final String ENABLE_FORWARD_LOOKUP = "enable_forward_lookup";
+
+        /**
+         * Enable looking up of phone numbers of people
+         *
+         * @hide
+         */
+        public static final String ENABLE_PEOPLE_LOOKUP = "enable_people_lookup";
+
+        /**
+         * Enable looking up of information of phone numbers not in the contacts
+         *
+         * @hide
+         */
+        public static final String ENABLE_REVERSE_LOOKUP = "enable_reverse_lookup";
+
+        /**
+         * The forward lookup provider
+         *
+         * @hide
+         */
+        public static final String FORWARD_LOOKUP_PROVIDER = "forward_lookup_provider";
+
+        /**
+         * The people lookup provider
+         *
+         * @hide
+         */
+        public static final String PEOPLE_LOOKUP_PROVIDER = "people_lookup_provider";
+
+        /**
+         * The reverse lookup provider
+         *
+         * @hide
+         */
+        public static final String REVERSE_LOOKUP_PROVIDER = "reverse_lookup_provider";
+
+        /**
+         * The OpenCNAM paid account ID
+         *
+         * @hide
+         */
+        public static final String DIALER_OPENCNAM_ACCOUNT_SID = "dialer_opencnam_account_sid";
+
+        /**
+         * The OpenCNAM authentication token
+         *
+         * @hide
+         */
+        public static final String DIALER_OPENCNAM_AUTH_TOKEN = "dialer_opencnam_auth_token";
+
         /**
          * These are all public system settings
          *
@@ -4882,6 +4938,14 @@ public final class Settings {
             PRIVATE_SETTINGS.add(FONT_INFO);
             PRIVATE_SETTINGS.add(FONT_PACKAGES);
             PRIVATE_SETTINGS.add(LONG_PRESS_POWER_TORCH);
+            PRIVATE_SETTINGS.add(ENABLE_FORWARD_LOOKUP);
+            PRIVATE_SETTINGS.add(ENABLE_PEOPLE_LOOKUP);
+            PRIVATE_SETTINGS.add(ENABLE_REVERSE_LOOKUP);
+            PRIVATE_SETTINGS.add(FORWARD_LOOKUP_PROVIDER);
+            PRIVATE_SETTINGS.add(PEOPLE_LOOKUP_PROVIDER);
+            PRIVATE_SETTINGS.add(REVERSE_LOOKUP_PROVIDER);
+            PRIVATE_SETTINGS.add(DIALER_OPENCNAM_ACCOUNT_SID);
+            PRIVATE_SETTINGS.add(DIALER_OPENCNAM_AUTH_TOKEN);
         }
 
         /**
