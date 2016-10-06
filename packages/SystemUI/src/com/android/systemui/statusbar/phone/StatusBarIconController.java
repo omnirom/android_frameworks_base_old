099778b6f7603fddfecf37d580df9da7a5f12a42
ezio84 <brabus84@gmail.com>
2016-10-06 10:56:48 +0200

    base: battery style: enable dark intensity when needed
    Change-Id: I1915f58850aa120b1b7a4828e4a41d541a5e0867

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -527,8 +527,7 @@ public class StatusBarIconController extends StatusBarIconList implements Tunabl
             v.setImageTintList(ColorStateList.valueOf(getTint(mTintArea, v, mIconTint)));
         }
         mSignalCluster.setIconTint(mIconTint, mDarkIntensity, mTintArea);
-        //mBatteryViewManager.setDarkIntensity(
-        //        isInArea(mTintArea, mBatteryViewManager) ? mDarkIntensity : 0);
+        mBatteryViewManager.setDarkIntensity(mDarkIntensity);
         mClock.setTextColor(getTint(mTintArea, mClock, mIconTint));
     }
 
