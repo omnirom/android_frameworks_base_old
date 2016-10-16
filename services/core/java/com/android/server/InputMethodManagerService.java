5b45ab29ee55ea773921b316f85bf4afe412c950
maxwen <max.weninger@gmail.com>
2016-10-17 00:33:40 +0200

    [1/2] base: configure IME switcher bits
    show/hide notification and show/hide navbar button
    
    Change-Id: Ibfa27e25ad86e56f635cab5fa557a8509a959d11

--- services/core/java/com/android/server/InputMethodManagerService.java
+++ services/core/java/com/android/server/InputMethodManagerService.java
@@ -1823,8 +1823,10 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
             // mImeWindowVis should be updated before calling shouldShowImeSwitcherLocked().
             final boolean needsToShowImeSwitcher = shouldShowImeSwitcherLocked(vis);
             if (mStatusBar != null) {
+                final boolean showIMEButton = Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.NAVIGATION_BAR_IME_BUTTON, 1, UserHandle.USER_CURRENT) == 1;
                 mStatusBar.setImeWindowStatus(token, vis, backDisposition,
-                        needsToShowImeSwitcher);
+                        needsToShowImeSwitcher && showIMEButton);
             }
             final InputMethodInfo imi = mMethodMap.get(mCurMethodId);
             if (imi != null && needsToShowImeSwitcher) {
@@ -1839,13 +1841,17 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
                 try {
                     if ((mNotificationManager != null)
                             && !mIWindowManager.hasNavigationBar()) {
-                        if (DEBUG) {
-                            Slog.d(TAG, "--- show notification: label =  " + summary);
+                        final boolean showIMENotification = Settings.System.getIntForUser(mContext.getContentResolver(),
+                                Settings.System.STATUS_BAR_IME_NOTIFICATION, 1, UserHandle.USER_CURRENT) == 1;
+                        if (showIMENotification) {
+                            if (DEBUG) {
+                                Slog.d(TAG, "--- show notification: label =  " + summary);
+                            }
+                            mNotificationManager.notifyAsUser(null,
+                                    com.android.internal.R.string.select_input_method,
+                                    mImeSwitcherNotification.build(), UserHandle.ALL);
+                            mNotificationShown = true;
                         }
-                        mNotificationManager.notifyAsUser(null,
-                                com.android.internal.R.string.select_input_method,
-                                mImeSwitcherNotification.build(), UserHandle.ALL);
-                        mNotificationShown = true;
                     }
                 } catch (RemoteException e) {
                 }
