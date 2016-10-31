433bd434762c956bc21dc8a9292ee19256bf8815
maxwen <max.weninger@gmail.com>
2016-10-31 23:48:50 +0100

    base: SystemUI: make QS columns count configurable
    And add infrastructure to add more settings and
    distribute them to QS panel
    
    PS4
    we must explicitly hide the columns submenu
    
    base: SystemUI: add column count 6
    
    now that we have hide label
    
    base: SystemUI: qs colums menu with checkboxes
    
    base: SystemUI add landscape qs columns config
    
    allow max 9 columns in landscape
    
    Change-Id: I401cca347da1a94f352a9ffcb99f1409ddc4d008

--- packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
@@ -253,5 +253,8 @@ public class QuickQSPanel extends QSPanel {
                         R.id.expand_indicator);
             }
         }
+        @Override
+        public void updateSettings() {
+        }
     }
 }
