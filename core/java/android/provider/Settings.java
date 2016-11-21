993f4c9f4e35405cee4930d72a053a1c30eeaac3
maxwen <max.weninger@gmail.com>
2016-11-21 22:50:13 +0100

    base: SystemUI: all tile scroller for quickbar settings
    allow horizontal scrolling through all tiles also
    in the quickbar
    
    Change-Id: I2a2090d48ff89ac0f23a27851af842d4aac46da1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3867,6 +3867,11 @@ public final class Settings {
         public static final String VOLUME_DIALOG_HEADERS = "volume_dialog_headers";
 
         /**
+         * @hide
+         */
+        public static final String QS_QUICKBAR_SCROLL_ENABLED = "qs_quickbar_scroll_enabled";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
