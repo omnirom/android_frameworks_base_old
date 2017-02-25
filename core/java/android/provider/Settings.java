9a916034013a2ccf0c9a7c77c1ee3ae66d374ef3
maxwen <max.weninger@gmail.com>
2017-02-25 17:31:05 +0100

    [1/2] base: SystemUI: OmniJaws qs tile integration
    Change-Id: I4a00255a362298820f9f7997b3089e135358e0f6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3994,6 +3994,11 @@ public final class Settings {
         public static final String LOCKSCREEN_ROTATION = "lockscreen_rotation";
 
         /**
+         * @hide
+         */
+        public static final String OMNIJAWS_WEATHER_ICON_PACK = "omnijaws_weather_icon_pack";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
