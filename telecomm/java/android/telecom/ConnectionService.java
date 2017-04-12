005299295cffa276eb5078b7934b3296ea882488
Suchand Ghosh <suchan@codeaurora.org>
2017-04-12 14:26:23 +0530

    IMS: Conference URI support.
    Add Telephony extras EXTRAS_IS_CONFERENCE_URI,
    EXTRA_DIAL_CONFERENCE_URI which will require by Telecomm,
    TeleService, Telephony, Framework, IMS framework etc.
    
    IMS: Allow placeCall with complete URI
    
    Add extra "org.codeaurora.extra.SKIP_SCHEMA_PARSING".
    Application need to set the intent extra to dial with
    complete uri.
    
    IMS: Add participant support.
    
    Add Phone Capabilities ADD_PARTICIPANT and
    Telephony Property ADD_PARTICIPANT_KEY.
    
    IMS: Allow add participant with normal IMS call
    
    We should allow add participant with normal IMS
    call to make it conference.
    Send add participant through existing connection
    of normal IMS call.
    
    IMS: Add Participant support.
    
    Do not create new connection while adding participant
    with existing call. Rather send add participant request
    through IMSConference.
    
    IMS: Rectify the capability mask for add participant
    
    Properly set the add participant capability mask.
    
    Change-Id: Ib98ff7cd0c9116a626f97afa17bec4c253293dff

--- telecomm/java/android/telecom/ConnectionService.java
+++ telecomm/java/android/telecom/ConnectionService.java
@@ -183,6 +183,8 @@ public abstract class ConnectionService extends Service {
     private static final int MSG_HANDOVER_FAILED = 32;
     private static final int MSG_HANDOVER_COMPLETE = 33;
     private static final int MSG_DEFLECT = 34;
+    //Proprietary values starts after this.
+    private static final int MSG_ADD_PARTICIPANT_WITH_CONFERENCE = 40;
 
     private static Connection sNullConnection;
 
@@ -514,6 +516,14 @@ public abstract class ConnectionService extends Service {
             } finally {
                 Log.endSession();
             }
+        }
+
+        @Override
+        public void addParticipantWithConference(String callId, String participant) {
+            SomeArgs args = SomeArgs.obtain();
+            args.arg1 = callId;
+            args.arg2 = participant;
+            mHandler.obtainMessage(MSG_ADD_PARTICIPANT_WITH_CONFERENCE, args).sendToTarget();
         }
 
         @Override
@@ -976,6 +986,17 @@ public abstract class ConnectionService extends Service {
                     } finally {
                         args.recycle();
                         Log.endSession();
+                    }
+                    break;
+                }
+                case MSG_ADD_PARTICIPANT_WITH_CONFERENCE: {
+                    SomeArgs args = (SomeArgs) msg.obj;
+                    try {
+                        String callId = (String) args.arg1;
+                        String participant = (String) args.arg2;
+                        addParticipantWithConference(callId, participant);
+                    } finally {
+                        args.recycle();
                     }
                     break;
                 }
@@ -1474,6 +1495,11 @@ public abstract class ConnectionService extends Service {
                 mAdapter.onPhoneAccountChanged(id, pHandle);
             }
         }
+
+        public void onCdmaConnectionTimeReset(Connection c) {
+            String id = mIdByConnection.get(c);
+            mAdapter.resetCdmaConnectionTime(id);
+        }
     };
 
     /** {@inheritDoc} */
@@ -1767,6 +1793,17 @@ public abstract class ConnectionService extends Service {
         Conference conference = connection.getConference();
         if (conference != null) {
             conference.onSeparate(connection);
+        }
+    }
+
+    private void addParticipantWithConference(String callId, String participant) {
+        Log.d(this, "ConnectionService addParticipantWithConference(%s, %s)", participant, callId);
+        Conference conference = findConferenceForAction(callId, "addParticipantWithConference");
+        Connection connection = findConnectionForAction(callId, "addParticipantWithConnection");
+        if (connection != getNullConnection()) {
+            onAddParticipant(connection, participant);
+        } else if (conference != getNullConference()) {
+            conference.onAddParticipant(participant);
         }
     }
 
@@ -2377,6 +2414,18 @@ public abstract class ConnectionService extends Service {
      * @hide
      */
     public void onConferenceRemoved(Conference conference) {}
+
+    /** Add participant with connection. Invoked when user has made a request to add
+     * participant with specified connection. In response, the participant should add with
+     * the connection.
+     *
+     * @param connection A connection where participant need to add.
+     * @param participant Address of participant which will be added.
+     * @return
+     *
+     * @hide
+     */
+    public void onAddParticipant(Connection connection, String participant) {}
 
     /**
      * Indicates that a remote conference has been created for existing {@link RemoteConnection}s.
