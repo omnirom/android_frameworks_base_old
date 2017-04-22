f8fedb1f834c65ef6e33afccc2314814b5c88f39
maxwen <max.weninger@gmail.com>
2017-04-23 01:42:08 +0200

    base: SystemUI: dont hide weather forcast data on errors
    see https://gerrit.omnirom.org/23116 for details
    
    Change-Id: Iaa2fa7b6e0d6638b142b19149c5b647d64f3b2e0

--- packages/SystemUI/src/com/android/systemui/omni/OmniJawsClient.java
+++ packages/SystemUI/src/com/android/systemui/omni/OmniJawsClient.java
@@ -42,7 +42,7 @@ import android.util.Log;
 import com.android.systemui.R;
 
 public class OmniJawsClient {
-    private static final String TAG = "WeatherService:OmniJawsClient";
+    private static final String TAG = "SystemUI:OmniJawsClient";
     private static final boolean DEBUG = false;
     public static final String SERVICE_PACKAGE = "org.omnirom.omnijaws";
     public static final Uri WEATHER_URI
@@ -52,6 +52,8 @@ public class OmniJawsClient {
 
     private static final String ICON_PACKAGE_DEFAULT = "org.omnirom.omnijaws";
     private static final String ICON_PREFIX_DEFAULT = "weather";
+    private static final String EXTRA_ERROR = "error";
+    public static final int EXTRA_ERROR_DISABLED = 2;
 
     public static final String[] WEATHER_PROJECTION = new String[]{
             "city",
@@ -116,7 +118,7 @@ public class OmniJawsClient {
 
     public static interface OmniJawsObserver {
         public void weatherUpdated();
-        public void weatherError();
+        public void weatherError(int errorReason);
     }
 
     private class WeatherUpdateReceiver extends BroadcastReceiver {
@@ -128,7 +130,8 @@ public class OmniJawsClient {
                     observer.weatherUpdated();
                 }
                 if (action.equals(WEATHER_ERROR)) {
-                    observer.weatherError();
+                    int errorReason = intent.getIntExtra(EXTRA_ERROR, 0);
+                    observer.weatherError(errorReason);
                 }
             }
         }
