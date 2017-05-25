eb912f0df1997d9183d809bb819d62de45a77951
maxwen <max.weninger@gmail.com>
2017-05-25 22:37:32 +0200

    [1/2] base: enable power save config of system apps
    allow forcing gms to follow doze restrictions
    
    Change-Id: I3a15b5f836ecfac48745b8daeb45a1252764d070

--- services/core/java/com/android/server/DeviceIdleController.java
+++ services/core/java/com/android/server/DeviceIdleController.java
@@ -1259,6 +1259,28 @@ public class DeviceIdleController extends SystemService
                 FileDescriptor err, String[] args, ResultReceiver resultReceiver) {
             (new Shell()).exec(this, in, out, err, args, resultReceiver);
         }
+
+        @Override public void addSystemPowerSaveWhitelistApp(String name) {
+            getContext().enforceCallingOrSelfPermission(android.Manifest.permission.DEVICE_POWER,
+                    null);
+            long ident = Binder.clearCallingIdentity();
+            try {
+                addSystemPowerSaveWhitelistAppInternal(name);
+            } finally {
+                Binder.restoreCallingIdentity(ident);
+            }
+        }
+
+        @Override public void removeSystemPowerSaveWhitelistApp(String name) {
+            getContext().enforceCallingOrSelfPermission(android.Manifest.permission.DEVICE_POWER,
+                    null);
+            long ident = Binder.clearCallingIdentity();
+            try {
+                removeSystemPowerSaveWhitelistAppInternal(name);
+            } finally {
+                Binder.restoreCallingIdentity(ident);
+            }
+        }
     }
 
     public final class LocalService {
@@ -1472,6 +1494,35 @@ public class DeviceIdleController extends SystemService
     public boolean removePowerSaveWhitelistAppInternal(String name) {
         synchronized (this) {
             if (mPowerSaveWhitelistUserApps.remove(name) != null) {
+                reportPowerSaveWhitelistChangedLocked();
+                updateWhitelistAppIdsLocked();
+                writeConfigFileLocked();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean addSystemPowerSaveWhitelistAppInternal(String name) {
+        synchronized (this) {
+            try {
+                ApplicationInfo ai = getContext().getPackageManager().getApplicationInfo(name,
+                        PackageManager.MATCH_UNINSTALLED_PACKAGES);
+                if (mPowerSaveWhitelistApps.put(name, UserHandle.getAppId(ai.uid)) == null) {
+                    reportPowerSaveWhitelistChangedLocked();
+                    updateWhitelistAppIdsLocked();
+                    writeConfigFileLocked();
+                }
+                return true;
+            } catch (PackageManager.NameNotFoundException e) {
+                return false;
+            }
+        }
+    }
+
+    public boolean removeSystemPowerSaveWhitelistAppInternal(String name) {
+        synchronized (this) {
+            if (mPowerSaveWhitelistApps.remove(name) != null) {
                 reportPowerSaveWhitelistChangedLocked();
                 updateWhitelistAppIdsLocked();
                 writeConfigFileLocked();
@@ -2430,6 +2481,20 @@ public class DeviceIdleController extends SystemService
                         } catch (PackageManager.NameNotFoundException e) {
                         }
                     }
+                } else if (tagName.equals("wls")) {
+                    if (mPowerSaveWhitelistApps.size() != 0) {
+                        mPowerSaveWhitelistApps.clear();
+                    }
+                    String name = parser.getAttributeValue(null, "n");
+                    if (name != null && !name.equals("empty")) {
+                        try {
+                            ApplicationInfo ai = pm.getApplicationInfo(name,
+                                    PackageManager.MATCH_UNINSTALLED_PACKAGES);
+                            mPowerSaveWhitelistApps.put(ai.packageName,
+                                    UserHandle.getAppId(ai.uid));
+                        } catch (PackageManager.NameNotFoundException e) {
+                        }
+                    }
                 } else {
                     Slog.w(TAG, "Unknown element under <config>: "
                             + parser.getName());
@@ -2493,6 +2558,18 @@ public class DeviceIdleController extends SystemService
             out.startTag(null, "wl");
             out.attribute(null, "n", name);
             out.endTag(null, "wl");
+        }
+        if (mPowerSaveWhitelistApps.size() == 0) {
+            out.startTag(null, "wls");
+            out.attribute(null, "n", "empty");
+            out.endTag(null, "wls");
+        } else {
+            for (int i=0; i<mPowerSaveWhitelistApps.size(); i++) {
+                String name = mPowerSaveWhitelistApps.keyAt(i);
+                out.startTag(null, "wls");
+                out.attribute(null, "n", name);
+                out.endTag(null, "wls");
+            }
         }
         out.endTag(null, "config");
         out.endDocument();
