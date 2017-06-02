37cf003aca9dbdc46c2d4911e2083fb2478447de
maxwen <max.weninger@gmail.com>
2017-06-02 03:23:41 +0200

    [1/2] base: OmniSwitch: add preload task broadcast
    Broadcast also the preload phase to OmniSwitch to get
    an early start on loading tasks like stock recents
    
    Add: relying on broadcasts for all of this gets
    worse. It takes a very long time after rebooting
    until broadcasts will be processed leaving
    users without reaction on button presses even
    if the drag handle is already there for some time
    
    Change-Id: I976ee46d4863e7667faaa038b30ced481b934b47

--- core/java/com/android/internal/util/omni/OmniSwitchConstants.java
+++ core/java/com/android/internal/util/omni/OmniSwitchConstants.java
@@ -33,7 +33,12 @@ public class OmniSwitchConstants {
     /**
      * Intent broadcast action for toogle the omniswitch overlay
      */
-    private static final String ACTION_TOGGLE_OVERLAY = APP_PACKAGE_NAME + ".ACTION_TOGGLE_OVERLAY";
+    private static final String ACTION_TOGGLE_OVERLAY2 = APP_PACKAGE_NAME + ".ACTION_TOGGLE_OVERLAY2";
+
+    /**
+     * Intent broadcast action for telling omniswitch to preload tasks
+     */
+    private static final String ACTION_PRELOAD_TASKS = APP_PACKAGE_NAME + ".ACTION_PRELOAD_TASKS";
 
     private static final String ACTION_RESTORE_HOME_STACK = APP_PACKAGE_NAME + ".ACTION_RESTORE_HOME_STACK";
 
@@ -61,12 +66,26 @@ public class OmniSwitchConstants {
      * @hide
      */
     public static void toggleOmniSwitchRecents(Context context, UserHandle user) {
-        final Intent showIntent = new Intent(OmniSwitchConstants.ACTION_TOGGLE_OVERLAY);
-        context.sendBroadcastAsUser(showIntent, user);
+        final Intent intent = new Intent(OmniSwitchConstants.ACTION_TOGGLE_OVERLAY2);
+        intent.setPackage(APP_PACKAGE_NAME);
+        context.sendBroadcastAsUser(intent, user);
     }
 
-    public static void restoreHomeStack(Context context, UserHandle user) {
-        final Intent showIntent = new Intent(OmniSwitchConstants.ACTION_RESTORE_HOME_STACK);
-        context.sendBroadcastAsUser(showIntent, user);
+    /**
+     * @hide
+     */
+     public static void restoreHomeStack(Context context, UserHandle user) {
+        final Intent intent = new Intent(OmniSwitchConstants.ACTION_RESTORE_HOME_STACK);
+        intent.setPackage(APP_PACKAGE_NAME);
+        context.sendBroadcastAsUser(intent, user);
+    }
+
+    /**
+     * @hide
+     */
+     public static void preloadOmniSwitchRecents(Context context, UserHandle user) {
+        final Intent intent = new Intent(OmniSwitchConstants.ACTION_PRELOAD_TASKS);
+        intent.setPackage(APP_PACKAGE_NAME);
+        context.sendBroadcastAsUser(intent, user);
     }
 }
