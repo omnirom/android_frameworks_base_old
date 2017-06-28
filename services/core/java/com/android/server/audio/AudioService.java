eb0e1a7b40b48679cb85a78d4a346c43187311bf
Satheesh Kumar Pallemoni <palsat@codeaurora.org>
2017-06-28 12:56:04 +0530

    AudioService: Add synchronization on SCO clients.
    Added synchronization on setBluetoothScoOn API.
    
    CRs-Fixed: 2061903
    Change-Id: Ia9df2420ee5d2d5539ce916d42f98f19d9e62e71

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -2707,10 +2707,12 @@ public class AudioService extends IAudioService.Stub {
     public void setBluetoothScoOnInt(boolean on) {
         if (on) {
             mForcedUseForComm = AudioSystem.FORCE_BT_SCO;
-            if ((mBluetoothHeadset != null) &&
-                (mBluetoothHeadset.getAudioState(mBluetoothHeadsetDevice)
-                             != BluetoothHeadset.STATE_AUDIO_CONNECTED)) {
-                return;
+            synchronized(mScoClients) {
+                if ((mBluetoothHeadset != null) &&
+                    (mBluetoothHeadset.getAudioState(mBluetoothHeadsetDevice)
+                                 != BluetoothHeadset.STATE_AUDIO_CONNECTED)) {
+                    return;
+                }
             }
             AudioSystem.setParameters("BT_SCO=on");
         } else if (mForcedUseForComm == AudioSystem.FORCE_BT_SCO) {
