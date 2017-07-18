a474e004554e248771f94a0f3d7c598ed81070f0
maxwen <max.weninger@gmail.com>
2017-07-18 20:34:37 +0200

    base: SystemUI: add OmniSwitch hide on home
    and fix a minor bug with status bar logo visibility
    
    Change-Id: If21bccc3c6f8116a21508a2634c3ece25c100311

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -3562,6 +3562,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             // Remember that home is pressed and handle special actions.
             if (repeatCount == 0) {
                 mHomePressed = true;
+                if (mOmniSwitchRecents) {
+                    OmniSwitchConstants.hideOmniSwitchRecents(mContext, UserHandle.CURRENT);
+                }
                 if (mHomeDoubleTapPending) {
                     mHomeDoubleTapPending = false;
                     mHandler.removeCallbacks(mHomeDoubleTapTimeoutRunnable);
