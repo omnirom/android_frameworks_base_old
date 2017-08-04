d978dade8fed81e66cddef1221609f920f16ed4a
Matthew Hui <mhui@codeaurora.org>
2017-08-04 17:05:30 -0400

    SEEMP: rename optional app verifier intent name
    Use com.qualcomm.qti.intent.action as intent prefix, since the original
    prefix android.intent.action is reserved for system intents.
    CTS tests will fail if android.intent.action is used.
    
    Change-Id: I4a175dc48694843a768fdc8a5feea0969e66f088

--- core/java/android/accounts/AccountManager.java
+++ core/java/android/accounts/AccountManager.java
@@ -2578,8 +2578,8 @@ public class AccountManager {
         @Override
         public void doWork() throws RemoteException {
             android.util.SeempLog.record(31);
-            getAccountsByTypeAndFeatures(mAccountType, mFeatures,
-                    new AccountManagerCallback<Account[]>() {
+            getAccountByTypeAndFeatures(mAccountType, mFeatures,
+                    new AccountManagerCallback<Bundle>() {
                         @Override
                         public void run(AccountManagerFuture<Bundle> future) {
                             String accountName = null;
