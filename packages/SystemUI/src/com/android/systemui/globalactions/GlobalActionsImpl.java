31e065057f2c40727d6af8f04388bdea62b417ff
maxwen <max.weninger@gmail.com>
2017-08-28 03:37:08 +0200

    base: advanced reboot with fastboot
    correct default value of advanced reboot
    
    Change-Id: I030c636b06036847236a3cbb5d848bb2707a3e4a
    
    base: correct strings for advanced reboot
    
    add a customReboot API that is used for advanced
    reboot fromm GlobaActions and use correct strings
    for the reboot progress dialog
    
    [1/2] base: expose rebootCustom for OpenDelta
    
    Dont scare people by showing the wrong reboot progress
    
    Adapted for android 7.0
    
    base: use new shutdown ui for all reboot actions
    Change-Id: I22af2cd9c2656978868ca05a31566d166f56a4f4
    
    base: add reboot fastboot to power buttons menu
    
    Change-Id: Ifa1666eb09aa48979c6bd3e2a613a52e731bd4c4
    
    base: Edit advanced reboot action for Android-12
    
    Change-Id: If80afe88f5dc945bec16c84822d44fa368279083

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
@@ -79,7 +79,7 @@ public class GlobalActionsImpl implements GlobalActions, CommandQueue.Callbacks 
     }
 
     @Override
-    public void showShutdownUi(boolean isReboot, String reason) {
+    public void showShutdownUi(boolean isReboot, String reason, boolean rebootCustom) {
         ScrimDrawable background = new ScrimDrawable();
 
         final Dialog d = new Dialog(mContext,
@@ -143,8 +143,8 @@ public class GlobalActionsImpl implements GlobalActions, CommandQueue.Callbacks 
         reasonView.setTextColor(color);
         messageView.setTextColor(color);
 
-        messageView.setText(getRebootMessage(isReboot, reason));
-        String rebootReasonMessage = getReasonMessage(reason);
+        messageView.setText(getRebootMessage(isReboot, reason, rebootCustom));
+        String rebootReasonMessage = getReasonMessage(reason, rebootCustom);
         if (rebootReasonMessage != null) {
             reasonView.setVisibility(View.VISIBLE);
             reasonView.setText(rebootReasonMessage);
@@ -154,11 +154,17 @@ public class GlobalActionsImpl implements GlobalActions, CommandQueue.Callbacks 
     }
 
     @StringRes
-    private int getRebootMessage(boolean isReboot, @Nullable String reason) {
+    private int getRebootMessage(boolean isReboot, @Nullable String reason, boolean custom) {
         if (reason != null && reason.startsWith(PowerManager.REBOOT_RECOVERY_UPDATE)) {
             return R.string.reboot_to_update_reboot;
-        } else if (reason != null && reason.equals(PowerManager.REBOOT_RECOVERY)) {
+        } else if (reason != null && !custom && reason.equals(PowerManager.REBOOT_RECOVERY)) {
             return R.string.reboot_to_reset_message;
+        } else if (reason != null && reason.equals(PowerManager.REBOOT_RECOVERY)) {
+            return com.android.internal.R.string.reboot_to_recovery_message;
+        } else if (reason != null && reason.equals(PowerManager.REBOOT_BOOTLOADER)) {
+            return com.android.internal.R.string.reboot_to_bootloader_message;
+        } else if (reason != null && reason.equals(PowerManager.REBOOT_FASTBOOT)) {
+            return com.android.internal.R.string.reboot_to_fastboot_message;
         } else if (isReboot) {
             return R.string.reboot_to_reset_message;
         } else {
@@ -167,10 +173,10 @@ public class GlobalActionsImpl implements GlobalActions, CommandQueue.Callbacks 
     }
 
     @Nullable
-    private String getReasonMessage(@Nullable String reason) {
+    private String getReasonMessage(@Nullable String reason, boolean custom) {
         if (reason != null && reason.startsWith(PowerManager.REBOOT_RECOVERY_UPDATE)) {
             return mContext.getString(R.string.reboot_to_update_title);
-        } else if (reason != null && reason.equals(PowerManager.REBOOT_RECOVERY)) {
+        } else if (reason != null && reason.equals(PowerManager.REBOOT_RECOVERY) && !custom) {
             return mContext.getString(R.string.reboot_to_reset_title);
         } else {
             return null;
