a32bcb6419bc76199f7e2aa3b0c1226885f7fc1a
maxwen <max.weninger@gmail.com>
2017-08-28 03:37:08 +0200

    [1/2] base: advanced reboot
    Change-Id: I54e8c96543945200bf4ffc47d03782be145ac594
    
    base: correct default value of advanced reboot
    
    Change-Id: I00fbc5333f935a6d99cda98f0123dc3335c87811

--- services/core/java/com/android/server/statusbar/StatusBarManagerService.java
+++ services/core/java/com/android/server/statusbar/StatusBarManagerService.java
@@ -1208,7 +1208,7 @@ public class StatusBarManagerService extends IStatusBarService.Stub implements D
      * Allows the status bar to reboot the device.
      */
     @Override
-    public void reboot(boolean safeMode) {
+    public void reboot(boolean safeMode, String reason) {
         enforceStatusBarService();
         long identity = Binder.clearCallingIdentity();
         try {
@@ -1218,8 +1218,7 @@ public class StatusBarManagerService extends IStatusBarService.Stub implements D
                 if (safeMode) {
                     ShutdownThread.rebootSafeMode(getUiContext(), true);
                 } else {
-                    ShutdownThread.reboot(getUiContext(),
-                            PowerManager.SHUTDOWN_USER_REQUESTED, false);
+                    ShutdownThread.rebootCustom(getUiContext(), reason, false);
                 }
             });
         } finally {
