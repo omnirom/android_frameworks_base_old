3d29e9d6cb57e831ce26fbb953c684e02835a87b
ezio84 <brabus84@gmail.com>
2017-09-01 15:28:27 +0200

    [1/2] base: Double tap to sleep on lockscreen
    Change-Id: Icd8de7c5512362f22dcea9c3946347a1f0c13aba

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4806,6 +4806,17 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+         * Double tap on lockscreen to sleep
+         * @hide
+         */
+        public static final String OMNI_DOUBLE_TAP_SLEEP_LOCKSCREEN =
+                "double_tap_sleep_lockscreen";
+
+        /** @hide */
+        private static final Validator OMNI_DOUBLE_TAP_SLEEP_LOCKSCREEN_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -4909,6 +4920,7 @@ public final class Settings {
             OMNI_ASPECT_RATIO_APPS_LIST,
             OMNI_LOCK_POWER_MENU_DISABLED,
             OMNI_DOUBLE_TAP_SLEEP_GESTURE,
+            OMNI_DOUBLE_TAP_SLEEP_LOCKSCREEN,
         };
 
         /**
@@ -5062,6 +5074,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(OMNI_ASPECT_RATIO_APPS_LIST);
             PRIVATE_SETTINGS.add(OMNI_LOCK_POWER_MENU_DISABLED);
             PRIVATE_SETTINGS.add(OMNI_DOUBLE_TAP_SLEEP_GESTURE);
+            PRIVATE_SETTINGS.add(OMNI_DOUBLE_TAP_SLEEP_LOCKSCREEN);
         }
 
         /**
@@ -5210,6 +5223,8 @@ public final class Settings {
                     OMNI_LOCK_POWER_MENU_DISABLED_VALIDATOR);
             VALIDATORS.put(OMNI_DOUBLE_TAP_SLEEP_GESTURE,
                     OMNI_DOUBLE_TAP_SLEEP_GESTURE_VALIDATOR);
+            VALIDATORS.put(OMNI_DOUBLE_TAP_SLEEP_LOCKSCREEN,
+                    OMNI_DOUBLE_TAP_SLEEP_LOCKSCREEN_VALIDATOR);
         }
 
         /**
