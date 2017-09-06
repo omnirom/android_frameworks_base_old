4adec418cc86ffb572ad3bf0b02eeaa73e02bcfa
ezio84 <brabus84@gmail.com>
2017-09-06 21:32:08 +0200

    Add partial/full screenshot QS tile
    single tap to choose the mode, long press to fire
    
    thanks @kantjer for the icons!
    
    Change-Id: Ida407c9145de8cdeba6039f006d1e0eca16b955b

--- packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
@@ -2,7 +2,7 @@
  * Copyright (C) 2014 The Android Open Source Project
  * Copyright (C) 2012-2015 The CyanogenMod Project
  * Copyright (C) 2014-2015 The Euphoria-OS Project
- *
+ * Copyright (C) 2017 ABC rom
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,33 +18,31 @@
 
 package com.android.systemui.qs.tiles;
 
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.Handler;
-import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
-import android.os.RemoteException;
-import android.view.View;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
 
-import com.android.systemui.R;
-import com.android.systemui.plugins.qs.QSTile.BooleanState;
-import com.android.systemui.qs.QSHost;
-import com.android.systemui.qs.tileimpl.QSTileImpl;
-import com.android.systemui.screenshot.TakeScreenshotService;
+import com.android.internal.util.omni.OmniUtils;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.R;
 
 /** Quick settings tile: Screenshot **/
 public class ScreenshotTile extends QSTileImpl<BooleanState> {
 
-    private boolean mListening;
-    private final Object mScreenshotLock = new Object();
-    private ServiceConnection mScreenshotConnection = null;
+    private boolean mRegion = false;
 
     public ScreenshotTile(QSHost host) {
         super(host);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CUSTOM_QUICK_TILES;
     }
 
     @Override
@@ -53,33 +51,23 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
     }
 
     @Override
-    public void handleSetListening(boolean listening) {
-        if (mListening == listening) return;
-        mListening = listening;
-    }
+    public void handleSetListening(boolean listening) {}
 
     @Override
-    protected void handleClick() {
-        mHost.collapsePanels();
-        /* wait for the panel to close */
-        try {
-             Thread.sleep(1500);
-        } catch (InterruptedException ie) {
-             // Do nothing
-        }
-        takeScreenshot();
+    public void handleClick() {
+        mRegion = !mRegion;
+        refreshState();
     }
 
     @Override
     public void handleLongClick() {
         mHost.collapsePanels();
-        /* wait for the panel to close */
+
+        //finish collapsing the panel
         try {
-             Thread.sleep(1500);
-        } catch (InterruptedException ie) {
-             // Do nothing
-        }
-        takeScreenshot();
+             Thread.sleep(1000); //1s
+        } catch (InterruptedException ie) {}
+        OmniUtils.takeScreenshot(mRegion ? false : true);
     }
 
     @Override
@@ -88,87 +76,22 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
     }
 
     @Override
-    protected void handleUpdateState(BooleanState state, Object arg) {
-        state.label = mContext.getString(R.string.quick_settings_screenshot_label);
-        state.icon = ResourceIcon.get(R.drawable.ic_qs_screenshot);
-        state.contentDescription =  mContext.getString(
-                   R.string.quick_settings_screenshot_label);
-    }
-
-    @Override
     public CharSequence getTileLabel() {
         return mContext.getString(R.string.quick_settings_screenshot_label);
     }
 
     @Override
-    public int getMetricsCategory() {
-        return MetricsEvent.CUSTOM_QUICK_TILES;
-    }
-
-    final Runnable mScreenshotTimeout = new Runnable() {
-        @Override
-        public void run() {
-            synchronized (mScreenshotLock) {
-                if (mScreenshotConnection != null) {
-                    mContext.unbindService(mScreenshotConnection);
-                    mScreenshotConnection = null;
-                }
-            }
-        }
-    };
-
-    private void takeScreenshot() {
-        synchronized (mScreenshotLock) {
-            if (mScreenshotConnection != null) {
-                return;
-            }
-
-            Intent intent = new Intent(mContext, TakeScreenshotService.class);
-            ServiceConnection conn = new ServiceConnection() {
-                @Override
-                public void onServiceConnected(ComponentName name, IBinder service) {
-                    synchronized (mScreenshotLock) {
-                        if (mScreenshotConnection != this) {
-                            return;
-                        }
-
-                        Messenger messenger = new Messenger(service);
-                        Message msg = Message.obtain(null, 1);
-                        final ServiceConnection myConn = this;
-                        Handler h = new Handler(mHandler.getLooper()) {
-                            @Override
-                            public void handleMessage(Message msg) {
-                                synchronized (mScreenshotLock) {
-                                    if (mScreenshotConnection == myConn) {
-                                        mContext.unbindService(mScreenshotConnection);
-                                        mScreenshotConnection = null;
-                                        mHandler.removeCallbacks(mScreenshotTimeout);
-                                    }
-                                }
-                            }
-                        };
-                        msg.replyTo = new Messenger(h);
-                        msg.arg1 = msg.arg2 = 0;
-
-                        // Take the screenshot
-                        try {
-                            messenger.send(msg);
-                        } catch (RemoteException e) {
-                            // Do nothing here
-                        }
-                    }
-                }
-
-                @Override
-                public void onServiceDisconnected(ComponentName name) {
-                    // Do nothing here
-                }
-            };
-
-            if (mContext.bindService(intent, conn, mContext.BIND_AUTO_CREATE)) {
-                mScreenshotConnection = conn;
-                mHandler.postDelayed(mScreenshotTimeout, 10000);
-            }
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        if (mRegion) {
+            state.label = mContext.getString(R.string.quick_settings_region_screenshot_label);
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_region_screenshot);
+            state.contentDescription =  mContext.getString(
+                    R.string.quick_settings_region_screenshot_label);
+        } else {
+            state.label = mContext.getString(R.string.quick_settings_screenshot_label);
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_screenshot);
+            state.contentDescription =  mContext.getString(
+                    R.string.quick_settings_screenshot_label);
         }
     }
 }
