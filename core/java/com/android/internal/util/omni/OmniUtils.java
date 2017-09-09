f025ce47e330c8f8ace3a144d45cfbbc51df82e2
ezio84 <brabus84@gmail.com>
2017-09-09 14:04:02 +0200

    Add a sendKeycode api to OmniUtils
    code borrowed from com.android.systemui.statusbar.policy.KeyButtonView
    
    Change-Id: Idd9a8384d5842de6a85a7a7ae487bacc9b7418c6

--- core/java/com/android/internal/util/omni/OmniUtils.java
+++ core/java/com/android/internal/util/omni/OmniUtils.java
@@ -20,6 +20,13 @@ package com.android.internal.util.omni;
 import android.content.Context;
 import android.content.Intent;
 import android.os.UserHandle;
+import android.hardware.input.InputManager;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.SystemClock;
+import android.view.InputDevice;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
 
 public class OmniUtils {
     /**
@@ -45,5 +52,30 @@ public class OmniUtils {
         keyguardIntent.setPackage(SYSTEMUI_PACKAGE_NAME);
         keyguardIntent.putExtra(DISMISS_KEYGUARD_EXTRA_INTENT, launchIntent);
         context.sendBroadcastAsUser(keyguardIntent, user);
+    }
+
+    public static void sendKeycode(int keycode) {
+        long when = SystemClock.uptimeMillis();
+        final KeyEvent evDown = new KeyEvent(when, when, KeyEvent.ACTION_DOWN, keycode, 0,
+                0, KeyCharacterMap.VIRTUAL_KEYBOARD, 0,
+                KeyEvent.FLAG_FROM_SYSTEM | KeyEvent.FLAG_VIRTUAL_HARD_KEY,
+                InputDevice.SOURCE_KEYBOARD);
+        final KeyEvent evUp = KeyEvent.changeAction(evDown, KeyEvent.ACTION_UP);
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                InputManager.getInstance().injectInputEvent(evDown,
+                        InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
+            }
+        });
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                InputManager.getInstance().injectInputEvent(evUp,
+                        InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
+            }
+        }, 20);
     }
 }
