97d626c511485d7ad29e467c282c428d3b1a9586
ezio84 <brabus84@gmail.com>
2017-09-16 21:11:37 +0200

    Allow to customize notification led light [2/2]
    for both legacy apps and new api26 apps (per-channel led customization)
    
    Change-Id: I586d4ee156409aa1d1ecf12aadc6f601be40c8dc

--- services/core/java/com/android/server/notification/NotificationRecord.java
+++ services/core/java/com/android/server/notification/NotificationRecord.java
@@ -197,21 +197,30 @@ public final class NotificationRecord {
                 com.android.internal.R.integer.config_defaultNotificationLedOn);
         int defaultLightOff = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_defaultNotificationLedOff);
-
-        int channelLightColor = getChannel().getLightColor() != 0 ? getChannel().getLightColor()
+        int userSetLightColor = getChannel().getLightColor();
+        int userSetLightOnTime = getChannel().getLightOnTime();
+        int userSetLightOffTime = getChannel().getLightOffTime();
+        int channelLightColor = userSetLightColor != 0x00ffffff ? userSetLightColor
                 : defaultLightColor;
+        int channelLightOnTime = userSetLightOnTime != 0 ? userSetLightOnTime
+                : defaultLightOn;
+        int channelLightOffTime = userSetLightOffTime != 0 ? userSetLightOffTime
+                : defaultLightOff;
         Light light = getChannel().shouldShowLights() ? new Light(channelLightColor,
-                defaultLightOn, defaultLightOff) : null;
+                channelLightOnTime, channelLightOffTime) : null;
+
         if (mPreChannelsNotification
                 && (getChannel().getUserLockedFields()
                 & NotificationChannel.USER_LOCKED_LIGHTS) == 0) {
             final Notification notification = sbn.getNotification();
             if ((notification.flags & Notification.FLAG_SHOW_LIGHTS) != 0) {
-                light = new Light(notification.ledARGB, notification.ledOnMS,
-                        notification.ledOffMS);
+                light = new Light(userSetLightColor != 0x00ffffff ? userSetLightColor : notification.ledARGB,
+                        userSetLightOnTime != 0 ? userSetLightOnTime : notification.ledOnMS,
+                        userSetLightOffTime != 0 ? userSetLightOffTime : notification.ledOffMS);
                 if ((notification.defaults & Notification.DEFAULT_LIGHTS) != 0) {
-                    light = new Light(defaultLightColor, defaultLightOn,
-                            defaultLightOff);
+                    light = new Light(userSetLightColor != 0x00ffffff ? userSetLightColor : defaultLightColor,
+                        userSetLightOnTime != 0 ? userSetLightOnTime : defaultLightOn,
+                        userSetLightOffTime != 0 ? userSetLightOffTime : defaultLightOff);
                 }
             } else {
                 light = null;
