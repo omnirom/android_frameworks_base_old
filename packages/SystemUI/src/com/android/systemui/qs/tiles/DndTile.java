9bee6176cc9afb32f136d8ee94f4504e77cfbbdb
maxwen <max.weninger@gmail.com>
2017-09-21 00:38:01 +0200

    base: SystemUI: add back QS hide label
    if enabled label click (secondary) will be reassigned to long click
    for dual target tiles
    
    Change-Id: I440362ab38057cbfba7750d75825179bd830c4e8

--- packages/SystemUI/src/com/android/systemui/qs/tiles/DndTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DndTile.java
@@ -104,6 +104,11 @@ public class DndTile extends QSTileImpl<BooleanState> {
         }
     }
 
+    @Override
+    public boolean isDualTarget() {
+        return true;
+    }
+
     public static void setVisible(Context context, boolean visible) {
         Prefs.putBoolean(context, Prefs.Key.DND_TILE_VISIBLE, visible);
     }
