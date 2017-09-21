a94605e204181c1f4d686395cd236f06b60edc28
maxwen <max.weninger@gmail.com>
2017-09-21 16:02:23 +0200

    base: SettingsLib: stop ClassCastException
    Change-Id: I0e9f9f3f49279d3305d9003f1205f3c496af5c6f

--- packages/SettingsLib/src/com/android/settingslib/SuggestionParser.java
+++ packages/SettingsLib/src/com/android/settingslib/SuggestionParser.java
@@ -359,7 +359,11 @@ public class SuggestionParser {
         if (isSmartSuggestionEnabled) {
             return mSmartDismissControl;
         } else {
-            return suggestion.metaData.getString(META_DATA_DISMISS_CONTROL);
+            Object o = suggestion.metaData.get(META_DATA_DISMISS_CONTROL);
+            if (o instanceof String) {
+                return (String) o;
+            }
+            return null;
         }
     }
 
