789648766d18fb0339c7a0712986ce6a0f128831
maxwen <max.weninger@gmail.com>
2017-09-22 18:45:17 +0200

    base: tuner: add a hide config to left and right shortcut
    change the current none to be a hide shortcut
    and add a new default to use instead
    
    Change-Id: I6fdfa68a4b083df0ae6583000862dc7bb07a75c7

--- packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
@@ -112,7 +112,7 @@ public class TunerFragment extends PreferenceFragment {
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        menu.add(Menu.NONE, MENU_REMOVE, Menu.NONE, R.string.remove_from_settings);
+        //menu.add(Menu.NONE, MENU_REMOVE, Menu.NONE, R.string.remove_from_settings);
     }
 
     @Override
@@ -121,7 +121,7 @@ public class TunerFragment extends PreferenceFragment {
             case android.R.id.home:
                 getActivity().finish();
                 return true;
-            case MENU_REMOVE:
+            /*case MENU_REMOVE:
                 TunerService.showResetRequest(getContext(), new Runnable() {
                     @Override
                     public void run() {
@@ -130,7 +130,7 @@ public class TunerFragment extends PreferenceFragment {
                         }
                     }
                 });
-                return true;
+                return true;*/
         }
         return super.onOptionsItemSelected(item);
     }
