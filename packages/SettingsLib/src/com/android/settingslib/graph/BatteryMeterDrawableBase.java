5ff5f3869ff490881145c3e9e2621df1ae7e61a4
maxwen <max.weninger@gmail.com>
2017-10-03 14:32:57 +0200

    [1/2] base: poor man battery style tweaks
    for now just add hide/show image and or percentage
    
    Change-Id: I2d781be39f1f99c28166b63ea40d783a4316ef93

--- packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
+++ packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
@@ -497,4 +497,8 @@ public class BatteryMeterDrawableBase extends Drawable {
     protected float getRadiusRatio() {
         return RADIUS_RATIO;
     }
+
+    public int getPowersaveColor() {
+        return mPowersavePaint.getColor();
+    }
 }
