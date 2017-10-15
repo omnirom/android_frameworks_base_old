83af92e14830be9ed5e3b57cc749cd7a44ace127
maxwen <max.weninger@gmail.com>
2017-10-15 14:15:16 +0200

    base: fix haptic feedback for custom buttons
    Change-Id: Ia450d15cf6fdd0ad2e0c289b4894380b6063171c

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6267,6 +6267,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         final int keyCode = event.getKeyCode();
         final boolean virtualKey = event.getDeviceId() == KeyCharacterMap.VIRTUAL_KEYBOARD;
         final boolean isInjected = (policyFlags & WindowManagerPolicy.FLAG_INJECTED) != 0;
+        final boolean isVirtualHardKey = (policyFlags & KeyEvent.FLAG_VIRTUAL_HARD_KEY) != 0;
 
         // If screen is off then we treat the case where the keyguard is open but hidden
         // the same as if it were open and in front.
@@ -6343,6 +6344,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 && (policyFlags & WindowManagerPolicy.FLAG_VIRTUAL) != 0
                 && event.getRepeatCount() == 0
                 && !disableKey;
+
+        if (virtualKey && !isVirtualHardKey) {
+            useHapticFeedback = false;
+        }
 
         // Specific device key handling
         if (mDeviceKeyHandler != null) {
