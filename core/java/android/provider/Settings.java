062d2add168c3c74f5df97cf94b76a501f0d34c7
cristianomatos <cristianobmatos@gmail.com>
2017-11-02 18:45:20 -0400

    base: Recents panel clear all button tweaks [1/2]
    - Show or hide recents button
    - Place the button in top right or left
    
    Change-Id: I1c37b7d3b66e0a9b9652f0d0cda9152b4ab542ce

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4562,6 +4562,18 @@ public final class Settings {
         public static final String QS_QUICKBAR_COLUMNS = "qs_quickbar_columns";
 
         /**
+         * location of the clear all rectents button
+         * @hide
+         */
+        public static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+        /**
+         * show clear all recents button
+         *  @hide
+         */
+        public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
