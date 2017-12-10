27d792ad3c903214900bee8daf57265ec669404d
ezio84 <brabus84@gmail.com>
2017-12-10 18:51:02 +0100

    Let's use last screenOn brightness value for ambient display [1/2]
    aosp allowed this only through the config_allowAutoBrightnessWhileDozing
    bool.
    
    TO_DO: 8.1 introduced a new managemnt to use a custom light sensor
    to handle brightness in real time during AOD and Ambient pulsing,
    looks interesting:
    https://github.com/ezio84/abc_frameworks_base/commit/2981eb0d59f3568bfe84ce905c82fc17d62d21c5#diff-2298f293d0638ded26bf7b689560c650R83
    https://github.com/ezio84/abc_frameworks_base/commit/3e23eb59d09faca2e40e308a191d8499ffdb6ab3
    
    Change-Id: Ief6d0cc55434cb19940c2032d2ae10a5d44043c6

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -68,6 +68,7 @@ import android.content.pm.UserInfo;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.database.ContentObserver;
+import android.content.ContentResolver;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
@@ -876,6 +877,10 @@ public class StatusBar extends SystemUI implements DemoMode,
             mContext.getContentResolver().registerContentObserver(Settings.Secure.getUriFor(
                     Settings.Secure.LOCK_QS_DISABLED),
                     false, this, UserHandle.USER_ALL);
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LAST_DOZE_AUTO_BRIGHTNESS),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -887,6 +892,9 @@ public class StatusBar extends SystemUI implements DemoMode,
                     Settings.System.FORCE_AMBIENT_FOR_MEDIA))) {
                 setForceAmbient();
                 return;
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.LAST_DOZE_AUTO_BRIGHTNESS))) {
+                updateDozeBrightness();
             }
             update();
         }
@@ -925,6 +933,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             if (mNotificationPanel != null) {
                 mNotificationPanel.updateSettings();
             }
+            updateDozeBrightness();
         }
     }
     private OmniSettingsObserver mOmniSettingsObserver;
@@ -6095,6 +6104,16 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     private boolean isAmbientContainerAvailable() {
         return mAmbientMediaPlaying != 0 && mAmbientIndicationContainer != null;
+
+    }
+
+    private void updateDozeBrightness() {
+        int defaultDozeBrightness = mContext.getResources().getInteger(
+                com.android.internal.R.integer.config_screenBrightnessDoze);
+        int lastValue = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.LAST_DOZE_AUTO_BRIGHTNESS, defaultDozeBrightness,
+                UserHandle.USER_CURRENT);
+        mStatusBarWindowManager.updateDozeBrightness(lastValue);
     }
 
     private RemoteViews.OnClickHandler mOnClickHandler = new RemoteViews.OnClickHandler() {
