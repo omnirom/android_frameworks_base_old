70d7b328aa1621a20e16c2d75ed9e232eabc64be
Alex Cruz <du.alexcruz@gmail.com>
2018-01-10 16:36:10 -0500

    [1/2] base: Hide lockscreen items
    This adds the ability to add items such as data, clock and alarm in the
    lockscreen.
    
    Thanks to beanstown106 for the tip on the alarm text mod
    
    Change-Id: I42c7ad0c3de0b41a653b9b687acfe9536bf80a44

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1196,6 +1196,9 @@ public class NotificationPanelView extends PanelView implements
         } else {
             mKeyguardStatusBar.setAlpha(1f);
             mKeyguardStatusBar.setVisibility(keyguardShowing ? View.VISIBLE : View.INVISIBLE);
+            if (keyguardShowing) {
+                mKeyguardStatusBar.toggleContents(true);
+            }
             if (keyguardShowing && oldState != mStatusBarState) {
                 mKeyguardBottomArea.onKeyguardShowingChanged();
                 if (mQs != null) {
