dbbf8fbdd4e59d4e8027fef88354d3770c3cf72f
ezio84 <brabus84@gmail.com>
2018-01-12 10:54:32 +0100

    Multi ScreenRecord tile, blinking stop button, screenrecord service improvements [1/2]
    single tap to toggle quality:
    - 480*800 resolution and 1.5Mbps
    - default resolution (720p) and 4Mbps
    - default resolution (720p) and 8Mbps
    long press to start recording
    
    Show also a blinking red button at bottom right of the screen
    that can be tapped to stop the recording instead of using
    the notification.
    
    Also improve the screenrecord service and be sure
    to stop it when we are done.
    
    part 2/2 is in framework/av tree, it just sets max recording
    duration to 30 minutes instead of 3
    
    Change-Id: I9a0161e6ddafb721421585c7f16bba4b30319425

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -41,6 +41,7 @@ import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.PictureInPictureTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WeatherTile;
@@ -81,6 +82,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("screenshot")) return new ScreenshotTile(mHost);
         else if (tileSpec.equals("caffeine")) return new CaffeineTile(mHost);
         else if (tileSpec.equals("headsup")) return new HeadsUpTile(mHost);
+        else if (tileSpec.equals("screenrecord")) return new ScreenrecordTile(mHost);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(mHost, tileSpec);
