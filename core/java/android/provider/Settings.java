aab615b805840c769cd178b6b076bd2b7c50c271
maxwen <max.weninger@gmail.com>
2018-01-25 15:03:47 +0100

    base: SystemUI: persist screenrecord quality level
    save state set in qs tile into settings
    
    Change-Id: I708b324a22c3e605fa6411c1a9e1de28b584938b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4359,6 +4359,11 @@ public final class Settings {
         public static final String FORCE_AMBIENT_FOR_MEDIA = "force_ambient_for_media";
 
         /**
+         * @hide
+         */
+        public static final String SCREENRECORD_QUALITY_MODE = "screenrecord_quality_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
