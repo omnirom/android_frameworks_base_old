552c621b7677540eb29ef8944e8d39a302e07105
kldoc <fred.berbigier@gmail.com>
2018-02-01 21:50:10 +0800

    add Gesture haptic feedback support
    Change-Id: I447bb8f3dbd5c2953cbc9204815198e5b3e2a0bd

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4071,6 +4071,13 @@ public final class Settings {
         public static final String DEVICE_PROXI_CHECK_ENABLED = "device_proxi_check_enabled";
 
         /**
+         * Enable Gesture Haptic feedback
+         * KeyHandler
+         * @hide
+         */
+        public static final String DEVICE_GESTURE_FEEDBACK_ENABLED = "device_gesture_feedback_enabled";
+
+        /**
          * @hide
          */
         public static final String ACCELEROMETER_ROTATION_ANGLES = "accelerometer_rotation_angles";
@@ -4549,6 +4556,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(BUTTON_EXTRA_KEY_MAPPING);
             PRIVATE_SETTINGS.add(HARDWARE_KEYS_DISABLE);
             PRIVATE_SETTINGS.add(DEVICE_PROXI_CHECK_ENABLED);
+            PRIVATE_SETTINGS.add(DEVICE_GESTURE_FEEDBACK_ENABLED);
             PRIVATE_SETTINGS.add(ACCELEROMETER_ROTATION_ANGLES);
             PRIVATE_SETTINGS.add(LOCKSCREEN_ROTATION);
             PRIVATE_SETTINGS.add(VOLUME_LINK_NOTIFICATION);
