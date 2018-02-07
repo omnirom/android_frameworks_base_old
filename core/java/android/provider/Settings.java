423fe424a87287d64c37de81371f09c88b47659d
Kyrylo Mikos <kiril.mik.os@gmail.com>
2018-02-06 20:44:23 -0500

    Bringup expanded desktop and implement per-user configuration
    Change-Id: I43e8c815269a9e5fe9ac2d580439ff66fd6946b3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10481,11 +10481,20 @@ public final class Settings {
         /**
          * Defines global runtime overrides to window policy.
          *
-         * See {@link com.android.server.policy.PolicyControl} for value format.
+         * See {@link android.view.WindowManagerPolicyControl} for value format.
          *
          * @hide
          */
         public static final String POLICY_CONTROL = "policy_control";
+
+        /**
+         * Defines global runtime overrides to window policy.
+         *
+         * See {@link android.view.WindowManagerPolicyControl} for value definitions.
+         *
+         * @hide
+         */
+        public static final String POLICY_CONTROL_SELECTED = "policy_control_selected";
 
         /**
          * Defines global zen mode.  ZEN_MODE_OFF, ZEN_MODE_IMPORTANT_INTERRUPTIONS,
