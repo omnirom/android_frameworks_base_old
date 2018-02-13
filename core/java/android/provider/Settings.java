0e5f44896831ce480a9475c186ce304f1b8fb9df
yank555-lu <yank555.lu@gmail.com>
2018-02-13 09:50:28 -0800

    [1/2] base: SystemUI: status bar clock date position
    Change-Id: Iaff480dc574d2111595c4e7dc42526843a753561

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4536,6 +4536,14 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_DATE_FORMAT = "statusbar_clock_date_format";
 
         /**
+         * Position of date
+         * 0 - Left of clock
+         * 1 - Right of clock
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_DATE_POSITION = "statusbar_clock_date_position";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
