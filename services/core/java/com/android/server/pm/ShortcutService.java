e4cca6feb54a8a106f64cb883e0d46661c1acbb4
maxwen <max.weninger@gmail.com>
2018-02-23 03:05:34 +0100

    base: use omni fingerprint for ShortcutService
    make sure cached data gets updated
    no more derped deep shortcuts text and images
    
    Change-Id: I65d743a4991f92739e59b4655dcc086bd36a5401

--- services/core/java/com/android/server/pm/ShortcutService.java
+++ services/core/java/com/android/server/pm/ShortcutService.java
@@ -4238,7 +4238,7 @@ public class ShortcutService extends IShortcutService.Stub {
 
     // Injection point.
     String injectBuildFingerprint() {
-        return Build.FINGERPRINT;
+        return Build.OMNI_FINGERPRINT;
     }
 
     final void wtf(String message) {
