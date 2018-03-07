927c18fe22a249e3cd06cc0022c88a3fdabbd7e1
d34d <clark@cyngn.com>
2018-03-07 09:16:30 -0800

    Add dynamic theme fonts support
    Change-Id: Ib01db13b38cb7010e342de1761bfe18289093fa5

--- core/java/android/os/Process.java
+++ core/java/android/os/Process.java
@@ -455,10 +455,11 @@ public class Process {
                                   String instructionSet,
                                   String appDataDir,
                                   String invokeWith,
+                                  boolean refreshTheme,
                                   String[] zygoteArgs) {
         return zygoteProcess.start(processClass, niceName, uid, gid, gids,
                     debugFlags, mountExternal, targetSdkVersion, seInfo,
-                    abi, instructionSet, appDataDir, invokeWith, zygoteArgs);
+                    abi, instructionSet, appDataDir, invokeWith, refreshTheme, zygoteArgs);
     }
 
     /** @hide */
@@ -472,10 +473,11 @@ public class Process {
                                   String instructionSet,
                                   String appDataDir,
                                   String invokeWith,
+                                  boolean refreshTheme,
                                   String[] zygoteArgs) {
         return WebViewZygote.getProcess().start(processClass, niceName, uid, gid, gids,
                     debugFlags, mountExternal, targetSdkVersion, seInfo,
-                    abi, instructionSet, appDataDir, invokeWith, zygoteArgs);
+                    abi, instructionSet, appDataDir, invokeWith, refreshTheme, zygoteArgs);
     }
 
     /**
