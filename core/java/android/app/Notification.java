ad67d048cb0e298741f27a222b404ecf9c55bfdd
maxwen <max.weninger@gmail.com>
2018-03-10 03:11:54 +0100

    [1/2] base: fix notification overlay config
    for notification overlays to correctly tint notification texts
    we must add a way they can override whatever is in the primary
    overlay
    
    to make things easier we only use one for both
    config_useDarkBgNotificationIconTextTinting
    and config_enableNightMode since they are never
    used different anyway
    
    notification overlays need to use
    <bool name="config_notificationTinting_override">true</bool>
    and can then use config_useDarkBgNotificationTinting_override
    which will be used instead of both the other configs
    above
    
    Change-Id: Idc80703a9d6af034266446baec06c9f7a9b8c7ef

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -2824,7 +2824,7 @@ public class Notification implements Parcelable
                         == Configuration.UI_MODE_NIGHT_YES;
             }
             // UI_MODE_NIGHT doesnt seem to be ready, so just listen to config
-            mInNightMode = res.getBoolean(R.bool.config_enableNightMode);
+            mInNightMode = getColorUtil().getNightModeNotification(mContext);
 
             if (toAdopt == null) {
                 mN = new Notification();
@@ -4797,8 +4797,7 @@ public class Notification implements Parcelable
 
         private CharSequence processLegacyText(CharSequence charSequence, boolean ambient) {
             boolean isAlreadyLightText = isLegacy() || textColorsNeedInversion();
-            boolean wantLightText = ambient || mContext.getResources().getBoolean(
-                    R.bool.config_useDarkBgNotificationIconTextTinting);
+            boolean wantLightText = ambient || getColorUtil().getDarkNotificationTinting(mContext);
             if (isAlreadyLightText != wantLightText) {
                 return getColorUtil().invertCharSequenceColors(charSequence);
             } else {
