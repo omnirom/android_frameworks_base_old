0b3665427e1de5d6f9b2b9c41a286bdaafb2093c
maxwen <max.weninger@gmail.com>
2018-03-26 01:02:25 +0200

    base: expanded desktop: fix for endless layout loop on keyguard
    while this fixes the problem this can only be described
    as a workaround :)
    
    Change-Id: I209f6fc833d24c486587db0632e6e330c0060e41

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6043,7 +6043,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     mStatusBarController.updateVisibilityLw(false /*transientAllowed*/,
                             mLastSystemUiFlags, mLastSystemUiFlags);
                 }
-                if (statusBarExpanded && mNavigationBar != null) {
+                if (statusBarExpanded && mNavigationBar != null && !isStatusBarKeyguard()) {
                     if (mNavigationBarController.setBarShowingLw(true)) {
                         changes |= FINISH_LAYOUT_REDO_LAYOUT;
                     }
