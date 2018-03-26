963f7cc6df89499759f93659bb55ee5decf925ac
maxwen <max.weninger@gmail.com>
2018-03-26 22:04:52 +0200

    base: SystemUI: check all those darn chicken egg qs NPEs
    Change-Id: I556cf6886ce5d61c3c8f06a2ec95fb63ad5c4ac8

--- packages/SystemUI/src/com/android/systemui/qs/tiles/NfcTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/NfcTile.java
@@ -92,6 +92,9 @@ public class NfcTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
+        if (getAdapter() == null) {
+            return;
+        }
         if (state.slash == null) {
             state.slash = new SlashState();
         }
