5e5c97081409dae1c548ebfd8f78a43e1cc8dfba
maxwen <max.weninger@gmail.com>
2018-04-03 02:54:17 +0200

    [1/2] base: SystemUI: config battery saver system bar coloring usage
    Change-Id: I2822be7dd2f69e081256b2cde9e6725e7c1fa6f5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4588,18 +4588,10 @@ public final class Settings {
         public static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
 
         /**
-         * Hold current font name and name of the originating package Uses "|" as a delimiter
          *
          * @hide
          */
-        public static final String FONT_INFO = "font_info";
-
-        /**
-         * Hold list of installed font provider packages. Uses "|" as a delimiter
-         *
-         * @hide
-         */
-        public static final String FONT_PACKAGES = "font_packages";
+        public static final String BATTERY_SAVER_SYSTEM_BAR_COLOR_ENABLE = "battery_saver_system_bars_color_enable";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
@@ -4858,8 +4850,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(FINGERPRINT_SUCCESS_VIB);
             PRIVATE_SETTINGS.add(STATUS_BAR_QUICK_QS_PULLDOWN);
             PRIVATE_SETTINGS.add(LOCKSCREEN_PIN_SCRAMBLE_LAYOUT);
-            PRIVATE_SETTINGS.add(FONT_INFO);
-            PRIVATE_SETTINGS.add(FONT_PACKAGES);
+            PRIVATE_SETTINGS.add(BATTERY_SAVER_SYSTEM_BAR_COLOR_ENABLE);
         }
 
         /**
