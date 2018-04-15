2e3303b542b2344fdba71c8d29df26123248bb2e
maxwen <max.weninger@gmail.com>
2018-04-16 01:21:11 +0200

    base: SystemUI: cleanup battery styles
    remove old unused extra percent handling
    
    Change-Id: Ic92e2b6de97103a77485f942dbbe265a1f09ac54

--- packages/SystemUI/src/com/android/systemui/omni/BatteryViewManager.java
+++ packages/SystemUI/src/com/android/systemui/omni/BatteryViewManager.java
@@ -96,7 +96,7 @@ public class BatteryViewManager implements TunerService.Tunable {
         mBatteryStyleList.add(view);
 
         view = (IBatteryView) LayoutInflater.from(mContext).inflate(
-                R.layout.battery_percent_view, mContainerView, false);
+                R.layout.battery_meter_percent_view, mContainerView, false);
         mBatteryStyleList.add(view);
 
         view = (IBatteryView) LayoutInflater.from(mContext).inflate(
@@ -160,14 +160,11 @@ public class BatteryViewManager implements TunerService.Tunable {
         mCurrentBatteryView = mBatteryStyleList.get(mBatteryStyle);
         applyStyle();
 
-        int top = mContext.getResources().getDimensionPixelSize(R.dimen.battery_margin_top);
+        int top = mCurrentBatteryView.getTopMargin();
         ViewGroup.MarginLayoutParams lp = new ViewGroup.MarginLayoutParams(
                         ViewGroup.LayoutParams.WRAP_CONTENT,
                         ViewGroup.LayoutParams.MATCH_PARENT);
-        if (mLocation != BATTERY_LOCATION_KEYGUARD
-                && mLocation != BATTERY_LOCATION_AMBIENT) {
-            lp.setMargins(0, top, 0, 0);
-        }
+        lp.setMargins(0, top, 0, 0);
         mContainerView.addView((View) mCurrentBatteryView, lp);
         // percent only is done with mBatteryPercentView but we
         // still need a BatteryView as container to update level
