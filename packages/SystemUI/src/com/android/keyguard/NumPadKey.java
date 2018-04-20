c37be2f040f67110f8d28cc9a93b9890286cb3bf
maxwen <max.weninger@gmail.com>
2018-04-20 14:07:20 +0200

    base: SystemUI: restore keyguard default font styles
    after 34023208f03684eb163eb5925002d7b6d680dea0 two minor
    adjustments are needed to restore exactly the same styles
    as before for default
    
    could not care less but ofc some of the hawkeyes
    noticed it
    
    Change-Id: Iaf877925a2a4716ebd84e77b5dd4c8306053cdec

--- packages/SystemUI/src/com/android/keyguard/NumPadKey.java
+++ packages/SystemUI/src/com/android/keyguard/NumPadKey.java
@@ -17,7 +17,9 @@
 package com.android.keyguard;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.content.res.TypedArray;
+import android.graphics.Typeface;
 import android.os.PowerManager;
 import android.os.SystemClock;
 import android.os.UserHandle;
@@ -35,6 +37,7 @@ import com.android.internal.widget.LockPatternUtils;
 public class NumPadKey extends ViewGroup {
     // list of "ABC", etc per digit, starting with '0'
     static String sKlondike[];
+    private static final String FONT_FAMILY = "sans-serif-light";
 
     private int mDigit = -1;
     private int mTextViewResId;
@@ -110,6 +113,13 @@ public class NumPadKey extends ViewGroup {
         }
         a.recycle();
         setContentDescription(mDigitText.getText().toString());
+    }
+
+    @Override
+    protected void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Typeface tf = Typeface.create(FONT_FAMILY, Typeface.NORMAL);
+        mDigitText.setTypeface(tf);
     }
 
     public void setDigit(int digit) {
