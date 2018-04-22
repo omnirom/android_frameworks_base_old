d54e74265dbe7c2e0b4aa145a716870d387c5d44
Jesse Chan <jc@lineageos.org>
2018-04-21 23:45:57 -0700

    Add an option to force pre-O apps to use full screen aspect ratio
    When an app target pre-O releases, the default max aspect ratio
    is 1.86:1 which leads to ugly black areas on devices that have
    screens with higher aspect ratio (for example Galaxy S8/S9).
    
    This change adds an option to allow users to change default
    aspect ratio for pre-O apps to 2.1 which would fit recent devices.
    
    Wight554:
    Simplify the code, we don't need the Settings for that, just bool
    
    maxwen:
    remove the weired check for
    isGestureButtonEnabled
    out of compute bounds - why was this actually here?
    
    Change-Id: I88035fb079bad9a66b36fe5e861017af568b8f4c
    Signed-off-by: Wight554 <wight554@gmail.com>

--- services/core/java/com/android/server/am/ActivityRecord.java
+++ services/core/java/com/android/server/am/ActivityRecord.java
@@ -150,6 +150,7 @@ import android.content.pm.ActivityInfo;
 import android.content.pm.ApplicationInfo;
 import android.content.res.CompatibilityInfo;
 import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.GraphicBuffer;
 import android.graphics.Rect;
@@ -361,6 +362,9 @@ final class ActivityRecord extends ConfigurationContainer implements AppWindowCo
 
     private boolean mShowWhenLocked;
     private boolean mTurnScreenOn;
+
+    private final float mFullScreenAspectRatio = Resources.getSystem().getFloat(
+                    com.android.internal.R.dimen.config_screenAspectRatio);
 
     /**
      * Temp configs used in {@link #ensureActivityConfiguration(int, boolean)}
@@ -2421,12 +2425,10 @@ final class ActivityRecord extends ConfigurationContainer implements AppWindowCo
     // TODO(b/36505427): Consider moving this method and similar ones to ConfigurationContainer.
     private void computeBounds(Rect outBounds) {
         outBounds.setEmpty();
-        final float maxAspectRatio = info.maxAspectRatio;
 
-        if (service.mWindowManager.isGestureButtonEnabled()) {
-            return;
-        }
-
+        final boolean higherAspectRatio = Resources.getSystem().getBoolean(
+                com.android.internal.R.bool.config_haveHigherAspectRatioScreen);
+        final float maxAspectRatio = higherAspectRatio ? mFullScreenAspectRatio : info.maxAspectRatio;
         final ActivityStack stack = getStack();
         if (task == null || stack == null || task.inMultiWindowMode() || maxAspectRatio == 0
                 || isInVrUiMode(getConfiguration())) {
