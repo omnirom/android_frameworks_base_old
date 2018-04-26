9368caa37e9809d31bb2000f413620fadca6d663
enzoo96 <enzoo.site@gmail.com>
2018-04-26 20:02:48 +0000

    [1/2] base: hide indicator text
    Hide charging information and unlock hints
    Kang from: 90f446b30b0deaf32680c947ef960cd702acf0b1
    
    Change-Id: I1cc49cfe7fa9bd41b83e3a96fccbe94421f4e497

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8024,6 +8024,13 @@ public final class Settings {
         public static final String LOCK_QS_DISABLED = "lockscreen_qs_disabled";
 
         /**
+         * Disable indicator text
+         *
+         * @hide
+         */
+        public static final String LOCK_HIDE_INDICATOR_DISPLAY = "lockscreen_hide_indicator_display";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -82,6 +82,7 @@ public class KeyguardIndicationController {
     private int mTransientTextColor;
     private int mInitialTextColor;
     private boolean mVisible;
+    private boolean mVisibleOverwrite;
 
     private boolean mPowerPluggedIn;
     private boolean mPowerCharged;
@@ -182,18 +183,27 @@ public class KeyguardIndicationController {
 
     public void setVisible(boolean visible) {
         mVisible = visible;
-        mIndicationArea.setVisibility(visible ? View.VISIBLE : View.GONE);
-        if (visible) {
+        mIndicationArea.setVisibility(isReallyVisible() ? View.VISIBLE : View.GONE);
+        if (isReallyVisible()) {
             // If this is called after an error message was already shown, we should not clear it.
             // Otherwise the error message won't be shown
             if  (!mHandler.hasMessages(MSG_HIDE_TRANSIENT)) {
                 hideTransientIndication();
             }
             updateIndication();
-        } else if (!visible) {
+        } else if (!isReallyVisible()) {
             // If we unlock and return to keyguard quickly, previous error should not be shown
             hideTransientIndication();
         }
+    }
+
+    private boolean isReallyVisible() {
+        return mVisible && mVisibleOverwrite;
+    }
+
+    public void setVisibleOverwrite(boolean value) {
+        mVisibleOverwrite = value;
+        setVisible(mVisible);
     }
 
     /**
@@ -281,7 +291,7 @@ public class KeyguardIndicationController {
             mWakeLock.setAcquired(false);
         }
 
-        if (mVisible) {
+        if (isReallyVisible()) {
             // Walk down a precedence-ordered list of what indication
             // should be shown based on user or device state
             if (mDozing) {
@@ -382,7 +392,7 @@ public class KeyguardIndicationController {
         @Override
         public void onReceive(Context context, Intent intent) {
             mHandler.post(() -> {
-                if (mVisible) {
+                if (isReallyVisible()) {
                     updateIndication();
                 }
             });
@@ -521,7 +531,7 @@ public class KeyguardIndicationController {
 
         @Override
         public void onUserUnlocked() {
-            if (mVisible) {
+            if (isReallyVisible()) {
                 updateIndication();
             }
         }
