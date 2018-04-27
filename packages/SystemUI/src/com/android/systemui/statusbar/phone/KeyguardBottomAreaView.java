310046d951ca8c40b1f4fbe8529f7c9d2fc685b9
enzoo96 <enzoo.site@gmail.com>
2018-04-27 11:41:35 +0000

    [1/2] base: hide lock screen icon
    Change-Id: If659243269efcc310b5f84df5c543bfe6275a40e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -175,6 +175,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
 
     // omni additions
     private boolean mShowIndicator = true;
+    private boolean mShowLockicon;
 
     public KeyguardBottomAreaView(Context context) {
         this(context, null);
@@ -866,6 +867,8 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
 
     public void setDozing(boolean dozing, boolean animate) {
         mDozing = dozing;
+        boolean mShowLockicon = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.HIDE_LOCKSCREEN_ICON, 0, UserHandle.USER_CURRENT) == 0;
 
         updateCameraVisibility();
         updateLeftAffordanceIcon();
@@ -875,8 +878,8 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
             mLockIcon.setVisibility(INVISIBLE);
             mOverlayContainer.setVisibility(INVISIBLE);
         } else {
-            mLockIcon.setVisibility(VISIBLE);
-            mOverlayContainer.setVisibility(VISIBLE);
+            mLockIcon.setVisibility(mShowLockicon ? View.VISIBLE : View.GONE);
+            mOverlayContainer.setVisibility(mShowLockicon ? View.VISIBLE : View.GONE);
             if (animate) {
                 startFinishDozeAnimation();
             }
