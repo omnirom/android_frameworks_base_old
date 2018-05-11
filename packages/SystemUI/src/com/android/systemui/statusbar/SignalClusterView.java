302f19e1a41e4e4af6206e01ca728fbc769ea4b8
maxwen <max.weninger@gmail.com>
2018-05-11 13:53:04 +0200

    base: SystemUI: add tuner switch to hide  mobile roaming indicator
    in situations like EU its really no longer needed to
    show roaming that prominent. if people screw up and
    have to pay enormous phone bills we cant help them
    
    Change-Id: I3992777c3ecac32244798e607b081f4fc3104001

--- packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
@@ -66,6 +66,7 @@ public class SignalClusterView extends LinearLayout implements NetworkController
     private static final String SLOT_WIFI = "wifi";
     private static final String SLOT_ETHERNET = "ethernet";
     private static final String SLOT_VPN = "vpn";
+    private static final String SLOT_ROAMING = "roaming";
 
     private final NetworkController mNetworkController;
     private final SecurityController mSecurityController;
@@ -119,6 +120,7 @@ public class SignalClusterView extends LinearLayout implements NetworkController
     private boolean mActivityEnabled;
     private boolean mForceBlockWifi;
     private boolean mBlockVpn;
+    private boolean mBlockRoaming;
 
     private final IconLogger mIconLogger = Dependency.get(IconLogger.class);
 
@@ -174,13 +176,16 @@ public class SignalClusterView extends LinearLayout implements NetworkController
         boolean blockWifi = blockList.contains(SLOT_WIFI);
         boolean blockEthernet = blockList.contains(SLOT_ETHERNET);
         boolean blockVpn = blockList.contains(SLOT_VPN);
+        boolean blockRoaming = blockList.contains(SLOT_ROAMING);
 
         if (blockAirplane != mBlockAirplane || blockMobile != mBlockMobile
-                || blockEthernet != mBlockEthernet || blockWifi != mBlockWifi) {
+                || blockEthernet != mBlockEthernet || blockWifi != mBlockWifi
+                || blockRoaming != mBlockRoaming) {
             mBlockAirplane = blockAirplane;
             mBlockMobile = blockMobile;
             mBlockEthernet = blockEthernet;
             mBlockWifi = blockWifi || mForceBlockWifi;
+            mBlockRoaming = blockRoaming;
             // Re-register to get new callbacks.
             mNetworkController.removeCallback(this);
             mNetworkController.addCallback(this);
@@ -318,7 +323,7 @@ public class SignalClusterView extends LinearLayout implements NetworkController
         state.mMobileDescription = statusIcon.contentDescription;
         state.mMobileTypeDescription = typeContentDescription;
         state.mIsMobileTypeIconWide = statusType != 0 && isWide;
-        state.mRoaming = roaming;
+        state.mRoaming = roaming && !mBlockRoaming;
         state.mActivityIn = activityIn && mActivityEnabled;
         state.mActivityOut = activityOut && mActivityEnabled;
 
