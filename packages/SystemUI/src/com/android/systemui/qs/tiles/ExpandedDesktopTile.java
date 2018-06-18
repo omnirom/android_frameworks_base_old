0b06b509cd61b0e99d4da9b29d33dde64fa746f1
maxwen <max.weninger@gmail.com>
2018-06-18 23:35:56 +0200

    base: SystemUI: add expanded desktop qs tile
    This is independent of the expanded desktop setting
    and will not overwrite app level settings but add
    a global override that can be used to quickly toggle
    expanded desktop
    
    Change-Id: I4492da6f9ea81696c6d2a7d210f36f489a8ad811

--- packages/SystemUI/src/com/android/systemui/qs/tiles/ExpandedDesktopTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/ExpandedDesktopTile.java
@@ -19,8 +19,10 @@ package com.android.systemui.qs.tiles;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.provider.Settings;
+import android.provider.Settings.Global;
 import android.service.quicksettings.Tile;
 
+import com.android.systemui.qs.GlobalSetting;
 import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.tileimpl.QSTileImpl;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
@@ -28,21 +30,20 @@ import com.android.systemui.R;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
-public class NavbarDisableTile extends QSTileImpl<BooleanState> {
-    private boolean mNavbarKeysDisabled;
-    private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_navbar_disable_on);
+public class ExpandedDesktopTile extends QSTileImpl<BooleanState> {
 
-    public NavbarDisableTile(QSHost host) {
+    private final GlobalSetting mSetting;
+    private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_expanded_desktop_on);
+
+    public ExpandedDesktopTile(QSHost host) {
         super(host);
-        mNavbarKeysDisabled = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.NAVIGATION_BAR_SHOW, 1) == 0;
-    }
 
-    @Override
-    public boolean isAvailable() {
-        //final boolean deviceKeys = mContext.getResources().getBoolean(
-        //        com.android.internal.R.bool.config_showNavigationBar);
-        return true;
+        mSetting = new GlobalSetting(mContext, mHandler, Global.OVERRIDE_POLICY_CONTROL) {
+            @Override
+            protected void handleValueChanged(int value) {
+                handleRefreshState(value);
+            }
+        };
     }
 
     @Override
@@ -52,10 +53,7 @@ public class NavbarDisableTile extends QSTileImpl<BooleanState> {
 
     @Override
     public void handleClick() {
-        mNavbarKeysDisabled = !mNavbarKeysDisabled;
-        Settings.System.putInt(mContext.getContentResolver(),
-                Settings.System.NAVIGATION_BAR_SHOW,
-                mNavbarKeysDisabled ? 0 : 1);
+        setEnabled(!mState.value);
         refreshState();
     }
 
@@ -66,26 +64,48 @@ public class NavbarDisableTile extends QSTileImpl<BooleanState> {
 
     @Override
     public CharSequence getTileLabel() {
-        return mContext.getString(R.string.quick_settings_navbar_disable_label);
+        return mContext.getString(R.string.quick_settings_expanded_desktop_label);
+    }
+
+    private void setEnabled(boolean enabled) {
+        Settings.Global.putInt(mContext.getContentResolver(),
+                Settings.Global.OVERRIDE_POLICY_CONTROL,
+                enabled ? 1 : 0);
     }
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
+        if (mSetting == null) {
+            return;
+        }
+        final int value = arg instanceof Integer ? (Integer)arg : mSetting.getValue();
+        final boolean desktopExpanded = value != 0;
         if (state.slash == null) {
             state.slash = new SlashState();
         }
         state.icon = mIcon;
-        state.value = mNavbarKeysDisabled;
-        state.slash.isSlashed = state.value;
-        state.label = mContext.getString(R.string.quick_settings_navbar_disable_label);
-        if (mNavbarKeysDisabled) {
+        state.value = desktopExpanded;
+        state.slash.isSlashed = !state.value;
+        state.label = mContext.getString(R.string.quick_settings_expanded_desktop_label);
+        if (desktopExpanded) {
             state.contentDescription =  mContext.getString(
-                    R.string.accessibility_quick_settings_navbar_disable_on);
-            state.state = Tile.STATE_INACTIVE;
+                    R.string.accessibility_quick_settings_expanded_desktop_on);
+            state.state = Tile.STATE_ACTIVE;
         } else {
             state.contentDescription =  mContext.getString(
-                    R.string.accessibility_quick_settings_navbar_disable_off);
-            state.state = Tile.STATE_ACTIVE;
+                    R.string.accessibility_quick_settings_expanded_desktop_off);
+            state.state = Tile.STATE_INACTIVE;
+        }
+    }
+
+    @Override
+    protected String composeChangeAnnouncement() {
+        if (mState.value) {
+            return mContext.getString(
+                    R.string.accessibility_quick_settings_expanded_desktop_on);
+        } else {
+            return mContext.getString(
+                    R.string.accessibility_quick_settings_expanded_desktop_off);
         }
     }
 
