32a7cdf77dc9669839a58a62b818c567c7c1472e
Marko Man <darkobas@gmail.com>
2018-09-17 13:59:08 +0200

    policy: add more orientation sanity checks
    OP sensors still report invalid values, which can cause NPE
    
    Change-Id: I1cc4643aa015193af62729d3bbd53418290a221d

--- services/core/java/com/android/server/policy/WindowOrientationListener.java
+++ services/core/java/com/android/server/policy/WindowOrientationListener.java
@@ -1079,6 +1079,10 @@ public abstract class WindowOrientationListener {
 
         public int evaluateRotationChangeLocked() {
             unscheduleRotationEvaluationLocked();
+            // maxwen: op6_sensor_tweak
+            if (mDesiredRotation > 3) {
+                return -1;
+            }
             if (mDesiredRotation == mProposedRotation) {
                 return -1;
             }
