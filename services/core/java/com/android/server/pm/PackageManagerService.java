5be7b9441e499fedd0a264efaab1f79be4d37d80
gudenau <gudenau>
2018-09-25 09:44:26 +0200

    core: Add support for MicroG
    -fake signatures, enabled per app by dynamic permission
    
    Change-Id: I84fc7e5c606f4b57012d948a4cc6cb521db6b03e

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -3331,8 +3331,9 @@ public class PackageManagerService extends IPackageManager.Stub
                         || ArrayUtils.isEmpty(p.getRequestedPermissions())) ? Collections.emptySet()
                         : mPermissionManager.getGrantedPermissions(ps.name, userId);
 
-                PackageInfo packageInfo = PackageInfoUtils.generate(p, gids, flags,
-                        ps.firstInstallTime, ps.lastUpdateTime, permissions, state, userId, ps);
+                PackageInfo packageInfo = mayFakeSignature(p, PackageInfoUtils.generate(p, gids, flags,
+                        ps.firstInstallTime, ps.lastUpdateTime, permissions, state, userId, ps),
+                        permissions);
 
                 if (packageInfo == null) {
                     return null;
@@ -3367,6 +3368,24 @@ public class PackageManagerService extends IPackageManager.Stub
             } else {
                 return null;
             }
+        }
+
+        private PackageInfo mayFakeSignature(AndroidPackage p, PackageInfo pi,
+                Set<String> permissions) {
+            try {
+                if (permissions.contains("android.permission.FAKE_PACKAGE_SIGNATURE")
+                        && p.getTargetSdkVersion() > Build.VERSION_CODES.LOLLIPOP_MR1
+                        && p.getMetaData() != null) {
+                    String sig = p.getMetaData().getString("fake-signature");
+                    if (sig != null) {
+                        pi.signatures = new Signature[] {new Signature(sig)};
+                    }
+                }
+            } catch (Throwable t) {
+                // We should never die because of any failures, this is system code!
+                Log.w("PackageManagerService.FAKE_PACKAGE_SIGNATURE", t);
+            }
+            return pi;
         }
 
         public final PackageInfo getPackageInfo(String packageName, int flags, int userId) {
