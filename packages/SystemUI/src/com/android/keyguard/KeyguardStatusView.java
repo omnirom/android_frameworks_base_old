b9934bc4b9c5d63f73c772fa9469b92496121fd9
maxwen <max.weninger@gmail.com>
2018-09-27 13:26:05 +0200

    base: SystemUI: fix keyguard weather color update
    Change-Id: I9bb9861d824b44fb2d1e91dd61e731838a0c84ac

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -457,6 +457,9 @@ public class KeyguardStatusView extends GridLayout implements
         mKeyguardSlice.setDarkAmount(mDarkAmount);
         mClockView.setTextColor(blendedTextColor);
         mClockSeparator.setBackgroundColor(blendedTextColor);
+        if (mWeatherView != null) {
+            mWeatherView.blendARGB(mDarkAmount);
+        }
     }
 
     private void layoutOwnerInfo() {
