6b9587c0b04c4e48037cbae9601eab0d0fbb9e6b
aviraxp <416810799@qq.com>
2018-10-04 05:38:19 +0000

    ViewRootImpl: break out the loop when NPE caught
    * current gestures implementation here may lead to system hang
    
    Change-Id: Ia32e92de66f8c2d43e9dd73e604c2bd258f5948b

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -4303,6 +4303,7 @@ public final class ViewRootImpl implements ViewParent,
                             k++;
                         } catch (NullPointerException e) {
                             Log.e(ViewRootImpl.TAG, "mView does not exist, discard points. " + e);
+                            break;
                         }
                     }
                 } break;
