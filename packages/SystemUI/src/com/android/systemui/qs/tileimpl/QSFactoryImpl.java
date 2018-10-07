fcec7f8594f12d98be7746c63fa32362e340d3a0
maxwen <max.weninger@gmail.com>
2018-10-07 16:08:50 +0200

    base: SystemUI: add OmniRecord based qs tile
    Change-Id: Id30c323b20a96566c6ad993e47dcf75fae2d2ebd

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -41,6 +41,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WeatherTile;
 import com.android.systemui.qs.tiles.WifiTile;
@@ -111,6 +112,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new AdbOverNetworkTile(mHost);
             case "weather":
                 return new WeatherTile(mHost);
+            case "scr":
+                return new ScreenrecordTile(mHost);
         }
 
         // Intent tiles.
