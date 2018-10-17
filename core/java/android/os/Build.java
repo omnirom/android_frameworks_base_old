3bc0cf1c2107e580d5424b098cad1b1b08c30dbd
Marko Man <darkobas@gmail.com>
2018-10-17 10:22:17 +0200

    [2/2] base: allow device to override *caugh*caugh* prop
    Change-Id: Ib51a5e5ae87b004053dcf1c01d8aa6cf3ef63b65

--- core/java/android/os/Build.java
+++ core/java/android/os/Build.java
@@ -213,6 +213,12 @@ public class Build {
                 "ro.build.version.security_patch", "");
 
         /**
+         * The user-visible omni security patch level.
+         */
+        public static final String SECURITY_PATCH_OMNI = SystemProperties.get(
+                "ro.omni.version.security_patch", "");
+
+        /**
          * The user-visible SDK version of the framework in its raw String
          * representation; use {@link #SDK_INT} instead.
          *
