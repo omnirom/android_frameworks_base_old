00ecec74c85172ec5732ac0bc13d57c4e7cc46de
Wang Han <wanghan1995315@gmail.com>
2018-10-23 08:37:15 +0200

    ViewRootImpl: Break the loop on NPE caught
     * Otherwise, the loop will run forever because k never increases, 
       resulting in strange behavior
    
    Change-Id: Iadf8f96d678ae543b47904102e89033e5391b7a3

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -4302,6 +4302,7 @@ public final class ViewRootImpl implements ViewParent,
                             k++;
                         } catch (NullPointerException e) {
                             Log.e(ViewRootImpl.TAG, "mView does not exist, discard points. " + e);
+                            break;
                         }
                     }
                 } break;
