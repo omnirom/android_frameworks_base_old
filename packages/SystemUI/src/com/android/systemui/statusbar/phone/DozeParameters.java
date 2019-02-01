51b56a00a82257b165919b19333fe77d17381d48
Marko Man <darkobas@gmail.com>
2019-02-01 15:43:13 +0100

    [1/2] base: add option to enable AOD on charging only
    [micky387]
    Edited after https://github.com/omnirom/android_frameworks_base/commit/38c73670a48c62b02c8bb466377755dc84e3dc5b
    
    Change-Id: I95bf93e7902e9cedc02726403b9a6ab111c7c669

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/DozeParameters.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/DozeParameters.java
@@ -89,7 +89,6 @@ public class DozeParameters implements
 
     private final Set<Callback> mCallbacks = new HashSet<>();
 
-    private boolean mDozeAlwaysOn;
     private boolean mControlScreenOffAnimation;
     private boolean mIsQuickPickupEnabled;
 
@@ -148,7 +147,6 @@ public class DozeParameters implements
         keyguardUpdateMonitor.registerCallback(mKeyguardVisibilityCallback);
         tunerService.addTunable(
                 this,
-                Settings.Secure.DOZE_ALWAYS_ON,
                 Settings.Secure.ACCESSIBILITY_DISPLAY_INVERSION_ENABLED);
         configurationController.addCallback(this);
         statusBarStateController.addCallback(this);
@@ -264,7 +262,8 @@ public class DozeParameters implements
      * @return {@code true} if enabled and available.
      */
     public boolean getAlwaysOn() {
-        return mDozeAlwaysOn && !mBatteryController.isAodPowerSave();
+        return mAmbientDisplayConfiguration.alwaysOnEnabled(UserHandle.USER_CURRENT)
+                && !mBatteryController.isAodPowerSave();
     }
 
     /**
@@ -422,12 +421,6 @@ public class DozeParameters implements
 
     @Override
     public void onTuningChanged(String key, String newValue) {
-        mDozeAlwaysOn = mAmbientDisplayConfiguration.alwaysOnEnabled(mUserTracker.getUserId());
-
-        if (key.equals(Settings.Secure.DOZE_ALWAYS_ON)) {
-            updateControlScreenOff();
-        }
-
         dispatchAlwaysOnEvent();
     }
 
