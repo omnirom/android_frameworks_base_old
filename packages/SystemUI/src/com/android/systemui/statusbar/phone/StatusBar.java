a00dec8eea52a67573dcdbcb12a395c2bdf21c30
maxwen <max.weninger@gmail.com>
2019-02-04 23:16:49 +0100

    base: disable NIGHT_MODE_IN_BATTERY_SAVER = true
    Change-Id: I42479e5a9de501182dfd9b2264dcc08e919d5946

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -337,7 +337,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     private static final boolean DARK_THEME_IN_NIGHT_MODE = true;
 
     /** Whether to switch the device into night mode in battery saver. */
-    private static final boolean NIGHT_MODE_IN_BATTERY_SAVER = true;
+    private static final boolean NIGHT_MODE_IN_BATTERY_SAVER = false;
 
     /**
      * Never let the alpha become zero for surfaces that draw with SRC - otherwise the RenderNode
