fc6d410a638699af4c8ea89d5e19977e132abca0
maxwen <max.weninger@gmail.com>
2019-02-13 00:13:13 +0100

    SystemUI: Squashed battery temperature, watts and values on keyguard
    base: SystemUI: keyguard show charging watt
    
    Change-Id: I80c0f6d0551cc6ae792e4b8238ca379fef697882
    
    SystemUI: add option to display current on LS and doze
    
    Change-Id: I9afc4b252a996c610ca5e6f5c76b12f079fcd60c
    
    SystemUI: Optimize charging power display code on LS
    
    Fix display of watts and amps values and optimize code
    
    Change-Id: I8b03dced8b8b993cd1f55c0306be9e02f9dc704b
    
    SystemUI: statusbar: Add battery temperature to LS
    
    Temperature divider can be overriden in device overlay:
    
    <integer name="config_battTempDivider" translatable="false">10</integer>
    
    Also move all charging details one seperate line below and pretti-fy output.
    
    Change-Id: I14d1f6c6e27ecf0bdf3081fa968021ed01eade9d
    
    SystemUI: dont unnecessarily split battery indication
    
    if you have FOD space is really tight down there
    
    Change-Id: I7f6966dbcf56755ba82660a2449c13ab0bc62fa0

--- packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
+++ packages/SettingsLib/src/com/android/settingslib/fuelgauge/BatteryStatus.java
@@ -16,6 +16,7 @@
 
 package com.android.settingslib.fuelgauge;
 
+import static android.os.BatteryManager.BATTERY_HEALTH_OVERHEAT;
 import static android.os.BatteryManager.BATTERY_HEALTH_UNKNOWN;
 import static android.os.BatteryManager.BATTERY_STATUS_FULL;
 import static android.os.BatteryManager.BATTERY_STATUS_UNKNOWN;
@@ -26,6 +27,7 @@ import static android.os.BatteryManager.EXTRA_MAX_CHARGING_VOLTAGE;
 import static android.os.BatteryManager.EXTRA_PLUGGED;
 import static android.os.BatteryManager.EXTRA_PRESENT;
 import static android.os.BatteryManager.EXTRA_STATUS;
+import static android.os.BatteryManager.EXTRA_TEMPERATURE;
 
 import android.content.Context;
 import android.content.Intent;
@@ -54,15 +56,19 @@ public class BatteryStatus {
     public final int plugged;
     public final int health;
     public int maxChargingWattage;
+    public final double currBatteryTemp;
+    public final double currChargingVolt;
     public final boolean mPresent;
 
     public BatteryStatus(int status, int level, int plugged, int health,
-            int maxChargingWattage) {
+            int maxChargingWattage, double currChargingVolt, double currBatteryTemp) {
         this.status = status;
         this.level = level;
         this.plugged = plugged;
         this.health = health;
         this.maxChargingWattage = maxChargingWattage;
+        this.currChargingVolt = currChargingVolt;
+        this.currBatteryTemp = currBatteryTemp;
         this.mPresent = true;
     }
 
@@ -74,8 +80,9 @@ public class BatteryStatus {
         final int maxChargingMicroAmp = batteryChangedIntent.getIntExtra(EXTRA_MAX_CHARGING_CURRENT,
                 -1);
         int maxChargingMicroVolt = batteryChangedIntent.getIntExtra(EXTRA_MAX_CHARGING_VOLTAGE, -1);
+        currBatteryTemp = batteryChangedIntent.getIntExtra(EXTRA_TEMPERATURE, -1);
 
-        if (maxChargingMicroVolt <= 0) {
+        if ((maxChargingMicroVolt <= 0)) {
             maxChargingMicroVolt = DEFAULT_CHARGING_VOLTAGE_MICRO_VOLT;
         }
         if (maxChargingMicroAmp > 0) {
@@ -83,12 +90,16 @@ public class BatteryStatus {
             // to maintain precision equally on both factors.
             maxChargingWattage = (maxChargingMicroAmp / 1000)
                     * (maxChargingMicroVolt / 1000);
+            currChargingVolt = maxChargingMicroVolt;
         } else {
             maxChargingWattage = -1;
+            currChargingVolt = -1;
         }
         if (DEBUG) Log.d(TAG, "maxChargingMicroVolt = " + maxChargingMicroVolt
                 + " maxChargingMicroAmp = " + maxChargingMicroAmp
-                + " maxChargingWattage = " + maxChargingWattage);
+                + " maxChargingWattage = " + maxChargingWattage
+                + " currChargingVolt = " + currChargingVolt
+                + " currBatteryTemp = " + currBatteryTemp);
         mPresent = batteryChangedIntent.getBooleanExtra(EXTRA_PRESENT, true);
     }
 
@@ -134,6 +145,15 @@ public class BatteryStatus {
     }
 
     /**
+     * Whether battery is overheated.
+     *
+     * @return true if battery is overheated
+     */
+    public boolean isOverheated() {
+        return health == BATTERY_HEALTH_OVERHEAT;
+    }
+
+    /**
      * Return current chargin speed is fast, slow or normal.
      *
      * @return the charing speed
@@ -172,7 +192,9 @@ public class BatteryStatus {
     @Override
     public String toString() {
         return "BatteryStatus{status=" + status + ",level=" + level + ",plugged=" + plugged
-                + ",health=" + health + ",maxChargingWattage=" + maxChargingWattage + "}";
+                + ",health=" + health + ",maxChargingWattage=" + maxChargingWattage
+                + ",currChargingVolt=" + currChargingVolt
+                + ",currBatteryTemp=" + currBatteryTemp + "}";
     }
 
     public boolean isBatteryPresent() {
