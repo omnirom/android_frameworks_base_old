fe121be078c1fb0506ac19a6cba7dae06719a93d
phhusson <phh@phh.me>
2019-04-05 16:44:18 +0200

    Initial support for OnePlus in-display fingerprint sensor
    Tested on OnePlus 6T.
    
    Here are the list of things that are device-specific:
    - Display modes
    - fingerprint control (tell the fingerprint sensor the green circle is
    ready, and what is its brightness)
    
    Omni : Adapted for OnePlus
    TODO: read circle position and size from overlays
    
    Original commit : https://github.com/phhusson/platform_frameworks_base/commit/64b2e6d1fc378ac13783f5002e5a9d9379e75f3b
    
    Change-Id: Ia4e0afeb3a1fa8c53d2838ece989aa9c5badd84f

--- services/core/java/com/android/server/statusbar/StatusBarManagerService.java
+++ services/core/java/com/android/server/statusbar/StatusBarManagerService.java
@@ -597,6 +597,15 @@ public class StatusBarManagerService extends IStatusBarService.Stub {
     }
 
     @Override
+    public void handleInDisplayFingerprintView(boolean show, boolean isEnrolling) {
+        if (mBar != null) {
+            try {
+                mBar.handleInDisplayFingerprintView(show, isEnrolling);
+            } catch (RemoteException ex) {}
+        }
+    }
+
+    @Override
     public void toggleCameraFlash() {
         if (mBar != null) {
             try {
