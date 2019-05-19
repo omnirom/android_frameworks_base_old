9e1a1ad20a75dce948d008c9a1802e3978dfcc5d
Rabih Salamey <somcom3@gmail.com>
2019-05-19 03:40:03 -0400

    base: add camera flash toggle api
        * accepts a boolean to enable/disable camera torch
        * checks if camera flash is present
    
    Change-Id: I4bb47dfa650fb9424db545161a1d7571649f37f7
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- services/core/java/com/android/server/statusbar/StatusBarManagerService.java
+++ services/core/java/com/android/server/statusbar/StatusBarManagerService.java
@@ -676,6 +676,16 @@ public class StatusBarManagerService extends IStatusBarService.Stub implements D
     }
 
     @Override
+    public void toggleCameraFlashState(boolean enable) {
+        if (mBar != null) {
+            try {
+                mBar.toggleCameraFlashState(enable);
+            } catch (RemoteException ex) {
+            }
+        }
+    }
+
+    @Override
     public void disable(int what, IBinder token, String pkg) {
         disableForUser(what, token, pkg, mCurrentUserId);
     }
