b16a0b20880c1f26032086f4839f14b04a76b156
Vachounet <vachounet@live.fr>
2019-07-10 14:18:03 +0200

    [1/2] base: introduce DisplayModeManager
    * Allow to switch display mode for devices that support multiple mode (Ex.: OnePlus 7 Pro)
    
    Change-Id: I76c20b4394a64cf9901cd67faf39eca421465fce

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -667,6 +667,8 @@ public class WindowManagerService extends IWindowManager.Stub
      */
     private boolean mRotatingSeamlessly = false;
 
+    static DisplayModeManager mDisplayModeManager;
+
     private final class SettingsObserver extends ContentObserver {
         private final Uri mDisplayInversionEnabledUri =
                 Settings.Secure.getUriFor(Settings.Secure.ACCESSIBILITY_DISPLAY_INVERSION_ENABLED);
@@ -3500,6 +3502,8 @@ public class WindowManagerService extends IWindowManager.Stub
 
         // Make sure the last requested orientation has been applied.
         updateRotationUnchecked(false, false);
+
+        mDisplayModeManager = new DisplayModeManager(this, mContext);
     }
 
     private boolean checkBootAnimationCompleteLocked() {
