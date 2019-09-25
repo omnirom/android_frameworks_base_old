d5c79942ed9d8250dc166dd49e12f4d059023bd3
maxwen <max.weninger@gmail.com>
2019-09-25 16:19:49 +0200

    [1/2] base: navigation mode settings
    to configure the height of the back gesture
    
    [micky387]
    Move the onSettingsChanged to the ModeChangedListener into NavigationBar.java
    
    Change-Id: I6ebe0039e250f3b8f0b91b232df5941b58026499

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
@@ -1887,6 +1887,11 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
             setNavBarMode(mode);
             mView.setShouldShowSwipeUpUi(mOverviewProxyService.shouldShowSwipeUpUI());
         }
+
+        @Override
+        public void onSettingsChanged() {
+            mEdgeBackGestureHandler.onSettingsChanged();
+        }
     };
 
     private final Gefingerpoken mTouchHandler = new Gefingerpoken() {
