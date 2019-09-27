f9ff914943ba9f0912e12a00f855011983409de4
maxwen <max.weninger@gmail.com>
2019-09-27 23:13:28 +0200

    base: add CPU info overlay
    includes all modifications and fixes from P
    
    Change-Id: I0e6dee53ebcd864a522cfd3d78a5abb253991a9c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4842,6 +4842,15 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+         * @hide
+         */
+        public static final String OMNI_SHOW_CPU_OVERLAY = "show_cpu_overlay";
+
+        /** @hide */
+        private static final Validator OMNI_SHOW_CPU_OVERLAY_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -4949,6 +4958,7 @@ public final class Settings {
             OMNI_BACK_GESTURE_HEIGHT,
             OMNI_DOUBLE_TAP_SLEEP_GESTURE,
             OMNI_DOUBLE_TAP_SLEEP_LOCKSCREEN,
+            OMNI_SHOW_CPU_OVERLAY,
         };
 
         /**
@@ -5106,6 +5116,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(OMNI_BACK_GESTURE_HEIGHT);
             PRIVATE_SETTINGS.add(OMNI_DOUBLE_TAP_SLEEP_GESTURE);
             PRIVATE_SETTINGS.add(OMNI_DOUBLE_TAP_SLEEP_LOCKSCREEN);
+            PRIVATE_SETTINGS.add(OMNI_SHOW_CPU_OVERLAY);
         }
 
         /**
@@ -5260,6 +5271,8 @@ public final class Settings {
                     OMNI_DOUBLE_TAP_SLEEP_GESTURE_VALIDATOR);
             VALIDATORS.put(OMNI_DOUBLE_TAP_SLEEP_LOCKSCREEN,
                     OMNI_DOUBLE_TAP_SLEEP_LOCKSCREEN_VALIDATOR);
+            VALIDATORS.put(OMNI_SHOW_CPU_OVERLAY,
+                    OMNI_SHOW_CPU_OVERLAY_VALIDATOR);
         }
 
         /**
