3f9a123d3ba3a9dc9f81afa392e820973e4d6b5a
Marko Man <darkobas@gmail.com>
2019-10-08 13:14:11 +0200

    Screen off to skip track: Add Spotify/KDEConn/etc. remote session compatibility
    * It's nice to be able to skip tracks with the volume rocker even when using the app
      as a remote control (e.g. Spotify playing on PC, Chromecast or PS4 and using an
      app running on the phone as a remote controller).
    
    Change-Id: I0c26cace79e30166c9d6e9995a6c994579958971

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1825,6 +1825,12 @@ public class StatusBar extends SystemUI implements DemoMode,
     @Override
     public void handleSystemKey(int key) {
         if (SPEW) Log.d(TAG, "handleNavigationKey: " + key);
+
+        if (KeyEvent.KEYCODE_MEDIA_PREVIOUS == key || KeyEvent.KEYCODE_MEDIA_NEXT == key) {
+            mMediaManager.onSkipTrackEvent(key, mHandler);
+            return;
+        }
+
         if (!mCommandQueue.panelsEnabled() || !mKeyguardMonitor.isDeviceInteractive()
                 || mKeyguardMonitor.isShowing() && !mKeyguardMonitor.isOccluded()) {
             return;
@@ -1850,7 +1856,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                 mMetricsLogger.count(NotificationPanelView.COUNTER_PANEL_OPEN_QS, 1);
             }
         }
-
     }
 
     @Override
