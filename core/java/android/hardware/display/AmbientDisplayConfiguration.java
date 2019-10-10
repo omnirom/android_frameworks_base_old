c0808ea4433b9c9ab4d57087876441bb8a3627b2
kldoc <fred.berbigier@gmail.com>
2019-10-10 22:56:28 +0200

    [1/2] SystemUI: add Pulse and Ambient notification bars
    Add Pulse and Ambient Display bars for notifications
    
    Change-Id: Ie4f1730bb15d0cd4db7ceb3a0281c114707d3789

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -154,7 +154,8 @@ public class AmbientDisplayConfiguration {
      */
     @TestApi
     public boolean alwaysOnEnabled(int user) {
-        return alwaysOnEnabledSetting(user) || alwaysOnChargingEnabled(user);
+        //return alwaysOnEnabledSetting(user) || alwaysOnChargingEnabled(user);
+        return alwaysOnEnabledSetting(user) || alwaysOnChargingEnabled(user) || alwaysOnAmbientLightEnabled(user);
     }
 
     /**
@@ -231,6 +232,14 @@ public class AmbientDisplayConfiguration {
             final boolean dozeOnChargeEnabledNow = boolSettingSystem(Settings.System.OMNI_DOZE_ON_CHARGE_NOW, user, 0);
             return dozeOnChargeEnabledNow && alwaysOnAvailable() && !accessibilityInversionEnabled(user);
         }
+        return false;
+    }
+public boolean alwaysOnAmbientLightEnabled(int user) {
+        final boolean AmbientLightsEnabled = boolSettingSystem(Settings.System.OMNI_AMBIENT_NOTIFICATION_LIGHT_ENABLED, user, 0);
+          if (AmbientLightsEnabled) {
+              boolean AmbientLightsActivated = boolSettingSystem(Settings.System.OMNI_AMBIENT_NOTIFICATION_LIGHT_ACTIVATED, user, 0);
+              return AmbientLightsActivated && !accessibilityInversionEnabled(user) && alwaysOnAvailable();
+          }
         return false;
     }
 }
