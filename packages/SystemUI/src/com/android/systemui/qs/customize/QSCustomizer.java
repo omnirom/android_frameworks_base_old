45b33e80d196b5762e27a5455676aa06b0e7e07b
maxwen <max.weninger@gmail.com>
2019-11-06 00:45:23 +0100

    SystemUI: respect status bar header height also in edit mode
    Change-Id: I3ad4b26139c41d62331e92e0cbd6ed95fbb6353b

--- packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
+++ packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
@@ -94,6 +94,7 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
     private Menu mColumnsSubMenu;
     private Menu mColumnsLandscapeSubMenu;
     private Menu mQsColumnsSubMenu;
+    private boolean mHeaderImageEnabled;
 
     public QSCustomizer(Context context, AttributeSet attrs) {
         super(new ContextThemeWrapper(context, R.style.edit_theme), attrs);
@@ -169,6 +170,10 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         LayoutParams lp = (LayoutParams) mTransparentView.getLayoutParams();
         lp.height = mContext.getResources().getDimensionPixelSize(
                 com.android.internal.R.dimen.quick_qs_offset_height);
+        if (mHeaderImageEnabled) {
+            lp.height += mContext.getResources().getDimensionPixelSize(
+                    R.dimen.qs_header_image_offset);
+        }
         mTransparentView.setLayoutParams(lp);
     }
 
@@ -439,7 +444,7 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         }
     };
 
-    public void updateSettings() {
+    private void updateSettings() {
         final Resources res = mContext.getResources();
         boolean isPortrait = res.getConfiguration().orientation
                 == Configuration.ORIENTATION_PORTRAIT;
@@ -457,7 +462,11 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         mTileAdapter.setHideLabel(!showTitles);
         mLayout.setSpanCount(isPortrait ? columns : columnsLandscape);
         updateColumnsMenu(defaultColumns);
+        mHeaderImageEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.OMNI_STATUS_BAR_CUSTOM_HEADER, 0,
+                UserHandle.USER_CURRENT) == 1;
     }
+
     private void updateColumnsMenu(int defaultColumns) {
         int columns = Settings.System.getIntForUser(
                 mContext.getContentResolver(), Settings.System.OMNI_QS_LAYOUT_COLUMNS, defaultColumns,
