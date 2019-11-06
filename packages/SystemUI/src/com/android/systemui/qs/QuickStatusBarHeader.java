0b3481cb00f301c09c645a9448e0aa2f026daecc
maxwen <max.weninger@gmail.com>
2019-11-06 08:58:39 +0100

    SystemUI: no runtime padding for the top most qs status bar content
    devices take care of things like corner radious cutout
    and padding. So no need to overwrite this here
    
    else we get different padding in e.g. landscape
    
    Change-Id: Ib537a5248b71775e72377901d5232bcbefac3a26

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -462,22 +462,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
     }
 
     @Override
-    public WindowInsets onApplyWindowInsets(WindowInsets insets) {
-        DisplayCutout cutout = insets.getDisplayCutout();
-        Pair<Integer, Integer> padding = PhoneStatusBarView.cornerCutoutMargins(
-                cutout, getDisplay());
-        if (padding == null) {
-            mSystemIconsView.setPaddingRelative(
-                    getResources().getDimensionPixelSize(R.dimen.status_bar_padding_start), 0,
-                    getResources().getDimensionPixelSize(R.dimen.status_bar_padding_end), 0);
-        } else {
-            mSystemIconsView.setPadding(padding.first, 0, padding.second, 0);
-
-        }
-        return super.onApplyWindowInsets(insets);
-    }
-
-    @Override
     @VisibleForTesting
     public void onDetachedFromWindow() {
         setListening(false);
