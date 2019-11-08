caa7802c802e7daa13355f13f812b2bd92013ee8
Marko Man <darkobas@gmail.com>
2019-11-08 23:50:49 +0100

    Revert "base: fix: store omni fingerprint for package data"
    This reverts commit 5d2d168e79dd669cd06cf50bf7f4b73b69f69752.

--- services/core/java/com/android/server/policy/PermissionPolicyService.java
+++ services/core/java/com/android/server/policy/PermissionPolicyService.java
@@ -307,7 +307,7 @@ public final class PermissionPolicyService extends SystemService {
                 /* ignore */
             }
 
-            packageManagerInternal.setRuntimePermissionsFingerPrint(Build.OMNI_FINGERPRINT, userId);
+            packageManagerInternal.setRuntimePermissionsFingerPrint(Build.FINGERPRINT, userId);
         }
     }
 
