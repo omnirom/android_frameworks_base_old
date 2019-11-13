a2633a54be39541ceae1c81111b9f29868d4e6c1
isadore belle <isa@gerrit.omnirom.org>
2019-11-13 04:58:41 +0100

    Revert "SystemUI: keep width on qs expand move"
    This reverts commit 12cd6636b1571fb6241d7a894419332f9df9aa64.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: I7c034ad633c9d434f5a4019ae00a1223aacf7609

--- packages/SystemUI/src/com/android/systemui/qs/TileLayout.java
+++ packages/SystemUI/src/com/android/systemui/qs/TileLayout.java
@@ -91,8 +91,7 @@ public class TileLayout extends ViewGroup implements QSTileLayout {
         mCellMarginHorizontal = res.getDimensionPixelSize(R.dimen.qs_tile_margin_horizontal);
         mCellMarginVertical= res.getDimensionPixelSize(R.dimen.qs_tile_margin_vertical);
         mCellMarginTop = res.getDimensionPixelSize(R.dimen.qs_tile_margin_top);
-        // is already applied in the layout xml
-        mSidePadding = 0; //res.getDimensionPixelOffset(R.dimen.qs_tile_layout_margin_side);
+        mSidePadding = res.getDimensionPixelOffset(R.dimen.qs_tile_layout_margin_side);
         mMaxAllowedRows = Math.max(1, getResources().getInteger(R.integer.quick_settings_max_rows));
         updateSettings();
         return false;
