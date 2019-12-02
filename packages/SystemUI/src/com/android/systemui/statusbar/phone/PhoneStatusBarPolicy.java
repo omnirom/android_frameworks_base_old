475b406190f680b263f985237df07e7fbf0b8d65
maxwen <max.weninger@gmail.com>
2019-12-02 13:47:29 +0100

    SystemUI: tuner: add tuner switch to hide status bar location
    and fix useless big side margins of location icon
    
    Change-Id: I034808ad5136299054458567a0f03a47be910a23

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -95,7 +95,7 @@ public class PhoneStatusBarPolicy
     private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
 
     public static final int LOCATION_STATUS_ICON_ID =
-            com.android.internal.R.drawable.perm_group_location;
+            R.drawable.stat_sys_location;
 
     private final String mSlotCast;
     private final String mSlotHotspot;
@@ -108,7 +108,7 @@ public class PhoneStatusBarPolicy
     private final String mSlotRotate;
     private final String mSlotHeadset;
     private final String mSlotDataSaver;
-    private final String mSlotLocation;
+    private final String mSlotLocation = "location";
     private final String mSlotSensorsOff;
 
     private final Context mContext;
@@ -172,7 +172,6 @@ public class PhoneStatusBarPolicy
         mSlotRotate = context.getString(com.android.internal.R.string.status_bar_rotate);
         mSlotHeadset = context.getString(com.android.internal.R.string.status_bar_headset);
         mSlotDataSaver = context.getString(com.android.internal.R.string.status_bar_data_saver);
-        mSlotLocation = context.getString(com.android.internal.R.string.status_bar_location);
         mSlotSensorsOff = context.getString(com.android.internal.R.string.status_bar_sensors_off);
 
         // listen for broadcasts
