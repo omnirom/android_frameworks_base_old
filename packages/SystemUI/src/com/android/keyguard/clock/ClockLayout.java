1a1496980db6daf24a903a92b6190d9be7feac64
maxwen <max.weninger@gmail.com>
2020-01-07 19:50:18 +0100

    SystemUI: custom keyguard clock kick off
    allow custom clock to be in the same position as the default
    clock. Add bold hour clock back from the days where we
    had custom keyguard clock already
    
    Change-Id: I11bb2b0b70327bfc824b126f07043db49262c13b

--- packages/SystemUI/src/com/android/keyguard/clock/ClockLayout.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockLayout.java
@@ -45,6 +45,7 @@ public class ClockLayout extends FrameLayout {
     private int mBurnInPreventionOffsetY;
 
     private float mDarkAmount;
+    private boolean mBurnInProtection;
 
     public ClockLayout(Context context) {
         this(context, null);
@@ -90,6 +91,9 @@ public class ClockLayout extends FrameLayout {
     }
 
     private void positionChildren() {
+        if (!mBurnInProtection || mAnalogClock == null) {
+            return;
+        }
         final float offsetX = MathUtils.lerp(0f,
                 getBurnInOffset(mBurnInPreventionOffsetX * 2, true) - mBurnInPreventionOffsetX,
                 mDarkAmount);
