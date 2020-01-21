a2e24d4ed8d2108dd1a304f30c1a1e22a0c8ace3
maxwen <max.weninger@gmail.com>
2020-01-21 16:45:09 +0100

    [1/2] SystemUI: dont block IME space for back gesture
    by default if ime is visible the back gesture region starts
    above the ime height. add an option to disable that
    behaviour so that the back gesture stays the same
    
    Change-Id: I576bb480059b8a531dd7f99f6b4f357fe310969f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -81,7 +81,11 @@ public class EdgeBackGestureHandler implements DisplayListener {
         @Override
         public void onImeVisibilityChanged(boolean imeVisible, int imeHeight) {
             // No need to thread jump, assignments are atomic
-            mImeHeight = imeVisible ? imeHeight : 0;
+            if (mBlockImeSpace) {
+                mImeHeight = imeVisible ? imeHeight : 0;
+            } else {
+                mImeHeight = 0;
+            }
             // TODO: Probably cancel any existing gesture
         }
 
@@ -171,6 +175,8 @@ public class EdgeBackGestureHandler implements DisplayListener {
 
     // omni additions start
     private int mEdgeHeight;
+    // should back gesture be movewd above ime if its visible
+    private boolean mBlockImeSpace = true;
 
     public EdgeBackGestureHandler(Context context, OverviewProxyService overviewProxyService) {
         final Resources res = context.getResources();
@@ -244,7 +250,9 @@ public class EdgeBackGestureHandler implements DisplayListener {
 
     public void onSettingsChanged() {
         updateEdgeHeightValue();
-    }
+        mBlockImeSpace = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.OMNI_BACK_GESTURE_BLOCK_IME, 1, UserHandle.USER_CURRENT) == 1;
+}
 
     private void disposeInputChannel() {
         if (mInputEventReceiver != null) {
