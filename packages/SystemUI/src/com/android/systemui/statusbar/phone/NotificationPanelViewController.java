6d97f9c2cf600d893688f0b402b52482dc5950fd
maxwen <max.weninger@gmail.com>
2020-01-29 23:32:31 +0100

    SystemUI: kill any leftover notification pulse on new pulse start
    happens e.g with running aod pulse - screen turned off/on by
    covered proxi - we never enter setDozing(false) then
    
    Change-Id: Ib25e13757444841e5e2aa987b526bc7f43025bd8

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -2962,6 +2962,10 @@ public class NotificationPanelViewController extends PanelViewController {
                     if (pulseLights) {
                         mPulseLightsView.animateNotification(true);
                         mPulseLightsView.setVisibility(View.VISIBLE);
+                    } else {
+                        // bars can still be visible as leftover
+                        // but we dont want them here
+                        mPulseLightsView.setVisibility(View.GONE);
                     }
                     if (ambientLights) {
                         mPulseLightHandled = false;
