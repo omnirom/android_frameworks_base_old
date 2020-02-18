569a8bc3f01b3f27a2b224ddf441dea113f74223
maxwen <max.weninger@gmail.com>
2020-02-18 23:37:34 +0100

    [1/2] base: expose auto night mode sunset and sunrise values on API
    Change-Id: If393b9fe48b673fa5ac542561bf5de9b345e5949

--- services/core/java/com/android/server/display/color/ColorDisplayService.java
+++ services/core/java/com/android/server/display/color/ColorDisplayService.java
@@ -87,6 +87,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeParseException;
+import java.util.Calendar;
 
 /**
  * Controls the display's color transforms.
@@ -778,6 +779,28 @@ public final class ColorDisplayService extends SystemService {
                     R.integer.config_defaultNightDisplayCustomEndTime);
         }
         return new Time(LocalTime.ofSecondOfDay(endTimeValue / 1000));
+    }
+
+    private Time getNightDisplayAutoStartTimeInternal() {
+        TwilightManager mTwilightManager = getLocalService(TwilightManager.class);
+        TwilightState state = mTwilightManager.getLastTwilightState();
+        if (state != null) {
+            final Calendar c = Calendar.getInstance();
+            c.setTimeInMillis(state.sunsetTimeMillis());
+            return new Time(LocalTime.of(c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE)));
+        }
+        return null;
+    }
+
+    private Time getNightDisplayAutoEndTimeInternal() {
+        TwilightManager mTwilightManager = getLocalService(TwilightManager.class);
+        TwilightState state = mTwilightManager.getLastTwilightState();
+        if (state != null) {
+            final Calendar c = Calendar.getInstance();
+            c.setTimeInMillis(state.sunriseTimeMillis());
+            return new Time(LocalTime.of(c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE)));
+        }
+        return null;
     }
 
     private boolean setNightDisplayCustomEndTimeInternal(Time endTime) {
@@ -1662,6 +1685,26 @@ public final class ColorDisplayService extends SystemService {
             final long token = Binder.clearCallingIdentity();
             try {
                 dumpInternal(pw);
+            } finally {
+                Binder.restoreCallingIdentity(token);
+            }
+        }
+
+        @Override
+        public Time getNightDisplayAutoStartTime() {
+            final long token = Binder.clearCallingIdentity();
+            try {
+                return getNightDisplayAutoStartTimeInternal();
+            } finally {
+                Binder.restoreCallingIdentity(token);
+            }
+        }
+
+        @Override
+        public Time getNightDisplayAutoEndTime() {
+            final long token = Binder.clearCallingIdentity();
+            try {
+                return getNightDisplayAutoEndTimeInternal();
             } finally {
                 Binder.restoreCallingIdentity(token);
             }
