d5d3bfc5a018c3b1c8d192a4dc38ea322c7ac812
maxwen <max.weninger@gmail.com>
2020-02-25 00:54:30 +0100

    SystemUI: fix possible case where aod stays on after notification pulse
    Change-Id: I0e6fdbf358a429547b5f2909e728ebb7d7f881ba

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -2989,7 +2989,6 @@ public class NotificationPanelViewController extends PanelViewController {
                     }
                     if (ambientLights) {
                         mPulseLightHandled = false;
-                        mAmbientPulseLightRunning = false;
                         // tell power manager that we want to enable aod
                         // must do that here already not on pulsing = false
                         Settings.System.putIntForUser(mView.getContext().getContentResolver(),
