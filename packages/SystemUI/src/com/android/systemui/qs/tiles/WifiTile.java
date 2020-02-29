1e9500c2568169ab5df2e7e33884c25d0f58c37e
Joe Maples <joe@maples.dev>
2020-02-29 09:54:58 -0500

    QS: Use Settings.Panel intents for WiFi and NFC
    Change-Id: Id8bf912ac04600406d77d2527f57ce5812240925
    Signed-off-by: Joe Maples <joe@maples.dev>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/WifiTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/WifiTile.java
@@ -55,7 +55,7 @@ import javax.inject.Inject;
 
 /** Quick settings tile: Wifi **/
 public class WifiTile extends QSTileImpl<SignalState> {
-    private static final Intent WIFI_SETTINGS = new Intent(Settings.ACTION_WIFI_SETTINGS);
+    private static final Intent WIFI_SETTINGS = new Intent(Settings.Panel.ACTION_WIFI);
 
     protected final NetworkController mController;
     private final AccessPointController mWifiController;
