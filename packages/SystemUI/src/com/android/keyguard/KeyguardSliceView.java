7e1f4cdc3b6272df581758481d74973d88e522d5
maxwen <max.weninger@gmail.com>
2020-03-23 01:40:44 +0100

    [1/2] SystemUI: change layout of keyguard status slice view
    reduce size of media header
    show media artist as subtitle
    keep full date line
    
    Change-Id: I7b1045aebd0ef25d3eca45e178eaf0860c069764

--- packages/SystemUI/src/com/android/keyguard/KeyguardSliceView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardSliceView.java
@@ -94,6 +94,7 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
     private Uri mKeyguardSliceUri;
     @VisibleForTesting
     TextView mTitle;
+    TextView mSubTitle;
     private Row mRow;
     private int mTextColor;
     private float mDarkAmount = 0;
@@ -112,6 +113,7 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
     private final int mRowPadding;
     private float mRowTextSize;
     private float mRowWithHeaderTextSize;
+    private float mHeaderTextSize;
 
     private static boolean mKeyguardTransitionAnimations = true;
 
@@ -147,6 +149,7 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
     protected void onFinishInflate() {
         super.onFinishInflate();
         mTitle = findViewById(R.id.title);
+        mSubTitle = findViewById(R.id.subTitle);
         mRow = findViewById(R.id.row);
         mTextColor = Utils.getColorAttrDefaultColor(mContext, R.attr.wallpaperTextColor);
         mIconSize = (int) mContext.getResources().getDimension(R.dimen.widget_icon_size);
@@ -155,6 +158,8 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
                 R.dimen.widget_label_font_size);
         mRowWithHeaderTextSize = mContext.getResources().getDimensionPixelSize(
                 R.dimen.header_row_font_size);
+        mHeaderTextSize = mContext.getResources().getDimensionPixelSize(
+                R.dimen.header_font_size);
         mTitle.setOnClickListener(this);
     }
 
@@ -220,6 +225,7 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
         }
         if (!mHasHeader) {
             mTitle.setVisibility(GONE);
+            mSubTitle.setVisibility(GONE);
         } else {
             mTitle.setVisibility(VISIBLE);
 
@@ -227,6 +233,16 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
             SliceItem mainTitle = header.getTitleItem();
             CharSequence title = mainTitle != null ? mainTitle.getText() : null;
             mTitle.setText(title);
+            mTitle.setTextSize(TypedValue.COMPLEX_UNIT_PX, mHeaderTextSize);
+
+            SliceItem subTitle = header.getSubtitleItem();
+            if (subTitle != null) {
+                mSubTitle.setVisibility(VISIBLE);
+                CharSequence subTitleText = subTitle.getText();
+                mSubTitle.setText(subTitleText);
+                mSubTitle.setTextSize(TypedValue.COMPLEX_UNIT_PX, mRowWithHeaderTextSize);
+            }
+
             if (header.getPrimaryAction() != null
                     && header.getPrimaryAction().getAction() != null) {
                 mClickActions.put(mTitle, header.getPrimaryAction().getAction());
@@ -264,8 +280,7 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
             final SliceItem titleItem = rc.getTitleItem();
             button.setText(titleItem == null ? null : titleItem.getText());
             button.setContentDescription(rc.getContentDescription());
-            button.setTextSize(TypedValue.COMPLEX_UNIT_PX,
-                    mHasHeader ? mRowWithHeaderTextSize : mRowTextSize);
+            button.setTextSize(TypedValue.COMPLEX_UNIT_PX, mRowTextSize);
 
             Drawable iconDrawable = null;
             SliceItem icon = SliceQuery.find(item.getSlice(),
