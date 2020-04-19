72102333b865f35241166d72d9caf3ac2b0166a7
maxwen <max.weninger@gmail.com>
2020-04-19 23:36:13 +0200

    base: TaskUtils: simplify toggleLastApp
    Change-Id: I740b44c2af3be7f902dd6bc398d0da27db56de66

--- core/java/com/android/internal/util/omni/TaskUtils.java
+++ core/java/com/android/internal/util/omni/TaskUtils.java
@@ -47,61 +47,28 @@ import java.util.List;
 
 public class TaskUtils {
 
-    private static final String LAUNCHER_PACKAGE = "com.android.launcher3";
-    private static final String SYSTEMUI_PACKAGE = "com.android.systemui";
-
-    public static boolean killActiveTask(Context context, int userId) {
-        String defaultHomePackage = resolveCurrentLauncherPackageForUser(
-                context, userId);
-        boolean targetKilled = false;
-        final ActivityManager am = (ActivityManager) context
-                .getSystemService(Activity.ACTIVITY_SERVICE);
-        List<RunningAppProcessInfo> apps = am.getRunningAppProcesses();
-        for (RunningAppProcessInfo appInfo : apps) {
-            int uid = appInfo.uid;
-            // Make sure it's a foreground user application (not system,
-            // root, phone, etc.)
-            if (uid >= Process.FIRST_APPLICATION_UID
-                    && uid <= Process.LAST_APPLICATION_UID
-                    && appInfo.importance == RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
-                if (appInfo.pkgList != null && (appInfo.pkgList.length > 0)) {
-                    for (String pkg : appInfo.pkgList) {
-                        if (!pkg.equals(SYSTEMUI_PACKAGE) && !pkg.equals(defaultHomePackage)) {
-                            am.forceStopPackageAsUser(pkg, userId);
-                            targetKilled = true;
-                            break;
-                        }
-                    }
-                } else {
-                    Process.killProcess(appInfo.pid);
-                    targetKilled = true;
-                }
-            }
-            if (targetKilled) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     public static void toggleLastApp(Context context, int userId) {
-        String defaultHomePackage = resolveCurrentLauncherPackageForUser(context, userId);
+        final ActivityInfo homeInfo = new Intent(Intent.ACTION_MAIN)
+                .addCategory(Intent.CATEGORY_HOME).resolveActivityInfo(context.getPackageManager(), 0);
         final ActivityManager am = (ActivityManager) context
                 .getSystemService(Activity.ACTIVITY_SERVICE);
-        final List<ActivityManager.RecentTaskInfo> tasks = am.getRecentTasks(5,
-                ActivityManager.RECENT_IGNORE_UNAVAILABLE);
-        // lets get enough tasks to find something to switch to
-        // Note, we'll only get as many as the system currently has - up to 5
+        final List<ActivityManager.RecentTaskInfo> tasks = am.getRecentTasks(8,
+                ActivityManager.RECENT_IGNORE_UNAVAILABLE |
+                ActivityManager.RECENT_WITH_EXCLUDED);
+
         int lastAppId = 0;
         for (int i = 1; i < tasks.size(); i++) {
-            final String packageName = tasks.get(i).baseIntent.getComponent()
-                    .getPackageName();
-            if (!packageName.equals(defaultHomePackage)
-                    && !packageName.equals(SYSTEMUI_PACKAGE)) {
-                final ActivityManager.RecentTaskInfo info = tasks.get(i);
-                lastAppId = info.persistentId;
-                break;
+            final ActivityManager.RecentTaskInfo info = tasks.get(i);
+            boolean isExcluded = (info.baseIntent.getFlags() & Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+                    == Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS;
+            if (isExcluded) {
+                continue;
             }
+            if (isCurrentHomeActivity(info.baseIntent.getComponent(), homeInfo)) {
+                continue;
+            }
+            lastAppId = info.persistentId;
+            break;
         }
         if (lastAppId > 0) {
             ActivityOptions options = ActivityOptions.makeCustomAnimation(context,
@@ -114,20 +81,11 @@ public class TaskUtils {
         }
     }
 
-    private static String resolveCurrentLauncherPackageForUser(Context context,
-            int userId) {
-        final Intent launcherIntent = new Intent(Intent.ACTION_MAIN);
-        launcherIntent.addCategory(Intent.CATEGORY_HOME);
-        final PackageManager pm = context.getPackageManager();
-        final ResolveInfo launcherInfo = pm.resolveActivityAsUser(
-                launcherIntent, 0, userId);
-        if (launcherInfo != null) {
-            if (launcherInfo.activityInfo != null
-                    && !launcherInfo.activityInfo.packageName.equals("android")) {
-                return launcherInfo.activityInfo.packageName;
-            }
-        }
-        return LAUNCHER_PACKAGE;
+    private static boolean isCurrentHomeActivity(ComponentName component,
+            ActivityInfo homeInfo) {
+        return homeInfo != null
+                && homeInfo.packageName.equals(component.getPackageName())
+                && homeInfo.name.equals(component.getClassName());
     }
 
     private static int getRunningTask(Context context) {
