a06ab654213a432319763e2242f176ff9bc45485
maxwen <max.weninger@gmail.com>
2020-04-26 17:04:24 +0200

    DO NOT MERGE BFORE DISABLING DEBUG SystemUI: allow unlock with fod also on secondary screen
    if device has fod and for pin pattern and password
    added also a button to show the original unlock method on demand
    
    to prevent the fod to overlay the emergency and switch button
    there are two dimens to configure the position
    
    bottom margin of the switch button relative to global
    bottom margin below e.g. if you want switch button
    and emergency button above and below fod
    by default all is shown above the fod on devices like e.g. op7xx
    <dimen name="keyguard_fod_switch_bottom_margin">20dp</dimen>
    
    global bottom margin the emergency button is moved up
    <dimen name="keyguard_security_container_bottom_margin">70dp</dimen>
    
    Change-Id: Iba189135715953187c50237973d7f0811d593572

--- packages/SystemUI/src/com/android/keyguard/KeyguardHostView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardHostView.java
@@ -19,8 +19,10 @@ package com.android.keyguard;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
 import android.content.res.Resources;
+import android.hardware.biometrics.BiometricSourceType;
 import android.graphics.Canvas;
 import android.media.AudioManager;
 import android.os.SystemClock;
@@ -29,6 +31,7 @@ import android.telephony.TelephonyManager;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.widget.FrameLayout;
 
 import com.android.internal.widget.LockPatternUtils;
@@ -57,6 +60,9 @@ public class KeyguardHostView extends FrameLayout implements SecurityCallback {
     protected LockPatternUtils mLockPatternUtils;
     private OnDismissAction mDismissAction;
     private Runnable mCancelAction;
+    private boolean mHasFod;
+    private boolean mFpcAuth;
+    private KeyguardUpdateMonitor mUpdateMonitor;
 
     private final KeyguardUpdateMonitorCallback mUpdateCallback =
             new KeyguardUpdateMonitorCallback() {
@@ -90,6 +96,64 @@ public class KeyguardHostView extends FrameLayout implements SecurityCallback {
                 }
             }
         }
+
+        @Override
+        public void onBiometricRunningStateChanged(boolean running,
+            BiometricSourceType biometricSourceType) {
+            if (mHasFod && biometricSourceType == BiometricSourceType.FINGERPRINT) {
+                if (DEBUG) Log.d(TAG, "onBiometricRunningStateChanged running = " + running + " mFpcAuth = " + mFpcAuth);
+                if (!running) {
+                    if (!mFpcAuth) {
+                        mSecurityContainer.hideFod();
+                    }
+                } else {
+                    if (!mFpcAuth) {
+                        mSecurityContainer.showFod();
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void onBiometricAuthFailed(BiometricSourceType biometricSourceType) {
+            if (mHasFod && biometricSourceType == BiometricSourceType.FINGERPRINT) {
+                if (DEBUG) Log.d(TAG, "onBiometricAuthFailed");
+                mFpcAuth = false;
+            }
+        }
+
+        @Override
+        public void onBiometricAuthenticated(int userId, BiometricSourceType biometricSourceType) {
+            if (mHasFod && biometricSourceType == BiometricSourceType.FINGERPRINT) {
+                if (DEBUG) Log.d(TAG, "onBiometricAuthenticated");
+                mFpcAuth = true;
+            }
+        }
+
+        @Override
+        public void onFodVisibilityChanged(boolean visible) {
+            if (DEBUG) Log.d(TAG, "onFodVisibilityChanged " + visible);
+            FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) mSecurityContainer.getLayoutParams();
+            int bottomMargin = 0;
+            if (visible) {
+                bottomMargin = getContext().getResources().getDimensionPixelSize(R.dimen.keyguard_security_container_bottom_margin);
+            }
+            lp.setMargins(0, 0, 0, bottomMargin);
+            mSecurityContainer.setLayoutParams(lp);
+        }
+
+        @Override
+        public void onKeyguardVisibilityChanged(boolean visible) {
+            if (DEBUG) Log.d(TAG, "onKeyguardVisibilityChanged " + visible);
+        }
+
+        @Override
+        public void onKeyguardBouncerChanged(boolean isBouncer) {
+            if (DEBUG) Log.d(TAG, "onKeyguardBouncerChanged");
+            if (mHasFod && isFodRunning() && !isBouncer) {
+                mSecurityContainer.showFod();
+            }
+        }
     };
 
     // Whether the volume keys should be handled by keyguard. If true, then
@@ -107,7 +171,9 @@ public class KeyguardHostView extends FrameLayout implements SecurityCallback {
 
     public KeyguardHostView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        KeyguardUpdateMonitor.getInstance(context).registerCallback(mUpdateCallback);
+        mUpdateMonitor = KeyguardUpdateMonitor.getInstance(context);
+        mUpdateMonitor.registerCallback(mUpdateCallback);
+        mHasFod = context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_FOD);
     }
 
     @Override
@@ -155,6 +221,7 @@ public class KeyguardHostView extends FrameLayout implements SecurityCallback {
      */
     public void showPrimarySecurityScreen() {
         if (DEBUG) Log.d(TAG, "show()");
+        mFpcAuth = false;
         mSecurityContainer.showPrimarySecurityScreen(false);
     }
 
@@ -449,5 +516,9 @@ public class KeyguardHostView extends FrameLayout implements SecurityCallback {
 
     public SecurityMode getCurrentSecurityMode() {
         return mSecurityContainer.getCurrentSecurityMode();
+    }
+
+    private boolean isFodRunning() {
+        return mUpdateMonitor.isFingerprintDetectionRunning();
     }
 }
