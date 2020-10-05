cfad752412743867485fe151340f87087312b217
maxwen <max.weninger@gmail.com>
2020-10-06 00:56:24 +0200

    [1/4] base: bring back all changes from Build.FINGERPRINT to Build.TIME
    go now the easy way of just using Build.TIME
    there is no need to add back Build.DATE as we did in 11
    
    Change-Id: I731d762885cd418cce816be8bd1bc4fc0deb65d7

--- services/core/java/com/android/server/pm/ShortcutService.java
+++ services/core/java/com/android/server/pm/ShortcutService.java
@@ -5136,7 +5136,7 @@ public class ShortcutService extends IShortcutService.Stub {
 
     // Injection point.
     String injectBuildFingerprint() {
-        return Build.FINGERPRINT;
+        return String.valueOf(Build.TIME);
     }
 
     final void wtf(String message) {
