30f4ad081af1d822f628be0956066674b84ea302
maxwen <max.weninger@gmail.com>
2020-10-14 17:17:31 +0200

    SystemUI: qs tiles revenge
    -power
    -headsup
    -screenshot
    -nfc
    -aod
    
    Change-Id: I9b69f2ec9a9bcefd7b0ab503d9661fe797b2aa7b

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -27,6 +27,7 @@ import com.android.systemui.plugins.qs.QSTileView;
 import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
+import com.android.systemui.qs.tiles.AODTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CastTile;
@@ -35,12 +36,15 @@ import com.android.systemui.qs.tiles.ColorInversionTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
+import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.PowerButtonTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
+import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WeatherTile;
@@ -80,7 +84,11 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<UiModeNightTile> mUiModeNightTileProvider;
     private final Provider<ScreenRecordTile> mScreenRecordTileProvider;
     //Omni
+    private final Provider<AODTile> mAODTileProvider;
     private final Provider<WeatherTile> mWeatherTileProvider;
+    private final Provider<HeadsUpTile> mHeadsUpTileProvider;
+    private final Provider<ScreenshotTile> mScreenshotTileProvider;
+    private final Provider<PowerButtonTile> mPowerButtonTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
 
@@ -106,7 +114,11 @@ public class QSFactoryImpl implements QSFactory {
             Provider<GarbageMonitor.MemoryTile> memoryTileProvider,
             Provider<UiModeNightTile> uiModeNightTileProvider,
             Provider<ScreenRecordTile> screenRecordTileProvider,
-            Provider<WeatherTile> weatherTileProvider) {
+            Provider<AODTile> aodTileProvider,
+            Provider<WeatherTile> weatherTileProvider,
+            Provider<HeadsUpTile> headsupTileProvider,
+            Provider<ScreenshotTile> screenshotTileProvider,
+            Provider<PowerButtonTile> powerButtonTileProvider) {
         mQsHostLazy = qsHostLazy;
         mWifiTileProvider = wifiTileProvider;
         mBluetoothTileProvider = bluetoothTileProvider;
@@ -129,7 +141,11 @@ public class QSFactoryImpl implements QSFactory {
         mUiModeNightTileProvider = uiModeNightTileProvider;
         mScreenRecordTileProvider = screenRecordTileProvider;
         //Omni
+        mAODTileProvider = aodTileProvider;
         mWeatherTileProvider = weatherTileProvider;
+        mHeadsUpTileProvider = headsupTileProvider;
+        mScreenshotTileProvider = screenshotTileProvider;
+        mPowerButtonTileProvider = powerButtonTileProvider;
     }
 
     public QSTile createTile(String tileSpec) {
@@ -182,8 +198,16 @@ public class QSFactoryImpl implements QSFactory {
             case "screenrecord":
                 return mScreenRecordTileProvider.get();
             // Omni tiles
+            case "aod":
+                return mAODTileProvider.get();
             case "weather":
                 return mWeatherTileProvider.get();
+            case "heads_up":
+                return mHeadsUpTileProvider.get();
+            case "screenshot":
+                return mScreenshotTileProvider.get();
+            case "power":
+                return mPowerButtonTileProvider.get();
         }
 
         // Custom tiles
