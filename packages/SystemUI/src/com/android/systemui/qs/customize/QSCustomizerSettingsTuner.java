aec897f4fb958ecee76f3274b7f05b174035a0a3
Vachounet <vachounet@live.fr>
2020-10-28 17:12:41 +0100

    SystemUI: implement a viewpager for qs customizations
    Change-Id: I32cd1ba811f6e3abd67106273cbb3eb7ee93b179

---
+++ packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizerSettingsTuner.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2020 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.systemui.qs.customize;
+
+import android.content.Context;
+import android.os.UserHandle;
+import android.util.AttributeSet;
+import android.view.ContextThemeWrapper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.android.systemui.R;
+
+public class QSCustomizerSettingsTuner extends LinearLayout {
+
+    private static final String QS_SHOW_BRIGHTNESS = "qs_show_brightness";
+
+    public QSCustomizerSettingsTuner(Context context, AttributeSet attrs) {
+        super(new ContextThemeWrapper(context, R.style.edit_theme), attrs);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+    }
+}
