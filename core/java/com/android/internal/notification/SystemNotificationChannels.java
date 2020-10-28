6aecd01b2e9ae893b453cd9c4f3e6bc821cbf88d
maxwen <max.weninger@gmail.com>
2020-10-28 15:40:01 +0100

    base: add more notification channels to blockable
    vpn,usb
    
    Change-Id: I5443d46d5141a73a537057ee0fed3f55cb59661d
    (cherry picked from commit 1b597698432709b5ffe96139333fdc1cd6718c13)
    (cherry picked from commit 8e77c2539d21817fe21430c5d9d1d483b6979337)

--- core/java/com/android/internal/notification/SystemNotificationChannels.java
+++ core/java/com/android/internal/notification/SystemNotificationChannels.java
@@ -140,6 +140,7 @@ public class SystemNotificationChannels {
                 VPN,
                 context.getString(R.string.notification_channel_vpn),
                 NotificationManager.IMPORTANCE_LOW);
+        vpn.setBlockable(true);
         channelsList.add(vpn);
 
         final NotificationChannel deviceAdmin = new NotificationChannel(
@@ -164,6 +165,7 @@ public class SystemNotificationChannels {
                 USB,
                 context.getString(R.string.notification_channel_usb),
                 NotificationManager.IMPORTANCE_MIN);
+        usb.setBlockable(true);
         channelsList.add(usb);
 
         NotificationChannel foregroundChannel = new NotificationChannel(
