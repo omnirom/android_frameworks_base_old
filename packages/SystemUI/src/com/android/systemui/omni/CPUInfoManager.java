005e9dd854611b39f7c7e741a181baf9fe1c0ee2
maxwen <max.weninger@gmail.com>
2020-12-06 18:25:00 +0100

    SystemUI: move CPUInfoService files into omni package
    Change-Id: I227c81a3f1ed8e849933756a732a423a8d0da96b

--- packages/SystemUI/src/com/android/systemui/omni/CPUInfoManager.java
+++ packages/SystemUI/src/com/android/systemui/omni/CPUInfoManager.java
@@ -1,18 +1,20 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2020 The OmniROM Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-package com.android.systemui;
+package com.android.systemui.omni;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -47,7 +49,7 @@ public class CPUInfoManager extends SystemUI implements OmniSettingsService.Omni
     public void onIntSettingChanged(String key, Integer newValue) {
         if (Settings.System.OMNI_SHOW_CPU_OVERLAY.equals(key)) {
             try {
-                Intent cpuinfo = new Intent(mContext, com.android.systemui.CPUInfoService.class);
+                Intent cpuinfo = new Intent(mContext, com.android.systemui.omni.CPUInfoService.class);
                 if (newValue != null && newValue == 1) {
                     mContext.startService(cpuinfo);
                 } else {
