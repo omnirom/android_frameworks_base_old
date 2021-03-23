dbc945b383cdac9b6931f64e5d85f626d7b6d11f
Oliver Scott <olivercscott@gmail.com>
2021-03-22 22:59:11 -0400

    TtsEngines: avoid crashes caused by null engine name
    Change-Id: I5ef9d0fab7a4087d2dce5bd16e2a515903f83c51

--- core/java/android/speech/tts/TtsEngines.java
+++ core/java/android/speech/tts/TtsEngines.java
@@ -507,7 +507,7 @@ public class TtsEngines {
         for (String value : prefValues) {
             final int delimiter = value.indexOf(':');
             if (delimiter > 0) {
-                if (engineName.equals(value.substring(0, delimiter))) {
+                if ((value.substring(0, delimiter)).equals(engineName)) {
                     return value.substring(delimiter + 1);
                 }
             }
