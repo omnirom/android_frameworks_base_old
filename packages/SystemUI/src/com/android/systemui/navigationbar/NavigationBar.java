e3141d93eeb7162637cb9a9ddaae7c59cc3be410
maxwen <max.weninger@gmail.com>
2021-04-15 14:19:15 +0200

    SystemUI: proper reliable long press gesture handle to switch last app
    Using our TaskUtils
    
    SystemUI: reduce active touch height of home handle
    
    dont block all the space above
    
    Change-Id: I82eb6cf21b41b941075bc0228e1c9751b24a6731

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
@@ -159,7 +159,10 @@ import com.android.systemui.util.ViewController;
 import com.android.wm.shell.back.BackAnimation;
 import com.android.wm.shell.pip.Pip;
 
+
 import dagger.Lazy;
+
+import org.omnirom.omnilib.utils.TaskUtils;
 
 import java.io.PrintWriter;
 import java.util.Locale;
@@ -1290,6 +1293,10 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
         ButtonDispatcher homeButton = mView.getHomeButton();
         homeButton.setOnTouchListener(this::onHomeTouch);
 
+        ButtonDispatcher homeHandleButton = mView.getHomeHandle();
+        homeHandleButton.setLongClickable(true);
+        homeHandleButton.setOnLongClickListener(this::onHomeHandleLongClick);
+
         reconfigureHomeLongClick();
 
         ButtonDispatcher accessibilityButton = mView.getAccessibilityButton();
@@ -1826,6 +1833,8 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
                 useNearestRegion);
         updateButtonLocation(region, mView.getKeyButtonViewById(R.id.dpad_left), inScreenSpace);
         updateButtonLocation(region, mView.getKeyButtonViewById(R.id.dpad_right), inScreenSpace);
+        updateButtonLocation(
+                region, touchRegionCache, mView.getHomeHandle(), inScreenSpace, useNearestRegion);
         if (includeFloatingButtons && mView.getFloatingRotationButton().isVisible()) {
             // Note: this button is floating so the nearest region doesn't apply
             updateButtonLocation(
@@ -1992,4 +2001,9 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
             return false;
         }
     };
+
+    private boolean onHomeHandleLongClick(View v) {
+        TaskUtils.toggleLastApp(mContext, UserHandle.myUserId());
+        return true;
+    }
 }
