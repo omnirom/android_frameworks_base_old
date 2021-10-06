1376f44868c37d7bd20cf054f588a1a8db7a7043
maxwen <max.weninger@gmail.com>
2021-10-06 15:41:59 +0200

    SystemUI: notifications - hey  google - dont confuse rotation with orientation
    and dont use fixed 4 columns for landscape calculation
    
    Change-Id: I7d0b0ee93b33a2f89503f3acdaa017cf482a87c3

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
@@ -428,6 +428,7 @@ public class NotificationStackScrollLayout extends ViewGroup implements Dumpable
     private int mCornerRadius;
     private int mMinimumPaddings;
     private int mQsTilePadding;
+    private int mQsTileColumns;
     private boolean mSkinnyNotifsInLandscape;
     private int mSidePaddings;
     private final Rect mBackgroundAnimationRect = new Rect();
@@ -961,6 +962,7 @@ public class NotificationStackScrollLayout extends ViewGroup implements Dumpable
         mBottomMargin = res.getDimensionPixelSize(R.dimen.notification_panel_margin_bottom);
         mMinimumPaddings = res.getDimensionPixelSize(R.dimen.notification_side_paddings);
         mQsTilePadding = res.getDimensionPixelOffset(R.dimen.qs_tile_margin_horizontal);
+        mQsTileColumns = res.getInteger(R.integer.quick_settings_num_columns);
         mSkinnyNotifsInLandscape = res.getBoolean(R.bool.config_skinnyNotifsInLandscape);
         mSidePaddings = mMinimumPaddings;  // Updated in onMeasure by updateSidePadding()
         mMinInteractionHeight = res.getDimensionPixelSize(
@@ -982,7 +984,7 @@ public class NotificationStackScrollLayout extends ViewGroup implements Dumpable
             return;
         }
         final int innerWidth = viewWidth - mMinimumPaddings * 2;
-        final int qsTileWidth = (innerWidth - mQsTilePadding * 3) / 4;
+        final int qsTileWidth = (innerWidth - mQsTilePadding * (mQsTileColumns - 1)) / mQsTileColumns;
         mSidePaddings = mMinimumPaddings + qsTileWidth + mQsTilePadding;
     }
 
