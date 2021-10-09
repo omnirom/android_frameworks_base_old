972a2fbce3df19cda83d4d3dd04134299a86c5ca
maxwen <max.weninger@gmail.com>
2021-10-09 17:46:51 +0200

    SystemUI: add config_battery_hide_on_unknown to hide statusbar battery
    battery less devices dont want to show battery in statusbar
    and qs
    
    Change-Id: If43019f13fd5dd6450ee18b4d171640dcc099413

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -82,6 +82,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
     private boolean mDisplayShieldEnabled;
     // Error state where we know nothing about the current battery state
     private boolean mBatteryStateUnknown;
+    private boolean mHideBatteryOnUnknown;
     // Lazily-loaded since this is expected to be a rare-if-ever state
     private Drawable mUnknownStateDrawable;
 
@@ -113,6 +114,8 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
 
         mShowPercentAvailable = context.getResources().getBoolean(
                 com.android.internal.R.bool.config_battery_percentage_setting_available);
+        mHideBatteryOnUnknown = context.getResources().getBoolean(
+                R.bool.config_battery_hide_on_unknown);
 
         setupLayoutTransition();
 
@@ -377,11 +380,19 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
 
         if (mBatteryStateUnknown) {
             mBatteryIconView.setImageDrawable(getUnknownStateDrawable());
+            if (mHideBatteryOnUnknown) {
+                setVisibility(View.GONE);
+            }
         } else {
             mBatteryIconView.setImageDrawable(mDrawable);
+            setVisibility(View.VISIBLE);
         }
 
         updateShowPercent();
+    }
+
+    boolean isBatteryHidden() {
+        return mBatteryStateUnknown && mHideBatteryOnUnknown;
     }
 
     /**
