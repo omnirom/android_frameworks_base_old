abceeee4302627d70e4b2a9b92b697c72f0c0a59
maxwen <max.weninger@gmail.com>
2021-10-09 17:46:51 +0200

    SystemUI: add config_battery_hide_on_unknown to hide statusbar battery
    battery less devices dont want to show battery in statusbar
    and qs
    
    Change-Id: If43019f13fd5dd6450ee18b4d171640dcc099413

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -38,6 +38,7 @@ import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -82,6 +83,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
     private boolean mDisplayShieldEnabled;
     // Error state where we know nothing about the current battery state
     private boolean mBatteryStateUnknown;
+    private boolean mHideBatteryOnUnknown;
     // Lazily-loaded since this is expected to be a rare-if-ever state
     private Drawable mUnknownStateDrawable;
 
@@ -113,6 +115,8 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
 
         mShowPercentAvailable = context.getResources().getBoolean(
                 com.android.internal.R.bool.config_battery_percentage_setting_available);
+        mHideBatteryOnUnknown = context.getResources().getBoolean(
+                R.bool.config_battery_hide_on_unknown);
 
         setupLayoutTransition();
 
@@ -377,11 +381,19 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
 
         if (mBatteryStateUnknown) {
             mBatteryIconView.setImageDrawable(getUnknownStateDrawable());
+            if (mHideBatteryOnUnknown) {
+                setVisibility(View.GONE);
+            }
         } else {
             mBatteryIconView.setImageDrawable(mDrawable);
+            setVisibility(View.VISIBLE);
         }
 
         updateShowPercent();
+    }
+
+    boolean isBatteryHidden() {
+        return mBatteryStateUnknown && mHideBatteryOnUnknown;
     }
 
     /**
