e5145b708159eb905b77b37616bf76fc50807ede
maxwen <max.weninger@gmail.com>
2021-11-27 01:26:35 +0100

    base: dont enter POLICY_DIM if diming is not wanted
    if stay on while plugged is enabled and dim timout is 0
    we really dont want any dimming
    
    Change-Id: Ia5f9bdaad995286cac860b8db78a31d11daa060e

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -3316,7 +3316,8 @@ public final class PowerManagerService extends SystemService
                 || !mBootCompleted
                 || (mDisplayGroupPowerStateMapper.getUserActivitySummaryLocked(groupId)
                 & USER_ACTIVITY_SCREEN_BRIGHT) != 0
-                || mScreenBrightnessBoostInProgress) {
+                || mScreenBrightnessBoostInProgress
+                || (mStayOn && mMaximumScreenDimDurationConfig == 0)) {
             return DisplayPowerRequest.POLICY_BRIGHT;
         }
 
