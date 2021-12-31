672f3b01ef08180a4e05ade01d1302df47ba7f71
maxwen <max.weninger@gmail.com>
2021-12-31 16:06:28 +0100

    SystemUI: add missing initial call to updateResources in brightness mirror
    else initial position of brightness mirror is wrong
    and better save then sorry also call updateResources
    in reinflate
    
    Change-Id: Iab99f3b141b5ad1ac4d9a294c9070f7b747db433

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BrightnessMirrorController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BrightnessMirrorController.java
@@ -68,6 +68,7 @@ public class BrightnessMirrorController
             mBrightnessMirror.setVisibility(View.INVISIBLE);
         });
         mVisibilityCallback = visibilityCallback;
+        updateResources();
     }
 
     public void showMirror() {
@@ -148,6 +149,7 @@ public class BrightnessMirrorController
     }
 
     private void reinflate() {
+        updateResources();
         int index = mStatusBarWindow.indexOfChild(mBrightnessMirror);
         mStatusBarWindow.removeView(mBrightnessMirror);
         mBrightnessMirror = (FrameLayout) LayoutInflater.from(mStatusBarWindow.getContext())
