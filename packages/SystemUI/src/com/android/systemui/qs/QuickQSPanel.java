8de374e1e69cc29db58c89748f5de8d9f8bc2c13
maxwen <max.weninger@gmail.com>
2022-03-24 17:22:17 +0100

    SystemUI: respect QS_SHOW_BRIGHTNESS settings also in split notification shade
    partly revert of
    
    05d9591277668d7f614f2fe762bb40192e2f6af3
    Showing brightness slider in collapsed QS in split shade
    
    Change-Id: I8e634d4a8cfffbdb0ad3bf326ef664c5e30fb2f2

--- packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
@@ -48,6 +48,11 @@ public class QuickQSPanel extends QSPanel {
     }
 
     @Override
+    public void setBrightnessView(View view) {
+        // Don't add brightness view
+    }
+
+    @Override
     void initialize() {
         super.initialize();
         if (mHorizontalContentContainer != null) {
