a51196fa8d6eb303f7a122f5d9c1fbc25323ba31
micky387 <mickaelsaibi@free.fr>
2022-12-13 12:24:23 +0100

    Revert "udfps: Implement default udfps hbm provider"
    This reverts commit ea8b19b6eb05788bbaa052662719aee804f96359.
    
    Prepare for android-13.0.0_r16
    
    useless with
    https://android.googlesource.com/platform/frameworks/base/+/6eda3a1d1a48f31591314185a814e54335f84098%5E%21/#F7
    
    Change-Id: I596874e17684d03526cad1a75f89e7c549b11c66

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -604,7 +604,7 @@ public class UdfpsController implements DozeReceiver {
             @NonNull VibratorHelper vibrator,
             @NonNull UdfpsHapticsSimulator udfpsHapticsSimulator,
             @NonNull UdfpsShell udfpsShell,
-            @NonNull UdfpsHbmProvider hbmProvider,
+            @NonNull Optional<UdfpsHbmProvider> hbmProvider,
             @NonNull KeyguardStateController keyguardStateController,
             @NonNull DisplayManager displayManager,
             @Main Handler mainHandler,
@@ -636,7 +636,7 @@ public class UdfpsController implements DozeReceiver {
         mPowerManager = powerManager;
         mAccessibilityManager = accessibilityManager;
         mLockscreenShadeTransitionController = lockscreenShadeTransitionController;
-        mHbmProvider = hbmProvider;
+        mHbmProvider = hbmProvider.orElse(null);
         screenLifecycle.addObserver(mScreenObserver);
         mScreenOn = screenLifecycle.getScreenState() == ScreenLifecycle.SCREEN_ON;
         mConfigurationController = configurationController;
