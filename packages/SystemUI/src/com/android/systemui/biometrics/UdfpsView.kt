5e35dba20556f75d5f1612acbe4c7ca248c5e32d
Michael Bestas <mkbestas@lineageos.org>
2023-03-15 03:22:37 +0200

    fixup! udfps: Restore illumination dot for global hbm
    Change-Id: I30b67b5bcb5b549dca502fbb3c8501c367e15477

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsView.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsView.kt
@@ -181,7 +181,7 @@ class UdfpsView(
 
         mUdfpsDisplayMode?.enable {
             onDisplayConfigured?.run()
-            ghbmView?.drawIlluminationDot(sensorRect)
+            ghbmView?.drawIlluminationDot(RectF(sensorRect))
         }
     }
 
