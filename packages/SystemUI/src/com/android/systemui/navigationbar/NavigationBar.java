10c3d0e7eee886fd98f0b279d2362eec0a47fb06
micky387 <mickaelsaibi@free.fr>
2023-10-26 12:59:08 +0200

    [1/2] SystemUI: arrow keys while typing resurrection
    [micky387] adapt to A12 with new navigationbar package
    
    Change-Id: Idd87c6451cfd359fc97be07e308465e5205fad59
    
    SystemUI: add back dimen navigation_extra_key_width
    
    we need this for navbar extra keys
    
    Change-Id: I3522a25e8714d16c96a2f5c2d60c640a5267ee2a

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
@@ -1824,6 +1824,8 @@ public class NavigationBar extends ViewController<NavigationBarView> implements 
         updateButtonLocation(
                 region, touchRegionCache, mView.getAccessibilityButton(), inScreenSpace,
                 useNearestRegion);
+        updateButtonLocation(region, mView.getKeyButtonViewById(R.id.dpad_left), inScreenSpace);
+        updateButtonLocation(region, mView.getKeyButtonViewById(R.id.dpad_right), inScreenSpace);
         if (includeFloatingButtons && mView.getFloatingRotationButton().isVisible()) {
             // Note: this button is floating so the nearest region doesn't apply
             updateButtonLocation(
