3357abad46afc960ede0765c99843dbd9ed9e70d
micky387 <mickaelsaibi@free.fr>
2024-08-14 12:42:36 -0400

    base: Use old battery percent on hideimage with newStatusBarIcons feature
    If we hide the new battery image and the battery percent is inside, then show only the percent like the old style.
    
    Change-Id: I355ce125f8401686a576ba97440d77a2ffe003d8

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterViewController.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterViewController.java
@@ -173,6 +173,7 @@ public class BatteryMeterViewController extends ViewController<BatteryMeterView>
 
         mView.updateShowPercent();
         mView.updateShowImage();
+        mView.isImageHidden();
     }
 
     @Override
@@ -242,6 +243,7 @@ public class BatteryMeterViewController extends ViewController<BatteryMeterView>
             super.onChange(selfChange, uri);
             mView.updateShowPercent();
             mView.updateShowImage();
+            mView.isImageHidden();
             if (TextUtils.equals(uri.getLastPathSegment(),
                     Settings.Global.BATTERY_ESTIMATES_LAST_UPDATE_TIME)) {
                 // update the text for sure if the estimate in the cache was updated
